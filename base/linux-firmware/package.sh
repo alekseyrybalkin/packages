a2d74ec220f852baff28ba94a885a956d896aaf3
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-09-28 23:12:21 +0400

    linux-formware: fix paths

--- base/linux-firmware/package.sh
+++ base/linux-firmware/package.sh
@@ -12,7 +12,7 @@ kiin_install() {
   # it's sickening.
   # btw, do I really need all of these?
   mkdir -pv ${pkgdir}/lib/firmware
-  cp -v ../iwlwifi-5000-1.ucode ${pkgdir}/lib/firmware
-  cp -v ../iwlwifi-5000-2.ucode ${pkgdir}/lib/firmware
-  cp -v ../iwlwifi-5000-5.ucode ${pkgdir}/lib/firmware
+  cp -v ./iwlwifi-5000-1.ucode ${pkgdir}/lib/firmware
+  cp -v ./iwlwifi-5000-2.ucode ${pkgdir}/lib/firmware
+  cp -v ./iwlwifi-5000-5.ucode ${pkgdir}/lib/firmware
 }
