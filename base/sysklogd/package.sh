fbca4de6ee9fc01b13ea74058468cd4fbb1e7ce9
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-09-28 23:52:05 +0400

    sysklogd: do not install syslog.conf, it is in git repo now

--- base/sysklogd/package.sh
+++ base/sysklogd/package.sh
@@ -16,18 +16,4 @@ kiin_install() {
   mkdir -pv ${pkgdir}/usr/sbin
   make prefix=${pkgdir} install
   mv -v ${pkgdir}/usr/sbin/* ${pkgdir}/sbin
-
-  cat > ${pkgdir}/etc/syslog.conf << "EOF"
-# Begin /etc/syslog.conf
-
-auth,authpriv.* -/var/log/auth.log
-*.*;auth,authpriv.none -/var/log/sys.log
-daemon.* -/var/log/daemon.log
-kern.* -/var/log/kern.log
-mail.* -/var/log/mail.log
-user.* -/var/log/user.log
-*.emerg *
-
-# End /etc/syslog.conf
-EOF
 }
