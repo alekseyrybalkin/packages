ad63a0276852c3aa386625206f4c8cea7e62044f
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-09-29 20:23:18 +0400

    openssh: update to 6.1p1

--- essentials/openssh/package.sh
+++ essentials/openssh/package.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
 pkgname=openssh
-pkgver=6.0p1
+pkgver=6.1p1
+urls="http://ftp.openbsd.org/pub/OpenBSD/OpenSSH/portable/${pkgname}-${pkgver}.tar.gz"
 srctar=${pkgname}-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  #groupadd -g 50 sshd
-  #useradd -c 'sshd PrivSep' -d /var/lib/sshd -g sshd \
-  #    -s /bin/false -u 50 sshd
   ./configure --prefix=/usr \
               --sysconfdir=/etc/ssh \
               --datadir=/usr/share/sshd \
@@ -18,14 +16,10 @@ kiin_make() {
 }
 
 kiin_install() {
-  # do we need these two lines?
-  install -v -m700 -d ${pkgdir}/var/lib/sshd
-  chown -v root:sys ${pkgdir}/var/lib/sshd
-
   make DESTDIR=${pkgdir} install
-  install -v -m755 -d ${pkgdir}/usr/share/doc/openssh-6.0p1
+  install -v -m755 -d ${pkgdir}/usr/share/doc/${pkgname}-${pkgver}
   install -v -m644 INSTALL LICENCE OVERVIEW README* \
-      ${pkgdir}/usr/share/doc/openssh-6.0p1
+      ${pkgdir}/usr/share/doc/${pkgname}-${pkgver}
   # remove /var, filesystem package creates everything in there
   rm -rvf ${pkgdir}/var
 }
