db1b992de9bdf32bda67f6ae9753aad54c99c70f
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-09-29 22:17:20 +0400

    sudo: update to 1.8.6p3

--- essentials/sudo/package.sh
+++ essentials/sudo/package.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 pkgname=sudo
-pkgver=1.8.5p3
+pkgver=1.8.6p3
+urls="http://www.${pkgname}.ws/${pkgname}/dist/${pkgname}-${pkgver}.tar.gz"
 srctar=${pkgname}-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
   ./configure --prefix=/usr                   \
               --libexecdir=/usr/lib/sudo      \
-              --docdir=/usr/share/doc/sudo-1.8.5p3 \
+              --docdir=/usr/share/doc/${pkgname}-${pkgver} \
               --with-all-insults              \
               --with-env-editor               \
               --without-pam                   \
@@ -18,6 +19,8 @@ kiin_make() {
 
 kiin_install() {
   make DESTDIR=${pkgdir} install
+  # do not replace /etc/sudoers, since it is in git repo now
+  mv -v ${pkgdir}/etc/sudoers{,.packaged}
   # remove /var, filesystem package creates everything in there
   rm -rvf ${pkgdir}/var
 }
