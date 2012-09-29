011c77b028d21cadf39f304a76776216384f3dfa
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-09-29 19:28:16 +0400

    udev: update to 193

--- base/udev/package.sh
+++ base/udev/package.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
 pkgname=udev
-pkgver=189
+pkgver=193
+urls="http://anduin.linuxfromscratch.org/sources/other/udev-lfs-${pkgver}.tar.bz2 \
+  http://www.freedesktop.org/software/systemd/systemd-${pkgver}.tar.xz"
 srctar=systemd-${pkgver}.tar.xz
 srcdir=${location}/systemd-${pkgver}
 
 kiin_make() {
   MAKEFLAGS=
-  tar -xvf ../udev-lfs-189.tar.bz2
-  make -f udev-lfs-189/Makefile.lfs
+  tar -xvf ../udev-lfs-${pkgver}.tar.bz2
+  make -f udev-lfs-${pkgver}/Makefile.lfs
 }
 
 kiin_install() {
-  make DESTDIR=${pkgdir} -f udev-lfs-189/Makefile.lfs install
-  #bash udev-lfs-189/init-net-rules.sh
+  make DESTDIR=${pkgdir} -f udev-lfs-${pkgver}/Makefile.lfs install
 }
