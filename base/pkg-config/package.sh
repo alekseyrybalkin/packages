4f5d90420901f954d099837e5429c72a5fad4a12
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-09-29 20:08:19 +0400

    pkg-config: update to 0.27.1

--- base/pkg-config/package.sh
+++ base/pkg-config/package.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 pkgname=pkg-config
-pkgver=0.27
+pkgver=0.27.1
+urls="http://pkgconfig.freedesktop.org/releases/${pkgname}-${pkgver}.tar.gz"
 srctar=${pkgname}-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
   ./configure --prefix=/usr         \
-              --docdir=/usr/share/doc/pkg-config-0.27
+              --docdir=/usr/share/doc/${pkgname}-${pkgver}
   make
 }
 
