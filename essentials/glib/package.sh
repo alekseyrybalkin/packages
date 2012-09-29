344c710f7579c53bb9069baeb075634b33607849
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-09-29 19:57:35 +0400

    glib: update to 2.34.0

--- essentials/glib/package.sh
+++ essentials/glib/package.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 pkgname=glib
-pkgver=2.32.4
+pkgver=2.34.0
+urls="http://ftp.gnome.org/pub/gnome/sources/${pkgname}/2.34/${pkgname}-${pkgver}.tar.xz"
 srctar=${pkgname}-${pkgver}.tar.xz
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
   ./configure --prefix=/usr \
               --sysconfdir=/etc \
-              --with-pcre=system &&
+              --with-pcre=system
   make
 }
 
