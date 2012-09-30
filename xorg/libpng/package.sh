5db5bdff1592b6aa969769df028ec79c894a22fd
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-10-01 01:07:05 +0400

    libpng: add apng patch

--- xorg/libpng/package.sh
+++ xorg/libpng/package.sh
@@ -2,11 +2,13 @@
 
 pkgname=libpng
 pkgver=1.5.13
-urls="http://downloads.sourceforge.net/${pkgname}/${pkgname}-${pkgver}.tar.xz"
+urls="http://downloads.sourceforge.net/${pkgname}/${pkgname}-${pkgver}.tar.xz \
+  http://sourceforge.net/projects/${pkgname}-apng/files/${pkgname}15/${pkgver}/${pkgname}-${pkgver}-apng.patch.gz"
 srctar=${pkgname}-${pkgver}.tar.xz
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
+  gzip -cd ../${pkgname}-${pkgver}-apng.patch.gz | patch -p1
   ./configure --prefix=/usr \
     --disable-static
   make
