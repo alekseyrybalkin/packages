f63f995366cf1458e63bee7594f0ff9c08d71f2f
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-09-30 04:17:39 +0400

    add libxslt

---
+++ essentials/libxslt/package.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+pkgname=libxslt
+pkgver=1.1.27
+urls="http://xmlsoft.org/sources/${pkgname}-${pkgver}.tar.gz"
+srctar=${pkgname}-${pkgver}.tar.gz
+srcdir=${location}/${pkgname}-${pkgver}
+
+kiin_make() {
+  ./configure --prefix=/usr --disable-static
+  make
+}
+
+kiin_install() {
+  make DESTDIR=${pkgdir} install
+}
