6b5987c68f50cde416bac9e05e6b22a6362dc062
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-09-30 17:36:10 +0400

    add xcb-util

---
+++ xorg/xcb/xcb-util/package.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+pkgname=xcb-util
+pkgver=0.3.9
+urls="http://xorg.freedesktop.org/releases/individual/xcb/${pkgname}-${pkgver}.tar.bz2"
+srctar=${pkgname}-${pkgver}.tar.bz2
+srcdir=${location}/${pkgname}-${pkgver}
+
+XORG_PREFIX="/usr"
+XORG_CONFIG="--prefix=$XORG_PREFIX \
+  --sysconfdir=/etc \
+  --mandir=$XORG_PREFIX/share/man \
+  --localstatedir=/var"
+
+kiin_make() {
+  ./configure $XORG_CONFIG
+  make
+}
+
+kiin_install() {
+  make DESTDIR=${pkgdir} install
+}
