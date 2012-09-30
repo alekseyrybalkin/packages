4db71a7409daa875f90543419c07f7de62c269f3
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-10-01 01:10:45 +0400

    rxvt-unicode: make terminfo work as expected

--- gui/rxvt-unicode/package.sh
+++ gui/rxvt-unicode/package.sh
@@ -7,10 +7,38 @@ srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  ./configure --prefix=/usr --enable-everything
+  ./configure --prefix=/usr \
+    --with-terminfo=/usr/share/terminfo \
+    --enable-256-color \
+    --enable-combining \
+    --enable-fading \
+    --enable-font-styles \
+    --enable-iso14755 \
+    --enable-keepscrolling \
+    --enable-lastlog \
+    --enable-mousewheel \
+    --enable-next-scroll \
+    --enable-perl \
+    --enable-pixbuf \
+    --enable-pointer-blank \
+    --enable-rxvt-scroll \
+    --enable-selectionscrolling \
+    --enable-slipwheeling \
+    --enable-smart-resize \
+    --enable-startup-notification \
+    --enable-transparency \
+    --enable-unicode3 \
+    --enable-utmp \
+    --enable-wtmp \
+    --enable-xft \
+    --enable-xim \
+    --enable-xterm-scroll \
+    --disable-frills
   make
 }
 
 kiin_install() {
+  export TERMINFO=${pkgdir}/usr/share/terminfo
+  install -d $TERMINFO
   make DESTDIR=${pkgdir} install
 }
