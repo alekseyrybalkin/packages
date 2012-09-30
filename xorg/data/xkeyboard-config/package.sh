7ba1092aa2979d0f8154fabb7eb4624201a0a6a0
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-09-30 19:01:32 +0400

    xkeyboard-config: update to 2.7

--- xorg/data/xkeyboard-config/package.sh
+++ xorg/data/xkeyboard-config/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=xkeyboard-config
-pkgver=1.7
+pkgver=2.7
 urls="http://xorg.freedesktop.org/releases/individual/data/${pkgname}/${pkgname}-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
