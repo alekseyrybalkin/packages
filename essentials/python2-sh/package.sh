d3362786290782f6a81370bb6369326f3d8f7ca3
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-09-30 15:01:11 +0400

    add python2-sh

---
+++ essentials/python2-sh/package.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+pkgname=python2-sh
+pkgver=1.03
+urls="http://pypi.python.org/packages/source/s/sh/sh-${pkgver}.tar.gz"
+srctar=sh-${pkgver}.tar.gz
+srcdir=${location}/sh-${pkgver}
+
+kiin_make() {
+  echo "nothing to make..."
+}
+
+kiin_install() {
+  python2 setup.py install --root=${pkgdir}
+}
