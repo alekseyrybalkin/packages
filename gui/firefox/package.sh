9c71fc5a398186aa5380db8d5efbd8b10bb111a7
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-10-01 01:12:52 +0400

    add firefox

---
+++ gui/firefox/package.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+pkgname=firefox
+pkgver=15.0.1
+urls="ftp://ftp.mozilla.org/pub/mozilla.org/${pkgname}/releases/${pkgver}/source/${pkgname}-${pkgver}.source.tar.bz2"
+srctar=${pkgname}-${pkgver}.source.tar.bz2
+srcdir=${location}/mozilla-release
+
+kiin_make() {
+  cp ../mozconfig .
+  sed -i 's/\(MOZ_PKG_FATAL_WARNINGS =\).*/\1 0/' \
+    browser/installer/Makefile.in
+  sed -i 's# ""##' browser/base/Makefile.in
+  make -f client.mk
+  make -C firefox-build-dir/browser/installer
+}
+
+kiin_install() {
+  mkdir -pv ${pkgdir}/usr/lib/${pkgname}-${pkgver}
+  tar -xvf firefox-build-dir/dist/${pkgname}-${pkgver}.en-US.linux-$(uname -m).tar.bz2 \
+    -C ${pkgdir}/usr/lib/${pkgname}-${pkgver} --strip-components=1
+  mkdir -p ${pkgdir}/usr/bin
+  ln -sfv ../lib/${pkgname}-${pkgver}/firefox ${pkgdir}/usr/bin
+  mkdir -pv ${pkgdir}/usr/lib/mozilla/plugins
+  ln -sfv ../mozilla/plugins ${pkgdir}/usr/lib/${pkgname}-${pkgver}
+  chown -R -v root:root ${pkgdir}/usr/lib/${pkgname}-${pkgver}
+}
