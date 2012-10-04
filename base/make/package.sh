7eee60e05af10cb149ac82e2fd25a488fd2a068f
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-10-04 15:16:20 +0400

    make: update upstream fixes to version 3

--- base/make/package.sh
+++ base/make/package.sh
@@ -2,11 +2,13 @@
 
 pkgname=make
 pkgver=3.82
+urls="http://ftp.gnu.org/gnu/${pkgname}/${pkgname}-${pkgver}.tar.bz2 \
+  http://www.linuxfromscratch.org/patches/lfs/development/${pkgname}-${pkgver}-upstream_fixes-3.patch"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  patch -Np1 -i ../make-3.82-upstream_fixes-2.patch
+  patch -Np1 -i ../make-3.82-upstream_fixes-3.patch
   ./configure --prefix=/usr
   make
 }
