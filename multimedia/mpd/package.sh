df8b230c5cb151327bbab381bb50e6e240a88687
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-10-08 01:57:50 +0400

    mpd: update to 0.17.2

--- multimedia/mpd/package.sh
+++ multimedia/mpd/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=mpd
-pkgver=0.17.1
+pkgver=0.17.2
 urls="http://downloads.sourceforge.net/musicpd/${pkgname}-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
