44175085cb0fca0c125e7dbee0cfd58cf9402360
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-10-12 04:19:10 +0400

    mesa: update to 9.0

--- xorg/mesa/package.sh
+++ xorg/mesa/package.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
 pkgname=mesa
-pkgver=8.0.4
-urls="ftp://ftp.freedesktop.org/pub/${pkgname}/${pkgver}/MesaLib-${pkgver}.tar.bz2 \
-  http://www.linuxfromscratch.org/patches/blfs/svn/MesaLib-8.0.4-llvm-3.1-fixes-1.patch"
+pkgver=9.0
+urls="ftp://ftp.freedesktop.org/pub/${pkgname}/${pkgver}/MesaLib-${pkgver}.tar.bz2"
 srctar=MesaLib-${pkgver}.tar.bz2
 srcdir=${location}/Mesa-${pkgver}
 
 XORG_PREFIX="/usr"
 
 kiin_make() {
-  sed 's@FLAGS=\"-g@FLAGS=\"@' -i configure
-  patch -Np1 -i ../MesaLib-8.0.4-llvm-3.1-fixes-1.patch
+  autoreconf -fi
   ./configure --prefix=$XORG_PREFIX \
+    --sysconfdir=/etc \
     --enable-texture-float \
     --enable-gles1 \
     --enable-gles2 \
