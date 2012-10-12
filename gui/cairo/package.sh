57a26a3988328c56f8a63f2a2c0e553e87be9816
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-10-12 04:20:38 +0400

    cairo: update to 1.12.4

--- gui/cairo/package.sh
+++ gui/cairo/package.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
 pkgname=cairo
-pkgver=1.12.2
+pkgver=1.12.4
 urls="http://cairographics.org/releases/${pkgname}-${pkgver}.tar.xz \
-  http://www.linuxfromscratch.org/patches/blfs/svn/${pkgname}-${pkgver}-expose-snapshot-1.patch"
+  http://www.linuxfromscratch.org/patches/blfs/svn/${pkgname}-${pkgver}-expose_snapshot-1.patch"
 srctar=${pkgname}-${pkgver}.tar.xz
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  sed -i 's#check-plt,sh ##' src/Makefile.in
-  patch -p1 < ../${pkgname}-${pkgver}-expose-snapshot-1.patch
+  patch -Np1 -i ../${pkgname}-${pkgver}-expose_snapshot-1.patch
   ./configure --prefix=/usr --disable-static --enable-tee --enable-xcb
   make
 }
