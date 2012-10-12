1120b5c50d29a5d418344671e80399d53ceaf7d5
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-10-13 01:44:54 +0400

    add parted

---
+++ crypt/parted/package.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+pkgname=parted
+pkgver=3.1
+urls="http://ftp.gnu.org/gnu/${pkgname}/${pkgname}-${pkgver}.tar.xz"
+srctar=${pkgname}-${pkgver}.tar.xz
+srcdir=${location}/${pkgname}-${pkgver}
+
+kiin_make() {
+  ./configure --prefix=/usr --disable-static
+  make
+}
+
+kiin_install() {
+  make DESTDIR=${pkgdir} install
+}
