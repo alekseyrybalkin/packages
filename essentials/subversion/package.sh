25f34b58af372f1ade2d7807cd4bcac7c0c483e7
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-10-12 04:21:45 +0400

    subversion: update to 1.7.7

--- essentials/subversion/package.sh
+++ essentials/subversion/package.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 pkgname=subversion
-pkgver=1.7.6
+pkgver=1.7.7
+urls="http://archive.apache.org/dist/${pkgname}/${pkgname}-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
   MAKEFLAGS=
-  patch -Np1 -i ../subversion-1.7.6-build_fixes-1.patch
+  patch -p0 -i ../subversion.rpath.fix.patch
   ./configure --prefix=/usr --with-apr=/usr --with-apr-util=/usr \
     --with-zlib=/usr --with-neon=/usr --with-sqlite=/usr
   make external-all
@@ -17,8 +18,8 @@ kiin_make() {
 
 kiin_install() {
   make DESTDIR=${pkgdir} install
-  install -v -m755 -d ${pkgdir}/usr/share/doc/subversion-1.7.6
-  cp -v -R doc/* ${pkgdir}/usr/share/doc/subversion-1.7.6
+  install -v -m755 -d ${pkgdir}/usr/share/doc/subversion-${pkgver}
+  cp -v -R doc/* ${pkgdir}/usr/share/doc/subversion-${pkgver}
   make DESTDIR=${pkgdir} install-swig-pl
   make DESTDIR=${pkgdir} install-swig-py
 }
