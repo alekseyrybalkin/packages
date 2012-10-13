df2c03078a66d27ac86006a73512328ebba47301
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-10-13 04:03:12 +0400

    add busybox-small

---
+++ crypt/busybox-small/package.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+pkgname=busybox-small
+pkgver=1.20.2
+urls="http://www.busybox.net/downloads/busybox-${pkgver}.tar.bz2"
+srctar=busybox-${pkgver}.tar.bz2
+srcdir=${location}/busybox-${pkgver}
+
+kiin_make() {
+  cp ../config .config
+  sed '1,1i#include <sys/resource.h>' -i include/libbb.h
+  make
+}
+
+kiin_install() {
+  mkdir ${pkgdir}/bin
+  cp busybox ${pkgdir}/bin/busybox-small
+}
