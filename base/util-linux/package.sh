5143275f4a3eb86bdbb78c92ceb23fe4a02d6775
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-10-14 16:09:37 +0400

    util-linux: update to 2.22.1 and build kill

--- base/util-linux/package.sh
+++ base/util-linux/package.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 pkgname=util-linux
-pkgver=2.22
+pkgver=2.22.1
+urls="http://www.kernel.org/pub/linux/utils/${pkgname}/v2.22/${pkgname}-${pkgver}.tar.xz"
 srctar=${pkgname}-${pkgver}.tar.xz
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
   sed -i -e 's@etc/adjtime@var/lib/hwclock/adjtime@g' \
       $(grep -rl '/etc/adjtime' .)
-  #mkdir -pv /var/lib/hwclock
-  ./configure --disable-su --disable-sulogin --disable-login --disable-kill
+  ./configure --disable-su --disable-sulogin --disable-login
   make
 }
 
