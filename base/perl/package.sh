6d6014facf3a49010a866c4dfb9a4059c53efa46
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-10-17 07:28:33 +0400

    perl: fix Config_heavy.pl and .packlist

--- base/perl/package.sh
+++ base/perl/package.sh
@@ -2,6 +2,7 @@
 
 pkgname=perl
 pkgver=5.16.1
+urls="http://www.cpan.org/src/5.0/${pkgname}-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
 
@@ -22,4 +23,8 @@ kiin_make() {
 
 kiin_install() {
   make install
+  sed -i -e 's/\/sources\/base\/perl\/kiin-dest//g' \
+    ${pkgdir}/usr/lib/perl5/5.16.1/x86_64-linux/Config_heavy.pl
+  sed -i -e 's/\/sources\/base\/perl\/kiin-dest//g' \
+    ${pkgdir}/usr/lib/perl5/5.16.1/x86_64-linux/.packlist
 }
