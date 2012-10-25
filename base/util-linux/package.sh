047695aedee3fd12d4a5cdccfb48c77d3266e0a6
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-10-26 03:24:42 +0400

    util-linux: build without udev support

--- base/util-linux/package.sh
+++ base/util-linux/package.sh
@@ -9,7 +9,10 @@ srcdir=${location}/${pkgname}-${pkgver}
 kiin_make() {
   sed -i -e 's@etc/adjtime@var/lib/hwclock/adjtime@g' \
       $(grep -rl '/etc/adjtime' .)
-  ./configure --disable-su --disable-sulogin --disable-login
+  ./configure --without-udev \
+    --disable-su \
+    --disable-sulogin \
+    --disable-login
   make
 }
 
