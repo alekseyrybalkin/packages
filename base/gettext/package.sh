f8d412531897c3ed5fcea71f9118af0223781dc1
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-10-26 03:23:55 +0400

    gettext: build without system libxml and build after xz

--- base/gettext/package.sh
+++ base/gettext/package.sh
@@ -2,6 +2,7 @@
 
 pkgname=gettext
 pkgver=0.18.1.1
+urls="http://ftp.gnu.org/gnu/gettext/gettext-${pkgver}.tar.gz"
 srctar=${pkgname}-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
 
@@ -9,6 +10,7 @@ kiin_make() {
   sed -i -e '/gets is a/d' gettext-*/*/stdio.in.h
   ./configure --prefix=/usr \
               --docdir=/usr/share/doc/gettext-0.18.1.1 \
+              --with-included-libxml \
               --without-git
   make
 }
