189d02740b3c2ed4f4c7a9fe5b79b63bad1222dd
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-10-25 14:12:18 +0400

    bison: update to 2.6.4

--- base/bison/package.sh
+++ base/bison/package.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 pkgname=bison
-pkgver=2.6.2
+pkgver=2.6.4
+urls="http://ftp.gnu.org/gnu/bison/bison-${pkgver}.tar.xz"
 srctar=${pkgname}-${pkgver}.tar.xz
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
   ./configure --prefix=/usr
-  echo '#define YYENABLE_NLS 1' >> lib/config.h
   make
 }
 
