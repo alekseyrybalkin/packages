43fada000c281ae8ee3f3370745ebdb471f99bf8
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-10-26 23:30:46 +0400

    add poppler and epdfview

---
+++ gui/epdfview/package.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+pkgname=epdfview
+pkgver=0.1.8
+urls="http://trac.emma-soft.com/epdfview/chrome/site/releases/epdfview-${pkgver}.tar.bz2 \
+  http://www.linuxfromscratch.org/patches/blfs/svn/epdfview-${pkgver}-fixes-1.patch"
+srctar=${pkgname}-${pkgver}.tar.bz2
+srcdir=${location}/${pkgname}-${pkgver}
+
+kiin_make() {
+  patch -Np1 -i ../epdfview-0.1.8-fixes-1.patch
+  ./configure --prefix=/usr
+  make
+}
+
+kiin_install() {
+  make DESTDIR=${pkgdir} install
+}
