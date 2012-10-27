5181f42cf4bc1cadab57b4535511f6fc92b03580
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-10-27 21:43:02 +0400

    add acpi

---
+++ essentials/acpi/package.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+pkgname=acpi
+pkgver=1.6
+urls="http://downloads.sourceforge.net/acpiclient/${pkgname}-${pkgver}.tar.gz"
+srctar=${pkgname}-${pkgver}.tar.gz
+srcdir=${location}/${pkgname}-${pkgver}
+
+kiin_make() {
+  ./configure --prefix=/usr
+  make
+}
+
+kiin_install() {
+  make DESTDIR=${pkgdir} install
+}
