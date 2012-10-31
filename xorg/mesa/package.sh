4dec7609cf939e8bd3fcf8f9f4075a87dfa37469
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-11-01 00:44:37 +0400

    mesa: install demos, change dri driver from i915 to i965, fix some build options

--- xorg/mesa/package.sh
+++ xorg/mesa/package.sh
@@ -2,30 +2,40 @@
 
 pkgname=mesa
 pkgver=9.0
-urls="ftp://ftp.freedesktop.org/pub/${pkgname}/${pkgver}/MesaLib-${pkgver}.tar.bz2"
+urls="ftp://ftp.freedesktop.org/pub/${pkgname}/${pkgver}/MesaLib-${pkgver}.tar.bz2 \
+      http://www.linuxfromscratch.org/patches/blfs/svn/MesaLib-9.0-add_xdemos-4.patch"
 srctar=MesaLib-${pkgver}.tar.bz2
 srcdir=${location}/Mesa-${pkgver}
 
 XORG_PREFIX="/usr"
 
 kiin_make() {
+  patch -Np1 -i ../MesaLib-9.0-add_xdemos-4.patch
   autoreconf -fi
   ./configure --prefix=$XORG_PREFIX \
     --sysconfdir=/etc \
-    --enable-texture-float \
+    --with-dri-driverdir=/usr/lib/xorg/modules/dri \
+    --with-gallium-drivers='' \
+    --with-dri-drivers='i965' \
+    --enable-egl \
+    --with-egl-platforms=x11,drm \
+    --enable-shared-glapi \
+    --enable-glx-tls \
+    --enable-dri \
+    --enable-glx \
+    --enable-osmesa \
     --enable-gles1 \
     --enable-gles2 \
-    --enable-shared-glapi \
+    --enable-texture-float \
     --enable-shared-dricore \
-    --enable-glx-tls \
-    --without-nouveau \
-    --with-gallium-drivers='i915' \
-    --with-dri-drivers='i915'
+    --without-nouveau
   make
+  make -C xdemos DEMOS_PREFIX=/usr
 }
 
 kiin_install() {
   make DESTDIR=${pkgdir} install
+  make -C xdemos DEMOS_PREFIX=/usr DESTDIR=${pkgdir} install
   mkdir -pv ${pkgdir}${XORG_PREFIX}/share/doc/MesaLib-${pkgver}
   cp -rfv docs/* ${pkgdir}${XORG_PREFIX}/share/doc/MesaLib-${pkgver}
 }
