e872fe3108c7d585f32fbea5ff2a010a94504503
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-11-01 04:59:29 +0400

    add hicolor-icon-theme

---
+++ gui/hicolor-icon-theme/package.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+pkgname=hicolor-icon-theme
+pkgver=0.12
+urls="http://icon-theme.freedesktop.org/releases/hicolor-icon-theme-${pkgver}.tar.gz"
+srctar=${pkgname}-${pkgver}.tar.gz
+srcdir=${location}/${pkgname}-${pkgver}
+
+kiin_make() {
+  ./configure --prefix=/usr
+  make
+}
+
+kiin_install() {
+  make DESTDIR=${pkgdir} install
+}
