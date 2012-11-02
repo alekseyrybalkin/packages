45fc3eb302b8ff084bab7c8e619f73289b044a8e
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-11-02 19:39:50 +0400

    binutils: enable multilib

--- base/binutils/package.sh
+++ base/binutils/package.sh
@@ -5,13 +5,14 @@ pkgver=2.23
 urls="http://ftp.gnu.org/gnu/binutils/binutils-${pkgver}.tar.gz"
 srctar=${pkgname}-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
+multilib=1
 
 kiin_make() {
   rm -fv etc/standards.info
   sed -i.bak '/^INFO/s/standards.info //' etc/Makefile.in
   mkdir -v binutils-build
   cd binutils-build
-  ../configure --prefix=/usr --enable-shared
+  ../configure --prefix=/usr --enable-shared --enable-multilib
   make tooldir=${pkgdir}/usr
 }
 
