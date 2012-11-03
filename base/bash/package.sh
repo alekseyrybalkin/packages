bb7db14fda3a86c9f492f58b5090a69300f1bffc
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-11-04 02:45:06 +0400

    bash: update to 4.2.039

--- base/bash/package.sh
+++ base/bash/package.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
 pkgname=bash
-pkgver=4.2.037
-urls="http://ftp.gnu.org/gnu/${pkgname}/${pkgname}-4.2.tar.gz \
-  http://www.linuxfromscratch.org/patches/lfs/development/${pkgname}-4.2-fixes-9.patch"
+pkgver=4.2.039
+_patchlevel=039
+urls="http://ftp.gnu.org/gnu/${pkgname}/${pkgname}-4.2.tar.gz"
+if [ $_patchlevel -gt 000 ]; then
+  for (( p=1; p<=$((10#${_patchlevel})); p++ )); do
+    urls="$urls http://ftp.gnu.org/gnu/bash/bash-4.2-patches/bash42-$(printf "%03d" $p)"
+  done
+fi
 srctar=${pkgname}-4.2.tar.gz
 srcdir=${location}/${pkgname}-4.2
 
 kiin_make() {
-  patch -Np1 -i ../bash-4.2-fixes-9.patch
+  for (( p=1; p<=$((10#${_patchlevel})); p++ )); do
+    patch -Np0 -i ../bash42-$(printf "%03d" $p)
+  done
   ./configure --prefix=/usr                     \
               --bindir=/bin                     \
               --htmldir=/usr/share/doc/bash-4.2 \
