5c490d1f924890d617422368603aa75e53921923
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-11-04 02:45:33 +0400

    apr-util: compile with openssl support

--- essentials/apr-util/package.sh
+++ essentials/apr-util/package.sh
@@ -7,7 +7,11 @@ srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  ./configure --prefix=/usr --with-apr=/usr --with-gdbm=/usr
+  ./configure --prefix=/usr \
+    --with-apr=/usr \
+    --with-gdbm=/usr \
+    --with-openssl=/usr \
+    --with-crypto
   make
 }
 
