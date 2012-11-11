1d60049b4938885e32295db79cdacd46fa02725c
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-11-11 19:00:42 +0400

    sqlite: compile without readline support, to suppress history file creation

--- essentials/sqlite/package.sh
+++ essentials/sqlite/package.sh
@@ -9,7 +9,7 @@ srcdir=${location}/${pkgname}-autoconf-${_pkgver}
 
 kiin_make() {
   export CFLAGS="$CFLAGS -DSQLITE_ENABLE_FTS3=1 -DSQLITE_ENABLE_COLUMN_METADATA=1 -DSQLITE_ENABLE_UNLOCK_NOTIFY -DSQLITE_SECURE_DELETE"
-  ./configure --prefix=/usr --disable-static
+  ./configure --prefix=/usr --disable-static --disable-readline
   make
 }
 
