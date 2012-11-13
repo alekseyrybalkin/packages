5a97c1160b542c6f876b7795d0bdfa6a2263eb89
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-11-14 02:59:30 +0400

    add pciutils

---
+++ essentials/pciutils/package.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+pkgname=pciutils
+pkgver=3.1.10
+urls="http://ftp.kernel.org/pub/software/utils/pciutils/pciutils-${pkgver}.tar.xz"
+srctar=${pkgname}-${pkgver}.tar.xz
+srcdir=${location}/${pkgname}-${pkgver}
+
+kiin_make() {
+  make PREFIX=/usr \
+    SHAREDIR=/usr/share/misc \
+    MANDIR=/usr/share/man \
+    SHARED=yes \
+    ZLIB=no all
+}
+
+kiin_install() {
+  make PREFIX=/usr \
+    SHAREDIR=/usr/share/misc \
+    MANDIR=/usr/share/man \
+    SHARED=yes \
+    ZLIB=no \
+    DESTDIR=${pkgdir} install install-lib
+}
