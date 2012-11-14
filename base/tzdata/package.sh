55cce2509990f80ecb2d733d28775a88df1f5e4b
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-11-15 01:54:54 +0400

    tzdata: update to 2012j

--- base/tzdata/package.sh
+++ base/tzdata/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=tzdata
-pkgver=2012i
+pkgver=2012j
 urls="http://www.iana.org/time-zones/repository/releases/${pkgname}${pkgver}.tar.gz"
 _srctar=${pkgname}${pkgver}.tar.gz
 _srcdir=${location}/${pkgname}${pkgver}
