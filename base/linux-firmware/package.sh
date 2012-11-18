05dc40d40910133107b101b8077b62d28c98ee07
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-11-19 02:17:44 +0400

    linux-firmware: make it hostname-specific

--- base/linux-firmware/package.sh
+++ base/linux-firmware/package.sh
@@ -15,7 +15,9 @@ kiin_install() {
   # it's sickening.
   # btw, do I really need all of these?
   mkdir -pv ${pkgdir}/lib/firmware
-  cp -v ./iwlwifi-5000-1.ucode ${pkgdir}/lib/firmware
-  cp -v ./iwlwifi-5000-2.ucode ${pkgdir}/lib/firmware
-  cp -v ./iwlwifi-5000-5.ucode ${pkgdir}/lib/firmware
+  if [ `hostname` == 'alien' ]; then
+    cp -v ./iwlwifi-5000-1.ucode ${pkgdir}/lib/firmware
+    cp -v ./iwlwifi-5000-2.ucode ${pkgdir}/lib/firmware
+    cp -v ./iwlwifi-5000-5.ucode ${pkgdir}/lib/firmware
+  fi
 }
