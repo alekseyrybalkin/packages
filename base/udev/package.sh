787ac9c8e74700d7c8a52a0a6d5d8e8f4da6fd9c
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-11-23 12:54:29 +0400

    udev: update to 196

--- base/udev/package.sh
+++ base/udev/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=udev
-pkgver=195
+pkgver=196
 urls="http://anduin.linuxfromscratch.org/sources/other/udev-lfs-${pkgver}.tar.bz2 \
   http://www.freedesktop.org/software/systemd/systemd-${pkgver}.tar.xz"
 srctar=systemd-${pkgver}.tar.xz
@@ -10,9 +10,19 @@ srcdir=${location}/systemd-${pkgver}
 kiin_make() {
   MAKEFLAGS=
   tar -xvf ../udev-lfs-${pkgver}.tar.bz2
+  sed -i -e '/udevadm hwdb/d' udev-lfs-${pkgver}/Makefile.lfs
+  sed -i -e 's/create/update/' src/udev/udevadm-hwdb.c
   make -f udev-lfs-${pkgver}/Makefile.lfs
 }
 
 kiin_install() {
   make DESTDIR=${pkgdir} -f udev-lfs-${pkgver}/Makefile.lfs install
+}
+
+kiin_after_install() {
+  udevadm hwdb --update
+}
+
+kiin_after_upgrade() {
+  kiin_after_install
 }
