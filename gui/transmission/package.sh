189fb6eb5d44e9d3f74aff3ea45fcc3ea61f902e
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-11-29 01:51:28 +0400

    transmission: build without gtk, update to 2.73

--- gui/transmission/package.sh
+++ gui/transmission/package.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 pkgname=transmission
-pkgver=2.60
+pkgver=2.73
 urls="http://download.transmissionbt.com/files/transmission-${pkgver}.tar.xz"
 srctar=${pkgname}-${pkgver}.tar.xz
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  ./configure --prefix=/usr --with-gtk
+  ./configure --prefix=/usr --without-gtk
   make
 }
 
