142272b088557b1f034132730f060d4149b7f926
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-12-02 23:45:10 +0400

    linux-firmware: remove alien-specific instructions

--- base/linux-firmware/package.sh
+++ base/linux-firmware/package.sh
@@ -11,15 +11,6 @@ kiin_make() {
 }
 
 kiin_install() {
-  # install some stupid proprietary shit for wifi-card to work.
-  # it's sickening.
-  # btw, do I really need all of these?
-  mkdir -pv ${pkgdir}/lib/firmware
-  if [ `hostname` == 'alien' ]; then
-    cp -v ./iwlwifi-5000-1.ucode ${pkgdir}/lib/firmware
-    cp -v ./iwlwifi-5000-2.ucode ${pkgdir}/lib/firmware
-    cp -v ./iwlwifi-5000-5.ucode ${pkgdir}/lib/firmware
-  fi
   if [ `hostname` == 'ritchie' ]; then
     mkdir -pv ${pkgdir}/lib/firmware/brcm
     cp -v ./brcm/bcm43xx-0.fw ${pkgdir}/lib/firmware/brcm
