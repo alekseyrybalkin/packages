1680925cc46f3a89245ead42f231f13ee96df8d2
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-12-14 00:27:30 +0400

    add gnupg

---
+++ crypt/gnupg/package.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+pkgname=gnupg
+pkgver=1.4.12
+urls="ftp://ftp.gnupg.org/gcrypt/gnupg/gnupg-${pkgver}.tar.bz2"
+srctar=${pkgname}-${pkgver}.tar.bz2
+srcdir=${location}/${pkgname}-${pkgver}
+
+kiin_make() {
+  ./configure --prefix=/usr --libexecdir=/usr/lib
+  make
+}
+
+kiin_install() {
+  make DESTDIR=${pkgdir} install
+  install -v -m755 -d ${pkgdir}/usr/share/doc/gnupg-${pkgver}
+  mv -v ${pkgdir}/usr/share/gnupg/FAQ ${pkgdir}/usr/share/doc/gnupg-${pkgver}
+  install -v -m644 \
+    doc/{highlights-1.4.txt,OpenPGP,samplekeys.asc,DETAILS,*.texi} \
+    ${pkgdir}/usr/share/doc/gnupg-${pkgver}
+}
