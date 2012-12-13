1faa4e317b7dc44d25aa9fb3fda27805d5943c54
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-12-13 22:45:00 +0400

    util-linux: update to 2.22.2

--- base/util-linux/package.sh
+++ base/util-linux/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=util-linux
-pkgver=2.22.1
+pkgver=2.22.2
 urls="http://www.kernel.org/pub/linux/utils/${pkgname}/v2.22/${pkgname}-${pkgver}.tar.xz"
 srctar=${pkgname}-${pkgver}.tar.xz
 srcdir=${location}/${pkgname}-${pkgver}
