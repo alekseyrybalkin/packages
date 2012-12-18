7528a4799492240062605cb774a93dec9e20234a
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-12-18 06:30:03 +0400

    pixman: update to 0.28.2

--- xorg/pixman/package.sh
+++ xorg/pixman/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=pixman
-pkgver=0.28.0
+pkgver=0.28.2
 urls="http://cairographics.org/releases/${pkgname}-${pkgver}.tar.gz"
 srctar=${pkgname}-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
