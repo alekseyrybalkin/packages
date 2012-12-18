b0ade9f0767efccaac49c02adbd3c334da81161b
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-12-18 06:30:49 +0400

    swig: update to 2.0.9

--- essentials/swig/package.sh
+++ essentials/swig/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=swig
-pkgver=2.0.8
+pkgver=2.0.9
 urls="http://prdownloads.sourceforge.net/swig/swig-${pkgver}.tar.gz"
 srctar=${pkgname}-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
