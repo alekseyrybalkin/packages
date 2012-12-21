84f1d200e556a7273082e6be81a679a458dc9b4e
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-12-21 22:35:38 +0400

    kbd: update to 1.15.5

--- base/kbd/package.sh
+++ base/kbd/package.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
 
 pkgname=kbd
-pkgver=1.15.3
-urls="http://ftp.altlinux.org/pub/people/legion/kbd/kbd-${pkgver}.tar.gz \
-  http://www.linuxfromscratch.org/patches/lfs/development/kbd-${pkgver}-upstream_fixes-1.patch \
-  http://www.linuxfromscratch.org/patches/lfs/development/kbd-${pkgver}-backspace-1.patch"
+pkgver=1.15.5
+urls="http://ftp.altlinux.org/pub/people/legion/kbd/kbd-${pkgver}.tar.gz"
 srctar=${pkgname}-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  patch -Np1 -i ../kbd-${pkgver}-upstream_fixes-1.patch
-  patch -Np1 -i ../kbd-${pkgver}-backspace-1.patch
   sed -i '/guardado\ el/s/\(^.*en\ %\)\(.*\)/\14\$\2/' po/es.po
   sed -i 's/\(RESIZECONS_PROGS=\)yes/\1no/' configure &&
   sed -i 's/resizecons.8 //' man/man8/Makefile.in &&
   touch -d '2011-05-07 08:30' configure.ac
-  ./configure --prefix=/usr --datadir=/lib/kbd
+  ./configure --prefix=/usr \
+    --datadir=/lib/kbd \
+    --disable-vlock
   make
 }
 
