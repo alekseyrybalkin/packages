b9e0617f382243ac116c232b3877704264906b61
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-12-23 08:13:50 +0400

    sed: update to 4.2.2

--- base/sed/package.sh
+++ base/sed/package.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
 pkgname=sed
-pkgver=4.2.1
-urls="http://ftp.gnu.org/gnu/sed/sed-${pkgver}.tar.bz2 \
-   http://www.linuxfromscratch.org/patches/lfs/development/sed-${pkgver}-testsuite_fixes-1.patch"
+pkgver=4.2.2
+urls="http://ftp.gnu.org/gnu/sed/sed-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  patch -Np1 -i ../sed-${pkgver}-testsuite_fixes-1.patch
   ./configure --prefix=/usr --bindir=/bin --htmldir=/usr/share/doc/sed-${pkgver}
   make
   make html
