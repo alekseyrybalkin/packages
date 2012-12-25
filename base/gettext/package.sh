3dc13726ab0ec5106657026046239766311b9948
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-12-25 12:13:29 +0400

    gettext: update to 0.18.2

--- base/gettext/package.sh
+++ base/gettext/package.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 pkgname=gettext
-pkgver=0.18.1.1
+pkgver=0.18.2
 urls="http://ftp.gnu.org/gnu/gettext/gettext-${pkgver}.tar.gz"
 srctar=${pkgname}-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  sed -i -e '/gets is a/d' gettext-*/*/stdio.in.h
   if [ -z "$KIIN_LIB32" ]; then
     ./configure --prefix=/usr \
                 --docdir=/usr/share/doc/gettext-${pkgver} \
