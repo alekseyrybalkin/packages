c033e283041c520e88bcdd411de2418948f6ca6c
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-12-28 09:48:18 +0400

    transmission: update to 2.75

--- gui/transmission/package.sh
+++ gui/transmission/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=transmission
-pkgver=2.73
+pkgver=2.75
 urls="http://download.transmissionbt.com/files/transmission-${pkgver}.tar.xz"
 srctar=${pkgname}-${pkgver}.tar.xz
 srcdir=${location}/${pkgname}-${pkgver}
