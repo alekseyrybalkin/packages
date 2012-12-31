90352a7d9b39e12dade6fb264ec76cb669fa5d7e
Aleksey Rybalkin <aleksey@rybalkin.org>
2012-12-31 12:35:34 +0400

    groff: repair build without ghostscript

--- base/groff/package.sh
+++ base/groff/package.sh
@@ -12,7 +12,9 @@ kiin_make() {
 }
 
 kiin_install() {
+  mkdir -p ${pkgdir}/usr/share/doc/groff-1.22/pdf
   make prefix=${pkgdir}/usr install
   ln -sv eqn ${pkgdir}/usr/bin/geqn
   ln -sv tbl ${pkgdir}/usr/bin/gtbl
+  rm -rvf ${pkgdir}/usr/share/doc/groff-1.22/pdf
 }
