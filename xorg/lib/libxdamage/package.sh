f80e5b8260be18a8d2c12388635b5aa08c67635b
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-01-03 21:01:44 +0400

    libxdamage: update to 1.1.4

--- xorg/lib/libxdamage/package.sh
+++ xorg/lib/libxdamage/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=libxdamage
-pkgver=1.1.3
+pkgver=1.1.4
 urls="http://xorg.freedesktop.org/releases/individual/lib/libXdamage-${pkgver}.tar.bz2"
 srctar=libXdamage-${pkgver}.tar.bz2
 srcdir=${location}/libXdamage-${pkgver}
