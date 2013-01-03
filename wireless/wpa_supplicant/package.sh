6eafc7ad5fc317dd1dcbc658fc74932f9ac9e12c
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-01-03 14:32:48 +0400

    wpa_supplicant: update to 1.1

--- wireless/wpa_supplicant/package.sh
+++ wireless/wpa_supplicant/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=wpa_supplicant
-pkgver=1.0
+pkgver=1.1
 urls="http://hostap.epitest.fi/releases/${pkgname}-${pkgver}.tar.gz"
 srctar=${pkgname}-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
