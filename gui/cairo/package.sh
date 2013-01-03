f199a02c686b2b5c04153df70afc7468cfecc568
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-01-03 14:29:18 +0400

    cairo: remove expose_snapshot patch

--- gui/cairo/package.sh
+++ gui/cairo/package.sh
@@ -2,13 +2,11 @@
 
 pkgname=cairo
 pkgver=1.12.8
-urls="http://cairographics.org/releases/${pkgname}-${pkgver}.tar.xz \
-  http://www.linuxfromscratch.org/patches/blfs/svn/${pkgname}-${pkgver}-expose_snapshot-1.patch"
+urls="http://cairographics.org/releases/${pkgname}-${pkgver}.tar.xz"
 srctar=${pkgname}-${pkgver}.tar.xz
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  patch -Np1 -i ../${pkgname}-${pkgver}-expose_snapshot-1.patch
   ./configure --prefix=/usr --disable-static --enable-tee --enable-xcb
   make
 }
