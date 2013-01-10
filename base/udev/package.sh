3693f9d65757782e716cf3da6efdefda2e81e861
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-01-11 03:41:40 +0400

    udev: update to 197

--- base/udev/package.sh
+++ base/udev/package.sh
@@ -1,24 +1,21 @@
 #!/bin/sh
 
 pkgname=udev
-pkgver=196
-urls="http://anduin.linuxfromscratch.org/sources/other/udev-lfs-${pkgver}.tar.bz2 \
+pkgver=197
+urls="http://anduin.linuxfromscratch.org/sources/other/udev-lfs-${pkgver}-1.tar.bz2 \
   http://www.freedesktop.org/software/systemd/systemd-${pkgver}.tar.xz"
 srctar=systemd-${pkgver}.tar.xz
 srcdir=${location}/systemd-${pkgver}
 
 kiin_make() {
   MAKEFLAGS=
-  tar -xvf ../udev-lfs-${pkgver}.tar.bz2
-  sed -i -e 's/HAVE___SECURE_GETENV/HAVE_SECURE_GETENV/' udev-lfs-${pkgver}/cfg.h
-  sed -i -e '/udevadm hwdb/d' udev-lfs-${pkgver}/Makefile.lfs
-  sed -i -e 's/create/update/' src/udev/udevadm-hwdb.c
-  make -f udev-lfs-${pkgver}/Makefile.lfs
+  tar -xvf ../udev-lfs-${pkgver}-1.tar.bz2
+  make -f udev-lfs-${pkgver}-1/Makefile.lfs
 }
 
 kiin_install() {
   MAKEFLAGS=
-  make DESTDIR=${pkgdir} -f udev-lfs-${pkgver}/Makefile.lfs install
+  make DESTDIR=${pkgdir} -f udev-lfs-${pkgver}-1/Makefile.lfs install
 }
 
 kiin_after_install() {
