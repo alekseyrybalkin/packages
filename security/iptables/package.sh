8dc6ab0953a182194cc1ba484bf17bf95818ed29
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-01-18 01:20:40 +0400

    add iptables

---
+++ security/iptables/package.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+pkgname=iptables
+vcs="git"
+gittag=v1.4.17
+pkgver=1.4.17
+srcdir=${location}/${pkgname}-${pkgver}
+
+kiin_make() {
+  ./autogen.sh
+  LDFLAGS="-L$PWD/libiptc/.libs" ./configure --prefix=/usr \
+    --exec-prefix= \
+    --bindir=/usr/bin \
+    --with-xtlibdir=/lib/xtables \
+    --with-pkgconfigdir=/usr/lib/pkgconfig \
+    --enable-libipq \
+    --enable-devel
+  make
+}
+
+kiin_install() {
+  make DESTDIR=${pkgdir} install
+  ln -sfv ../../sbin/xtables-multi ${pkgdir}/usr/bin/iptables-xml
+  for file in libip4tc libip6tc libipq libiptc libxtables; do
+    ln -sfv ../../lib/`readlink ${pkgdir}/lib/${file}.so` ${pkgdir}/usr/lib/${file}.so
+    rm -v ${pkgdir}/lib/${file}.so
+  done
+}
