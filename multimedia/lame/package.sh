7cd3c6fa1e04a652d57ffc41435bc01aedf86287
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-01-24 10:03:34 +0400

    add lame

---
+++ multimedia/lame/package.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+pkgname=lame
+pkgver=3.99.5
+urls="http://downloads.sourceforge.net/lame/lame-${pkgver}.tar.gz"
+srctar=${pkgname}-${pkgver}.tar.gz
+srcdir=${location}/${pkgname}-${pkgver}
+
+kiin_make() {
+  ./configure --prefix=/usr \
+    --enable-mp3rtp \
+    --disable-static
+  make
+}
+
+kiin_install() {
+  make DESTDIR=${pkgdir} pkghtmldir=/usr/share/doc/lame-${pkgver} install
+}
