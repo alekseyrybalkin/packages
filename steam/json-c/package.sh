b4293ab8698e456b6f27e459db37e12e900743bf
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-01-26 10:09:21 +0400

    json-c: update to latest git version

--- steam/json-c/package.sh
+++ steam/json-c/package.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
 pkgname=json-c
-pkgver=0.9
-urls="http://oss.metaparadigm.com/json-c/json-c-${pkgver}.tar.gz"
-srctar=${pkgname}-${pkgver}.tar.gz
+vcs="git"
+gittag=3ae296f6946100e533a4358629aa34e74501a039
+pkgver=0.10
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
+  MAKEFLAGS=
+  sed -i -e 's/AM_CONFIG_HEADER/AC_CONFIG_HEADERS/g' configure.in
+  ./autogen.sh
   ./configure --prefix=/usr --disable-static --libdir=$LIBDIR
   make
 }
 
 kiin_install() {
+  MAKEFLAGS=
   make DESTDIR=${pkgdir} install
 }
