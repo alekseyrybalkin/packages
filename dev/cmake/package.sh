8a566c53b3ec35b86e0535173915778ddc5f269f
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-01-26 09:18:44 +0400

    add cmake

---
+++ dev/cmake/package.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+pkgname=cmake
+pkgver=2.8.10.2
+urls="http://www.cmake.org/files/v2.8/cmake-${pkgver}.tar.gz"
+srctar=${pkgname}-${pkgver}.tar.gz
+srcdir=${location}/${pkgname}-${pkgver}
+
+kiin_make() {
+  ./bootstrap --prefix=/usr \
+    --system-libs \
+    --mandir=/share/man \
+    --docdir=/share/doc/cmake-${pkgver} \
+    --no-system-libarchive
+  make
+}
+
+kiin_install() {
+  make DESTDIR=${pkgdir} install
+}
