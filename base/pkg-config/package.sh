1168d753ae7cf2fcc5d400ae29667dfdecb18cdb
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-01-28 12:18:28 +0400

    pkg-config: build from git trunk

--- base/pkg-config/package.sh
+++ base/pkg-config/package.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 pkgname=pkg-config
-pkgver=0.27.1
-urls="http://pkgconfig.freedesktop.org/releases/${pkgname}-${pkgver}.tar.gz"
-srctar=${pkgname}-${pkgver}.tar.gz
+vcs="git"
+gittag=74ceac54ef6f9247c00f08eecd8cca811a3c5934
+pkgver=0.28.20130124
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
+  sed -i -e 's/AM_PROG_CC_STDC/AC_PROG_CC/g' configure.ac
+  sed -i -e 's/AM_PROG_CC_STDC/AC_PROG_CC/g' glib/configure.ac
+  ./autogen.sh
   ./configure --prefix=/usr         \
               --with-internal-glib  \
               --docdir=/usr/share/doc/${pkgname}-${pkgver}
