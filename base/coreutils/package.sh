f8873be7a1d445579dca663a09ec2e904a56e818
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-01-29 19:26:51 +0400

    coreutils: move more programs to /bin for non-bash shells like dash

--- base/coreutils/package.sh
+++ base/coreutils/package.sh
@@ -21,7 +21,7 @@ kiin_install() {
   make DESTDIR=${pkgdir} install
   mv -v ${pkgdir}/usr/bin/{cat,chgrp,chmod,chown,cp,date,dd,df,echo} ${pkgdir}/bin
   mv -v ${pkgdir}/usr/bin/{false,ln,ls,mkdir,mknod,mv,pwd,rm} ${pkgdir}/bin
-  mv -v ${pkgdir}/usr/bin/{rmdir,stty,sync,true,uname} ${pkgdir}/bin
+  mv -v ${pkgdir}/usr/bin/{rmdir,stty,sync,true,uname,test,[} ${pkgdir}/bin
   mv -v ${pkgdir}/usr/bin/chroot ${pkgdir}/usr/sbin
   mv -v ${pkgdir}/usr/share/man/man1/chroot.1 ${pkgdir}/usr/share/man/man8/chroot.8
   sed -i s/\"1\"/\"8\"/1 ${pkgdir}/usr/share/man/man8/chroot.8
