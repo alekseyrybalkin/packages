bf37622852e68f5d6e519a24afd66f98cc8dbd69
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-01-29 19:27:44 +0400

    e2fsprogs: update to 1.42.7

--- base/e2fsprogs/package.sh
+++ base/e2fsprogs/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=e2fsprogs
-pkgver=1.42.6
+pkgver=1.42.7
 urls=" http://prdownloads.sourceforge.net/${pkgname}/${pkgname}-${pkgver}.tar.gz"
 srctar=${pkgname}-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
@@ -9,13 +9,13 @@ srcdir=${location}/${pkgname}-${pkgver}
 kiin_make() {
   mkdir -v build
   cd build
-  ../configure --prefix=/usr         \
-              --with-root-prefix="" \
-              --enable-elf-shlibs   \
-              --disable-libblkid    \
-              --disable-libuuid     \
-              --disable-uuidd       \
-              --disable-fsck
+  ../configure --prefix=/usr \
+    --with-root-prefix="" \
+    --enable-elf-shlibs \
+    --disable-libblkid \
+    --disable-libuuid \
+    --disable-uuidd \
+    --disable-fsck
   make
 }
 
@@ -26,8 +26,6 @@ kiin_install() {
   make DESTDIR=${pkgdir} install-libs
   chmod -v u+w ${pkgdir}/usr/lib/{libcom_err,libe2p,libext2fs,libss}.a
   gunzip -v ${pkgdir}/usr/share/info/libext2fs.info.gz
-  #install-info --dir-file=/usr/share/info/dir /usr/share/info/libext2fs.info
   makeinfo -o      doc/com_err.info ../lib/et/com_err.texinfo
   install -v -m644 doc/com_err.info ${pkgdir}/usr/share/info
-  #install-info --dir-file=/usr/share/info/dir /usr/share/info/com_err.info
 }
