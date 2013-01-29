aecf862df85fa34840a1043e7849fb4f942c4665
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-01-29 19:25:38 +0400

    libjpeg: update to libjpeg-turbo 1.2.1

--- gui/libjpeg/package.sh
+++ gui/libjpeg/package.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 
-pkgname=libjpeg
-pkgver=8d
-urls="http://www.ijg.org/files/jpegsrc.v${pkgver}.tar.gz"
-srctar=jpegsrc.v${pkgver}.tar.gz
-srcdir=${location}/jpeg-${pkgver}
+pkgname=libjpeg-turbo
+pkgver=1.2.1
+urls="http://downloads.sourceforge.net/libjpeg-turbo/libjpeg-turbo-${pkgver}.tar.gz"
+srctar=${pkgname}-${pkgver}.tar.gz
+srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  ./configure --prefix=/usr --libdir=$LIBDIR
+  ./configure --prefix=/usr \
+    --mandir=/usr/share/man \
+    --with-jpeg8 \
+    --disable-static \
+    --libdir=$LIBDIR
   make
 }
 
 kiin_install() {
-  make DESTDIR=${pkgdir} install
+  make DESTDIR=${pkgdir} \
+    docdir=/usr/share/doc/${pkgname} \
+    exampledir=/usr/share/doc/${pkgname} \
+    install
 }
