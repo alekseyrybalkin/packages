c8091a04b1a2c81e470bcb4124d2e786320da6e4
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-01-29 19:40:34 +0400

    add nasm, nspr and nss

---
+++ multimedia/nasm/package.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+pkgname=nasm
+pkgver=2.10.07
+urls="http://www.nasm.us/pub/nasm/releasebuilds/${pkgver}/nasm-${pkgver}.tar.xz"
+srctar=${pkgname}-${pkgver}.tar.xz
+srcdir=${location}/${pkgname}-${pkgver}
+
+kiin_make() {
+  ./configure --prefix=/usr
+  make
+}
+
+kiin_install() {
+  make INSTALLROOT=${pkgdir} install
+}
