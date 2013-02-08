e661e8225f42ea321368fd1762bbd427d6d168e8
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-02-08 16:26:29 +0400

    libdrm: update to 2.4.42

--- xorg/libdrm/package.sh
+++ xorg/libdrm/package.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 pkgname=libdrm
-pkgver=2.4.41
+pkgver=2.4.42
 urls="http://dri.freedesktop.org/${pkgname}/${pkgname}-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  sed -i -e "/man\/Makefile/d" configure.ac
   autoreconf --force --install
   ./configure --prefix=$XORG_PREFIX \
     --enable-udev \
