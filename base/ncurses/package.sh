a2667ca4102df831b4f97bd6e0ef02bd80c4fb8f
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-02-08 15:58:12 +0400

    ncurses: install pkgconfig files

--- base/ncurses/package.sh
+++ base/ncurses/package.sh
@@ -7,9 +7,13 @@ srctar=${pkgname}-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  ./configure --prefix=/usr --mandir=/usr/share/man --with-shared \
-              --without-debug --enable-widec                      \
-              --with-install-prefix=${pkgdir}
+  ./configure --prefix=/usr \
+    --mandir=/usr/share/man \
+    --with-shared \
+    --without-debug \
+    --enable-pc-files \
+    --enable-widec \
+    --with-install-prefix=${pkgdir}
   make
 }
 
@@ -18,10 +22,11 @@ kiin_install() {
   mkdir -pv ${pkgdir}/lib
   mv -v ${pkgdir}/usr/lib/libncursesw.so.5* ${pkgdir}/lib
   ln -sfv ../../lib/libncursesw.so.5 ${pkgdir}/usr/lib/libncursesw.so
-  for lib in ncurses form panel menu ; do \
-      rm -vf ${pkgdir}/usr/lib/lib${lib}.so ; \
-      echo "INPUT(-l${lib}w)" > ${pkgdir}/usr/lib/lib${lib}.so ; \
-      ln -sfv lib${lib}w.a ${pkgdir}/usr/lib/lib${lib}.a ; \
+  for lib in ncurses form panel menu ; do
+    rm -vf ${pkgdir}/usr/lib/lib${lib}.so
+    echo "INPUT(-l${lib}w)" > ${pkgdir}/usr/lib/lib${lib}.so
+    ln -sfv lib${lib}w.a ${pkgdir}/usr/lib/lib${lib}.a
+    ln -svf ${lib}w.pc ${pkgdir}/usr/lib/pkgconfig/${lib}.pc
   done
   ln -sfv libncurses++w.a ${pkgdir}/usr/lib/libncurses++.a
   rm -vf ${pkgdir}/usr/lib/libcursesw.so
