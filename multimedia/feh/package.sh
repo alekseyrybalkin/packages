e22e66a89a726875fcec5d52ce747180f8761656
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-02-13 18:38:04 +0400

    feh: update to 2.9

--- multimedia/feh/package.sh
+++ multimedia/feh/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=feh
-pkgver=2.8
+pkgver=2.9
 urls="http://feh.finalrewind.org/feh-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
