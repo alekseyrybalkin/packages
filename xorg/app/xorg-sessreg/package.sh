ec833fef44a34f758979bd1ae8bbd5cbf9ea8cb5
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-02-13 18:38:50 +0400

    xorg-sessreg: update to 1.0.8

--- xorg/app/xorg-sessreg/package.sh
+++ xorg/app/xorg-sessreg/package.sh
@@ -2,7 +2,7 @@
 
 _pkgname=sessreg
 pkgname=xorg-${_pkgname}
-pkgver=1.0.7
+pkgver=1.0.8
 urls="http://xorg.freedesktop.org/releases/individual/app/${_pkgname}-${pkgver}.tar.bz2"
 srctar=${_pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${_pkgname}-${pkgver}
