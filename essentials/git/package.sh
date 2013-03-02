2a0294fe7fea72be7fcd5728c2272fb75ae8687f
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-03-02 11:37:18 +0400

    git: update to 1.8.1.5

--- essentials/git/package.sh
+++ essentials/git/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=git
-pkgver=1.8.1.4
+pkgver=1.8.1.5
 urls="http://${pkgname}-core.googlecode.com/files/${pkgname}-${pkgver}.tar.gz \
   http://${pkgname}-core.googlecode.com/files/${pkgname}-manpages-${pkgver}.tar.gz"
 srctar=${pkgname}-${pkgver}.tar.gz
