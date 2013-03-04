8472fbf831e3a566e6423f2af68919be415766dc
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-03-04 04:11:11 +0400

    tzdata: update to 2013a

--- base/tzdata/package.sh
+++ base/tzdata/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=tzdata
-pkgver=2012j
+pkgver=2013a
 urls="http://www.iana.org/time-zones/repository/releases/${pkgname}${pkgver}.tar.gz"
 _srctar=${pkgname}${pkgver}.tar.gz
 _srcdir=${location}/${pkgname}${pkgver}
