8a4c0485a6d3958101e139f014d4a5cbe88f48d6
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-03-10 01:34:09 +0400

    xorg-server: update to 1.14.0

--- xorg/xorg-server/package.sh
+++ xorg/xorg-server/package.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
 pkgname=xorg-server
-pkgver=1.13.2.901
-urls="http://xorg.freedesktop.org/releases/individual/xserver/${pkgname}-${pkgver}.tar.bz2 \
-  http://www.linuxfromscratch.org/patches/blfs/svn/xorg-server-${pkgver}-use_pixman_glyph_cache-1.patch"
+pkgver=1.14.0
+urls="http://xorg.freedesktop.org/releases/individual/xserver/${pkgname}-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  patch -Np1 -i ../xorg-server-${pkgver}-use_pixman_glyph_cache-1.patch
   ./configure $XORG_CONFIG \
     --with-xkb-output=/var/lib/xkb \
     --enable-install-setuid \
