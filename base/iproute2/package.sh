cffa6629343d89118aae0708ee079fe0c3c231c7
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-03-09 12:46:46 +0400

    iproute2: update to 3.8.0

--- base/iproute2/package.sh
+++ base/iproute2/package.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 pkgname=iproute2
-pkgver=3.7.0
+pkgver=3.8.0
 urls="http://www.kernel.org/pub/linux/utils/net/${pkgname}/${pkgname}-${pkgver}.tar.xz"
 srctar=${pkgname}-${pkgver}.tar.xz
-srcdir=${location}/iproute-${pkgver}
+srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
   sed -i '/^TARGETS/s@arpd@@g' misc/Makefile
@@ -16,5 +16,5 @@ kiin_make() {
 kiin_install() {
   make DESTDIR=${pkgdir}              \
       MANDIR=/usr/share/man           \
-      DOCDIR=/usr/share/doc/${pkgname}-${pkgver} install
+      DOCDIR=/usr/share/doc/${pkgname} install
 }
