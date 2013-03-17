7079327197c7e28a66ccf3e1acccd76fe9a96eb9
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-03-17 18:37:04 +0400

    man-pages: update to 3.50

--- base/man-pages/package.sh
+++ base/man-pages/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=man-pages
-pkgver=3.48
+pkgver=3.50
 urls="http://www.kernel.org/pub/linux/docs/${pkgname}/${pkgname}-${pkgver}.tar.xz"
 srctar=${pkgname}-${pkgver}.tar.xz
 srcdir=${location}/${pkgname}-${pkgver}
