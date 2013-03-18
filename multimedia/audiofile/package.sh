9a2ae5f7ffbb8ed8eb00fbb91d1e719a0cd861ef
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-03-18 19:37:09 +0400

    audiofile: update to 0.3.6

--- multimedia/audiofile/package.sh
+++ multimedia/audiofile/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=audiofile
-pkgver=0.3.5
+pkgver=0.3.6
 urls="http://ftp.gnome.org/pub/gnome/sources/audiofile/0.3/audiofile-${pkgver}.tar.xz"
 srctar=${pkgname}-${pkgver}.tar.xz
 srcdir=${location}/${pkgname}-${pkgver}
