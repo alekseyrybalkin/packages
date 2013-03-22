a5b02b866a01ab5728e4b575d9d62e716d243b57
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-03-22 17:59:19 +0400

    xf86-video-intel: update to 2.21.5

--- xorg/driver/xf86-video-intel/package.sh
+++ xorg/driver/xf86-video-intel/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=xf86-video-intel
-pkgver=2.21.4
+pkgver=2.21.5
 urls="http://xorg.freedesktop.org/releases/individual/driver/${pkgname}-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
