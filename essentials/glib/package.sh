d07a4f2b86e210fbf9cfb528ad5bfba9599bdba0
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-03-22 17:58:13 +0400

    glib: update to 2.35.9

--- essentials/glib/package.sh
+++ essentials/glib/package.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 pkgname=glib
-pkgver=2.34.3
-urls="http://ftp.gnome.org/pub/gnome/sources/${pkgname}/2.34/${pkgname}-${pkgver}.tar.xz"
+pkgver=2.35.9
+urls="http://ftp.gnome.org/pub/gnome/sources/${pkgname}/2.35/${pkgname}-${pkgver}.tar.xz"
 srctar=${pkgname}-${pkgver}.tar.xz
 srcdir=${location}/${pkgname}-${pkgver}
 
