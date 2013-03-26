5f433afd88f0f6213fb0a860b620f24ed08bb0cd
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-03-26 11:16:07 +0400

    glib: update to 2.36.0

--- essentials/glib/package.sh
+++ essentials/glib/package.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 pkgname=glib
-pkgver=2.35.9
-urls="http://ftp.gnome.org/pub/gnome/sources/${pkgname}/2.35/${pkgname}-${pkgver}.tar.xz"
+pkgver=2.36.0
+urls="http://ftp.gnome.org/pub/gnome/sources/${pkgname}/2.36/${pkgname}-${pkgver}.tar.xz"
 srctar=${pkgname}-${pkgver}.tar.xz
 srcdir=${location}/${pkgname}-${pkgver}
 
