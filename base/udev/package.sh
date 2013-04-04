42e6f9e717cec785dedb254c0b89f647d6401556
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-04-04 09:30:17 +0400

    udev: update to 200

--- base/udev/package.sh
+++ base/udev/package.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 pkgname=udev
-pkgver=199
-urls="http://anduin.linuxfromscratch.org/sources/other/udev-lfs-${pkgver}-2.tar.bz2 \
+pkgver=200
+urls="http://anduin.linuxfromscratch.org/sources/other/udev-lfs-${pkgver}-1.tar.bz2 \
   http://www.freedesktop.org/software/systemd/systemd-${pkgver}.tar.xz"
 srctar=systemd-${pkgver}.tar.xz
 srcdir=${location}/systemd-${pkgver}
 
 kiin_make() {
   MAKEFLAGS=
-  tar -xvf ../udev-lfs-${pkgver}-2.tar.bz2
-  make -f udev-lfs-${pkgver}-2/Makefile.lfs
+  tar -xvf ../udev-lfs-${pkgver}-1.tar.bz2
+  make -f udev-lfs-${pkgver}-1/Makefile.lfs
 }
 
 kiin_install() {
   MAKEFLAGS=
-  make DESTDIR=${pkgdir} -f udev-lfs-${pkgver}-2/Makefile.lfs install
+  make DESTDIR=${pkgdir} -f udev-lfs-${pkgver}-1/Makefile.lfs install
 }
 
 kiin_after_install() {
