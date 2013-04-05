46a585d957cb5ceeaef2bfa1f3d66d096485b752
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-04-05 13:59:57 +0400

    bring back boost

---
+++ essentials/boost/package.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+pkgname=boost
+pkgver=1.52.0
+_pkgver=1_52_0
+urls="http://downloads.sourceforge.net/boost/boost_${_pkgver}.tar.bz2"
+srctar=${pkgname}_${_pkgver}.tar.bz2
+srcdir=${location}/${pkgname}_${_pkgver}
+
+kiin_make() {
+  ./bootstrap.sh --prefix=/usr
+  ./b2 stage threading=multi link=shared
+}
+
+kiin_install() {
+  ./b2 --prefix=${pkgdir}/usr install threading=multi link=shared
+}
