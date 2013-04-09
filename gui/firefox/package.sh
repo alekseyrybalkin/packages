6ce57661a559c2f6ceb262edad7516ae04a8ed89
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-04-10 01:55:48 +0400

    split firefox into xulrunner and firefox

--- gui/firefox/package.sh
+++ gui/firefox/package.sh
@@ -12,16 +12,13 @@ kiin_make() {
   echo "mk_add_options MOZ_MAKE_FLAGS='${MAKEFLAGS}'" >> mozconfig
   sed -i 's# ""##' browser/base/Makefile.in
   make -f client.mk
-  make -C firefox-build-dir/browser/installer
 }
 
 kiin_install() {
-  mkdir -pv ${pkgdir}/usr/lib/${pkgname}-${pkgver}
-  tar -xvf firefox-build-dir/dist/${pkgname}-${pkgver}.en-US.linux-$(uname -m).tar.bz2 \
-    -C ${pkgdir}/usr/lib/${pkgname}-${pkgver} --strip-components=1
-  mkdir -p ${pkgdir}/usr/bin
-  ln -sfv ../lib/${pkgname}-${pkgver}/firefox ${pkgdir}/usr/bin
+  make -C firefox-build-dir DESTDIR=${pkgdir} install
+  mkdir -pv ${pkgdir}/usr/bin
+  ln -sfv ../lib/firefox-${pkgver}/firefox ${pkgdir}/usr/bin
+  ln -sfv ../xulrunner-${pkgver} ${pkgdir}/usr/lib/firefox-${pkgver}/xulrunner
   mkdir -pv ${pkgdir}/usr/lib/mozilla/plugins
-  ln -sfv ../mozilla/plugins ${pkgdir}/usr/lib/${pkgname}-${pkgver}
-  chown -R -v root:root ${pkgdir}/usr/lib/${pkgname}-${pkgver}
+  ln -sfv ../mozilla/plugins ${pkgdir}/usr/lib/firefox-${pkgver}
 }
---
+++ gui/xulrunner/package.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+pkgname=xulrunner
+pkgver=20.0
+urls="ftp://ftp.mozilla.org/pub/mozilla.org/firefox/releases/${pkgver}/source/firefox-${pkgver}.source.tar.bz2"
+srctar=firefox-${pkgver}.source.tar.bz2
+srcdir=${location}/mozilla-release
+
+kiin_make() {
+  cp ../mozconfig .
+  sed -i -e '/MOZ_MAKE_FLAGS/d' mozconfig
+  echo "mk_add_options MOZ_MAKE_FLAGS='${MAKEFLAGS}'" >> mozconfig
+  make -f client.mk
+}
+
+kiin_install() {
+  make -C xulrunner-build-dir DESTDIR=${pkgdir} install
+
+  mkdir -pv ${pkgdir}/usr/lib/mozilla/plugins
+  rm -rf ${pkgdir}/usr/lib/xulrunner-${pkgver}/plugins
+  ln -sv ../mozilla/plugins ${pkgdir}/usr/lib/xulrunner-${pkgver}
+
+  chmod -v 755 ${pkgdir}/usr/lib/xulrunner-${pkgver}/libxpcom.so \
+    ${pkgdir}/usr/lib/xulrunner-devel-${pkgver}/sdk/bin/xpcshell
+
+  for library in libmozalloc.so libmozjs.so libxpcom.so libxul.so; do
+    ln -sfv ../../../xulrunner-${pkgver}/$library \
+      ${pkgdir}/usr/lib/xulrunner-devel-${pkgver}/sdk/lib/$library
+    ln -sfv xulrunner-${pkgver}/$library ${pkgdir}/usr/lib/$library
+  done
+
+  ln -sfv ../xulrunner-devel-${pkgver}/sdk/bin/run-mozilla.sh \
+    ${pkgdir}/usr/lib/xulrunner-${pkgver}
+  ln -sfv ../xulrunner-devel-${pkgver}/sdk/bin/xpcshell \
+    ${pkgdir}/usr/lib/xulrunner-${pkgver}
+}
