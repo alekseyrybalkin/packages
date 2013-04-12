7b9b0b38f46d4f9c242e52bad84375e7c54184d6
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-04-13 01:38:13 +0400

    libmpdclient: update to 2.8

--- multimedia/libmpdclient/package.sh
+++ multimedia/libmpdclient/package.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 pkgname=libmpdclient
-pkgver=2.7
-urls="http://downloads.sourceforge.net/project/musicpd/${pkgname}/${pkgver}/${pkgname}-${pkgver}.tar.gz"
-srctar=${pkgname}-${pkgver}.tar.gz
+pkgver=2.8
+urls="http://www.musicpd.org/download/${pkgname}/2/${pkgname}-${pkgver}.tar.bz2"
+srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  ./configure --prefix=/usr --disable-static
+  ./configure --prefix=/usr \
+    --disable-static \
+    --disable-documentation
   make
 }
 
