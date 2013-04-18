dfd15c6d9eaa4021a0fdd063c175d29f7d555427
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-04-18 10:52:25 +0400

    add fluxbox

---
+++ gui/fluxbox/package.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+pkgname=fluxbox
+pkgver=1.3.5
+urls="http://downloads.sourceforge.net/fluxbox/fluxbox-${pkgver}.tar.bz2"
+srctar=${pkgname}-${pkgver}.tar.bz2
+srcdir=${location}/${pkgname}-${pkgver}
+
+kiin_make() {
+  ./configure --prefix=/usr
+  make
+}
+
+kiin_install() {
+  make DESTDIR=${pkgdir} install
+}
