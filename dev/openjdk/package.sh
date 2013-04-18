79cbe7a04ae8697ad7510fbad52627f2a2e6da29
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-04-18 16:15:26 +0400

    openjdk: update to latest version

--- dev/openjdk/package.sh
+++ dev/openjdk/package.sh
@@ -1,53 +1,64 @@
 #!/bin/sh
 
 pkgname=openjdk
-pkgver=1.7.0.9
-icedtea_ver=2.3.3
+vcs="mercurial"
+hg_pkgname=icedtea
+hgtag=397dd0f8e62a
+pkgver=7+
 rhino_ver=1_7R3
-urls="http://icedtea.classpath.org/download/source/icedtea-${icedtea_ver}.tar.gz \
-  http://anduin.linuxfromscratch.org/files/BLFS/OpenJDK-${pkgver}/corba.tar.gz \
-  http://anduin.linuxfromscratch.org/files/BLFS/OpenJDK-${pkgver}/hotspot.tar.gz \
-  http://anduin.linuxfromscratch.org/files/BLFS/OpenJDK-${pkgver}/openjdk.tar.gz \
-  http://anduin.linuxfromscratch.org/files/BLFS/OpenJDK-${pkgver}/jaxp.tar.gz \
-  http://anduin.linuxfromscratch.org/files/BLFS/OpenJDK-${pkgver}/jaxws.tar.gz \
-  http://anduin.linuxfromscratch.org/files/BLFS/OpenJDK-${pkgver}/langtools.tar.gz \
-  http://anduin.linuxfromscratch.org/files/BLFS/OpenJDK-${pkgver}/jdk.tar.gz \
-  http://www.linuxfromscratch.org/patches/blfs/svn/icedtea-${icedtea_ver}-add_cacerts-1.patch \
-  http://www.linuxfromscratch.org/patches/blfs/svn/icedtea-${icedtea_ver}-fixed_paths-1.patch \
-  ftp://ftp.mozilla.org/pub/mozilla.org/js/rhino${rhino_ver}.zip \
-  http://rybalkin.org/kiin-files/openjdk7_nonreparenting-wm.diff"
-srctar=icedtea-${icedtea_ver}.tar.gz
-srcdir=${location}/icedtea-${icedtea_ver}
+
+HOTSPOT_CHANGESET=b9bbe418db87
+OPENJDK_CHANGESET=8714dddd443a
+CORBA_CHANGESET=04ad0a30f564
+JAXP_CHANGESET=e17ab897041e
+JAXWS_CHANGESET=7169780eff51
+JDK_CHANGESET=bc455fc9948f
+LANGTOOLS_CHANGESET=5f4ad2269018
+urls="ftp://ftp.mozilla.org/pub/mozilla.org/js/rhino${rhino_ver}.zip \
+  http://icedtea.classpath.org/hg/icedtea7-forest/hotspot/archive/${HOTSPOT_CHANGESET}.tar.gz \
+  http://icedtea.classpath.org/hg/icedtea7-forest/archive/${OPENJDK_CHANGESET}.tar.gz \
+  http://icedtea.classpath.org/hg/icedtea7-forest/corba/archive/${CORBA_CHANGESET}.tar.gz \
+  http://icedtea.classpath.org/hg/icedtea7-forest/jaxp/archive/${JAXP_CHANGESET}.tar.gz \
+  http://icedtea.classpath.org/hg/icedtea7-forest/jaxws/archive/${JAXWS_CHANGESET}.tar.gz \
+  http://icedtea.classpath.org/hg/icedtea7-forest/jdk/archive/${JDK_CHANGESET}.tar.gz \
+  http://icedtea.classpath.org/hg/icedtea7-forest/langtools/archive/${LANGTOOLS_CHANGESET}.tar.gz \
+  http://www.linuxfromscratch.org/patches/blfs/svn/icedtea-2.3.3-fixed_paths-1.patch"
+srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  cp -v ../corba.tar.gz .
-  cp -v ../hotspot.tar.gz .
-  cp -v ../jaxp.tar.gz .
-  cp -v ../jaxws.tar.gz .
-  cp -v ../jdk.tar.gz .
-  cp -v ../langtools.tar.gz .
-  cp -v ../openjdk.tar.gz .
-  patch -Np1 -i ../icedtea-${icedtea_ver}-add_cacerts-1.patch
-  patch -Np1 -i ../icedtea-${icedtea_ver}-fixed_paths-1.patch
-  sed -i -e 's/DISTRIBUTION_PATCHES\ =\ patches\/lfs-fixed-paths\.patch/DISTRIBUTION_PATCHES\ =\ patches\/lfs-fixed-paths\.patch\ patches\/openjdk7_nonreparenting-wm\.diff/g' Makefile.am
-  cp ../openjdk7_nonreparenting-wm.diff patches
+  cp ../${HOTSPOT_CHANGESET}.tar.gz .
+  cp ../${OPENJDK_CHANGESET}.tar.gz .
+  cp ../${CORBA_CHANGESET}.tar.gz .
+  cp ../${JAXP_CHANGESET}.tar.gz .
+  cp ../${JAXWS_CHANGESET}.tar.gz .
+  cp ../${JDK_CHANGESET}.tar.gz .
+  cp ../${LANGTOOLS_CHANGESET}.tar.gz .
+  patch -Np1 -i ../icedtea-2.3.3-fixed_paths-1.patch
+
   unset JAVA_HOME
   unset _JAVA_OPTIONS
   ./autogen.sh
   ./configure --with-jdk-home=/opt/jdk \
-              --enable-nss \
-              --disable-system-gif \
-              --disable-system-lcms
+    --enable-nss \
+    --disable-system-gif \
+    --disable-system-lcms \
+    --with-hotspot-src-zip=${srcdir}/${HOTSPOT_CHANGESET}.tar.gz \
+    --with-openjdk-src-zip=${srcdir}/${OPENJDK_CHANGESET}.tar.gz \
+    --with-corba-src-zip=${srcdir}/${CORBA_CHANGESET}.tar.gz \
+    --with-jaxp-src-zip=${srcdir}/${JAXP_CHANGESET}.tar.gz \
+    --with-jaxws-src-zip=${srcdir}/${JAXWS_CHANGESET}.tar.gz \
+    --with-jdk-src-zip=${srcdir}/${JDK_CHANGESET}.tar.gz \
+    --with-langtools-src-zip=${srcdir}/${LANGTOOLS_CHANGESET}.tar.gz
   make
 }
 
 kiin_install() {
   chmod 0644 openjdk.build/j2sdk-image/lib/sa-jdi.jar
   mkdir -p ${pkgdir}/opt
-  cp -R openjdk.build/j2sdk-image ${pkgdir}/opt/OpenJDK-${pkgver}
-  chown -R root:root ${pkgdir}/opt/OpenJDK-${pkgver}
+  cp -R openjdk.build/j2sdk-image ${pkgdir}/opt/${pkgname}-${pkgver}
+  chown -R root:root ${pkgdir}/opt/${pkgname}-${pkgver}
   unzip ../rhino${rhino_ver}.zip
   install -v -d -m755 ${pkgdir}/usr/share/java
   install -v -m755 rhino${rhino_ver}/*.jar ${pkgdir}/usr/share/java
-  ln -sv OpenJDK-${pkgver} ${pkgdir}/opt/jdk
+  ln -sv ${pkgname}-${pkgver} ${pkgdir}/opt/jdk
 }
