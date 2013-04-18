820ee11a80b4ad3e75fd9d753761dbbaa9641cf9
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-04-18 10:53:17 +0400

    docbook-xsl: update to 1.78.1

--- essentials/docbook-xsl/package.sh
+++ essentials/docbook-xsl/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=docbook-xsl
-pkgver=1.78.0
+pkgver=1.78.1
 urls="http://downloads.sourceforge.net/docbook/docbook-xsl-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
