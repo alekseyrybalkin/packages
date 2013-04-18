d0c9b7735728e02d74839a6f8a94e8b9842a6836
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-04-18 10:56:13 +0400

    freeglut: update to 2.8.1

--- xorg/freeglut/package.sh
+++ xorg/freeglut/package.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
 pkgname=freeglut
-pkgver=2.8.0
-urls="http://downloads.sourceforge.net/freeglut/freeglut-${pkgver}.tar.gz \
-  http://www.linuxfromscratch.org/patches/blfs/svn/freeglut-${pkgver}-remove_smooth_opengl3_demo-1.patch"
+pkgver=2.8.1
+urls="http://downloads.sourceforge.net/freeglut/freeglut-${pkgver}.tar.gz"
 srctar=${pkgname}-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  patch -Np1 -i ../freeglut-${pkgver}-remove_smooth_opengl3_demo-1.patch
   ./configure --prefix=/usr --disable-static --libdir=$LIBDIR
   make
 }
