2de75607115c041a2634b48985f82be8d6a667c5
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-04-19 16:34:02 +0400

    perl-ack: update to 2.02

--- perl-modules/perl-ack/package.sh
+++ perl-modules/perl-ack/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=perl-ack
-pkgver=1.96
+pkgver=2.02
 urls="http://search.cpan.org/CPAN/authors/id/P/PE/PETDANCE/ack-${pkgver}.tar.gz"
 srctar=ack-${pkgver}.tar.gz
 srcdir=${location}/ack-${pkgver}
@@ -15,6 +15,4 @@ kiin_install() {
   make DESTDIR=${pkgdir} install
   find ${pkgdir} -name perllocal.pod -delete
   find ${pkgdir} -name .packlist -delete
-  #sed -i -e 's/\/sources\/perl-modules\/perl-ack\/kiin-dest//g' \
-  #  ${pkgdir}/usr/lib/perl5/vendor_perl/5.16.1/x86_64-linux/auto/ack/.packlist
 }
