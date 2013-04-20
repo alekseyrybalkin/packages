c51cb0d5ab0d0afe812a2cf2e4450d8046a63584
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-04-20 22:23:35 +0400

    tidyhtml: update to revision 1.46

--- essentials/tidyhtml/package.sh
+++ essentials/tidyhtml/package.sh
@@ -1,22 +1,18 @@
 #!/bin/sh
 
 pkgname=tidyhtml
-pkgver=20101110
-urls="http://anduin.linuxfromscratch.org/sources/BLFS/svn/t/tidy-cvs_${pkgver}.tar.bz2"
-srctar=tidy-cvs_${pkgver}.tar.bz2
-srcdir=${location}/tidy-cvs_${pkgver}
+pkgver=1.46
+urls="http://tidy.cvs.sourceforge.net/viewvc/tidy/tidy/?view=tar"
+wget_set_filename=1
+srctar=tidy-${pkgver}.tar
+srcdir=${location}/tidy
 
 kiin_make() {
+  source build/gnuauto/setup.sh
   ./configure --prefix=/usr
   make
 }
 
 kiin_install() {
   make DESTDIR=${pkgdir} install
-  mkdir -p ${pkgdir}/usr/share/man/man1
-  install -v -m644 -D htmldoc/tidy.1 \
-    ${pkgdir}/usr/share/man/man1/tidy.1
-  install -v -m755 -d ${pkgdir}/usr/share/doc/tidy-cvs_${pkgver}
-  install -v -m644    htmldoc/*.{html,gif,css} \
-    ${pkgdir}/usr/share/doc/tidy-cvs_${pkgver}
 }
