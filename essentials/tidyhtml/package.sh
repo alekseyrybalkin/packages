41acd1a7d3c0f9a917fd9d1cd00e1528be171ac0
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-04-21 00:35:46 +0400

    tidyhtml: fix tarball name

--- essentials/tidyhtml/package.sh
+++ essentials/tidyhtml/package.sh
@@ -4,7 +4,7 @@ pkgname=tidyhtml
 pkgver=1.46
 urls="http://tidy.cvs.sourceforge.net/viewvc/tidy/tidy/?view=tar"
 wget_set_filename=1
-srctar=tidy-${pkgver}.tar
+srctar=tidy-${pkgver}.tar.gz
 srcdir=${location}/tidy
 
 kiin_make() {
