9297977533e711065ac69e6ae74ff7796050dc8e
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-04-23 19:49:27 +0400

    add intellij idea community edition

---
+++ dev/idea/package.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+pkgname=idea
+pkgver=12.1.1
+_pkgver=129.239
+urls="http://download.jetbrains.com/idea/ideaIC-${pkgver}.tar.gz"
+srctar=ideaIC-${pkgver}.tar.gz
+srcdir=${location}/idea-IC-${_pkgver}
+
+kiin_make() {
+  echo "nothing to make..."
+}
+
+kiin_install() {
+  mkdir -p ${pkgdir}/usr/share/idea
+  cp -a ./* ${pkgdir}/usr/share/idea
+  chown -R root:root ${pkgdir}/usr/share/idea
+  sed -i '/.*read IGNORE.*/ d' ${pkgdir}/usr/share/idea/bin/idea.sh
+  install -D -m755 ../idea.sh ${pkgdir}/usr/bin/idea
+}
