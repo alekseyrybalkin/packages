01038ac8acd50242b72a86e36150d61bc4beeb4d
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-05-11 00:51:09 +0400

    freetype2: update to 2.4.12 and use adobe cff engine

--- xorg/freetype2/package.sh
+++ xorg/freetype2/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=freetype2
-pkgver=2.4.11
+pkgver=2.4.12
 urls="http://downloads.sourceforge.net/freetype/freetype-${pkgver}.tar.bz2"
 srctar=freetype-${pkgver}.tar.bz2
 srcdir=${location}/freetype-${pkgver}
@@ -9,6 +9,8 @@ srcdir=${location}/freetype-${pkgver}
 kiin_make() {
   sed -i -r 's:.*(#.*SUBPIXEL_RENDERING.*) .*:\1:' \
     include/freetype/config/ftoption.h
+  sed -i -e '1059d' src/cff/cffobjs.c
+  sed -i -e '1058adriver->hinting_engine = FT_CFF_HINTING_ADOBE;' src/cff/cffobjs.c
   ./configure --prefix=/usr --disable-static --libdir=$LIBDIR
   make
 }
