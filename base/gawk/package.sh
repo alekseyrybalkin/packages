3b16f88000eb1ca2fbaeda15fae2af2cd78e0683
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-05-12 00:54:23 +0400

    gawk: update to 4.1.0

--- base/gawk/package.sh
+++ base/gawk/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=gawk
-pkgver=4.0.2
+pkgver=4.1.0
 urls="http://ftp.gnu.org/gnu/gawk/gawk-${pkgver}.tar.xz"
 srctar=${pkgname}-${pkgver}.tar.xz
 srcdir=${location}/${pkgname}-${pkgver}
@@ -13,6 +13,6 @@ kiin_make() {
 
 kiin_install() {
   make DESTDIR=${pkgdir} install
-  mkdir -p -v ${pkgdir}/usr/share/doc/gawk-${pkgver}
-  cp    -v doc/{awkforai.txt,*.{eps,pdf,jpg}} ${pkgdir}/usr/share/doc/gawk-${pkgver}
+  mkdir -p -v ${pkgdir}/usr/share/doc/gawk
+  cp    -v doc/{awkforai.txt,*.{eps,pdf,jpg}} ${pkgdir}/usr/share/doc/gawk
 }
