ff6a8913e7a027b1087ea3173888fd608be2e461
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-05-12 12:28:08 +0400

    perl-error: update to 0.17020

--- perl-modules/perl-error/package.sh
+++ perl-modules/perl-error/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=perl-error
-pkgver=0.17019
+pkgver=0.17020
 urls="http://search.cpan.org/CPAN/authors/id/S/SH/SHLOMIF/Error-${pkgver}.tar.gz"
 srctar=Error-${pkgver}.tar.gz
 srcdir=${location}/Error-${pkgver}
