88f86aa764dac0f45478b6705188b900e8f5a9c8
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-05-13 10:34:46 +0400

    idea: update to 12.1.3

--- dev/idea/package.sh
+++ dev/idea/package.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 pkgname=idea
-pkgver=12.1.2
-_pkgver=129.354
+pkgver=12.1.3
+_pkgver=129.451
 urls="http://download.jetbrains.com/idea/ideaIC-${pkgver}.tar.gz"
 srctar=ideaIC-${pkgver}.tar.gz
 srcdir=${location}/idea-IC-${_pkgver}
