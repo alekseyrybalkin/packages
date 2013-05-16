6563ed21f40afd50b178f3fc21a2bb3e7a17fe12
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-05-17 01:05:33 +0400

    groovy: build from latest git sources

--- dev/groovy/package.sh
+++ dev/groovy/package.sh
@@ -1,22 +1,32 @@
 #!/bin/sh
 
 pkgname=groovy
-pkgver=2.1.3
-urls="http://dist.groovy.codehaus.org/distributions/${pkgname}-binary-${pkgver}.zip"
-srczip=${pkgname}-binary-${pkgver}.zip
+vcs="git"
+gittag=e0fcf892053773267e243c87de254d722cae915f
+pkgver=2.1.3+
+_pkgver=2.2.0-SNAPSHOT
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  cd embeddable
-  ln -s "${pkgname}-all-${pkgver}.jar" "${pkgname}-all.jar"
-  for f in ../bin/*; do
-    sed 's:bin/sh:bin/sh\nGROOVY_HOME=/usr/share/groovy:' -i $f
-  done
-  cd ..
+  ./gradlew clean dist
 }
 
 kiin_install() {
+  unzip target/distributions/groovy-binary-${_pkgver}.zip
+  cd groovy-${_pkgver}
+  cd embeddable
+  mv ${pkgname}-all-${_pkgver}-indy.jar ${pkgname}-all-${_pkgver}.jar
+  ln -s ${pkgname}-all-${_pkgver}.jar ${pkgname}-all.jar
+  for f in ../bin/*; do
+    sed 's:bin/sh:bin/sh\nGROOVY_HOME=/usr/share/groovy:' -i $f
+  done
   mkdir -p ${pkgdir}/usr/share/groovy ${pkgdir}/usr/bin
+  cd ../indy
+  for i in *.jar; do
+    j=`echo $i | sed 's/-indy//g'`
+    cp $i ../lib/$j
+  done
+  cd ..
   cp -r lib ${pkgdir}/usr/share/groovy
   cp -r conf ${pkgdir}/usr/share/groovy
   cp -r embeddable ${pkgdir}/usr/share/groovy
