3fbaeba06a9b3e44279d42ecb2f6f0b89d0c4984
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-05-16 10:40:36 +0400

    mercurial: update to 2.6.1

--- dev/mercurial/package.sh
+++ dev/mercurial/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=mercurial
-pkgver=2.6
+pkgver=2.6.1
 urls="http://mercurial.selenic.com/release/${pkgname}-${pkgver}.tar.gz"
 srctar=${pkgname}-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
