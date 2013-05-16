42ec8aa9197aed1d09186127c2820e642cf3d96b
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-05-16 10:40:12 +0400

    automake: update to 1.13.2

--- base/automake/package.sh
+++ base/automake/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=automake
-pkgver=1.13.1
+pkgver=1.13.2
 urls="http://ftp.gnu.org/gnu/${pkgname}/${pkgname}-${pkgver}.tar.xz"
 srctar=${pkgname}-${pkgver}.tar.xz
 srcdir=${location}/${pkgname}-${pkgver}
