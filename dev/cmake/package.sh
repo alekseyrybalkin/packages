221acfd7da746db9df80df388127eb57832674b1
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-05-18 09:18:42 +0400

    cmake: update to 2.8.11

--- dev/cmake/package.sh
+++ dev/cmake/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=cmake
-pkgver=2.8.10.2
+pkgver=2.8.11
 urls="http://www.cmake.org/files/v2.8/cmake-${pkgver}.tar.gz"
 srctar=${pkgname}-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
