77a5c1ded2a01bd60dd2402b6daba5a1c539bcbc
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-05-19 10:58:39 +0400

    poppler: update to 0.22.4

--- gui/poppler/package.sh
+++ gui/poppler/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=poppler
-pkgver=0.22.3
+pkgver=0.22.4
 urls="http://poppler.freedesktop.org/poppler-${pkgver}.tar.gz"
 srctar=poppler-${pkgver}.tar.gz
 srcdir=${location}/poppler-${pkgver}
