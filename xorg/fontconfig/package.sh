8702db3e98a23c17acffe706d6ad8e67e6e6f7e6
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-05-21 00:04:35 +0400

    fontconfig: update to 2.10.93

--- xorg/fontconfig/package.sh
+++ xorg/fontconfig/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=fontconfig
-pkgver=2.10.92
+pkgver=2.10.93
 urls="http://${pkgname}.org/release/${pkgname}-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
