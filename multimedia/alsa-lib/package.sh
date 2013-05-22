2bb4e0904f7bb5cfe00b7df5973d666d19167e89
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-05-22 15:06:14 +0400

    alsa-{lib,utils}: update to 1.0.27.1

--- multimedia/alsa-lib/package.sh
+++ multimedia/alsa-lib/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=alsa-lib
-pkgver=1.0.27
+pkgver=1.0.27.1
 urls="ftp://ftp.alsa-project.org/pub/lib/${pkgname}-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
