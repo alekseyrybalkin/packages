2bc9c761d43dd684176f6729d1264e748ccfa474
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-05-23 22:59:28 +0400

    harfbuzz: update to 0.9.17

--- gui/harfbuzz/package.sh
+++ gui/harfbuzz/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=harfbuzz
-pkgver=0.9.16
+pkgver=0.9.17
 urls="http://www.freedesktop.org/software/${pkgname}/release/${pkgname}-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
