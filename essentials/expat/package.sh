3558daddd372d248d41efc44d7744e7787ca7fb1
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-05-30 20:44:50 +0400

    expat: disable static libs, fix doc folder path

--- essentials/expat/package.sh
+++ essentials/expat/package.sh
@@ -7,12 +7,12 @@ srctar=${pkgname}-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  ./configure --prefix=/usr --libdir=$LIBDIR
+  ./configure --prefix=/usr --disable-static --libdir=$LIBDIR
   make
 }
 
 kiin_install() {
   make DESTDIR=${pkgdir} install
-  install -v -m755 -d ${pkgdir}/usr/share/doc/expat-${pkgver}
-  install -v -m644 doc/*.{html,png,css} ${pkgdir}/usr/share/doc/expat-${pkgver}
+  install -v -m755 -d ${pkgdir}/usr/share/doc/expat
+  install -v -m644 doc/*.{html,png,css} ${pkgdir}/usr/share/doc/expat
 }
