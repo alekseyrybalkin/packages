7b16c251ae7ce2e9c70c008d8d388b9c7f8e8cf5
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-05-31 14:33:29 +0400

    sdl: fix build with newer libx11 and update to latest hg version

--- gaming/sdl/package.sh
+++ gaming/sdl/package.sh
@@ -2,11 +2,13 @@
 
 pkgname=sdl
 vcs="mercurial"
-hgtag="fba40d9f4a73"
-pkgver=1.2.15
+hgtag="f7fd5c3951b9"
+pkgver=1.2.15+
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
+  sed -i -e '168d' src/video/x11/SDL_x11sym.h
+  sed -i -e '167aSDL_X11_SYM(int,_XData32,(Display *dpy,register _Xconst long *data,unsigned len),(dpy,data,len),return)' src/video/x11/SDL_x11sym.h
   ./autogen.sh
   mkdir sdl-build
   cd sdl-build
