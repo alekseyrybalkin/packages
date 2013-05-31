99f89ba2e63632c4413c982870ae5b8078724117
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-05-31 14:11:18 +0400

    libxxf86dga: update to 1.1.4

--- xorg/lib/libxxf86dga/package.sh
+++ xorg/lib/libxxf86dga/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=libxxf86dga
-pkgver=1.1.3
+pkgver=1.1.4
 urls="http://xorg.freedesktop.org/releases/individual/lib/libXxf86dga-${pkgver}.tar.bz2"
 srctar=libXxf86dga-${pkgver}.tar.bz2
 srcdir=${location}/libXxf86dga-${pkgver}
