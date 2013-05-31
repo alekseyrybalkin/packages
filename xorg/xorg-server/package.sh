9cd3ad3c831cdb31508c533ab60ba5cba1287b03
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-05-31 14:12:07 +0400

    xorg-server: update to 1.14.1.901

--- xorg/xorg-server/package.sh
+++ xorg/xorg-server/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=xorg-server
-pkgver=1.14.99.1
+pkgver=1.14.1.901
 urls="http://xorg.freedesktop.org/releases/individual/xserver/${pkgname}-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
