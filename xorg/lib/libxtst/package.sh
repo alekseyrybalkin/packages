1d16f6adb8c460eead1721b175e6e62b55c13856
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-05-31 14:10:57 +0400

    libxtst: update to 1.2.2

--- xorg/lib/libxtst/package.sh
+++ xorg/lib/libxtst/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=libxtst
-pkgver=1.2.1
+pkgver=1.2.2
 urls="http://xorg.freedesktop.org/releases/individual/lib/libXtst-${pkgver}.tar.bz2"
 srctar=libXtst-${pkgver}.tar.bz2
 srcdir=${location}/libXtst-${pkgver}
