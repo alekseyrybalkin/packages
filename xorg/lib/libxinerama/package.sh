7bb78ebc0ce387100b016e70b67e3fb7ad4e301a
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-05-31 14:10:21 +0400

    libxinerama: update to 1.1.3

--- xorg/lib/libxinerama/package.sh
+++ xorg/lib/libxinerama/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=libxinerama
-pkgver=1.1.2
+pkgver=1.1.3
 urls="http://xorg.freedesktop.org/releases/individual/lib/libXinerama-${pkgver}.tar.bz2"
 srctar=libXinerama-${pkgver}.tar.bz2
 srcdir=${location}/libXinerama-${pkgver}
