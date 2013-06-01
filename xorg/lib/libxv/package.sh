428e608557aeaef792ba6ccbc303ec47c2e83ca5
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-06-01 11:41:50 +0400

    libxv: update to 1.0.8

--- xorg/lib/libxv/package.sh
+++ xorg/lib/libxv/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=libxv
-pkgver=1.0.7
+pkgver=1.0.8
 urls="http://xorg.freedesktop.org/releases/individual/lib/libXv-${pkgver}.tar.bz2"
 srctar=libXv-${pkgver}.tar.bz2
 srcdir=${location}/libXv-${pkgver}
