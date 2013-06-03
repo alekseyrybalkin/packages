59767c0682c0b4847374f803e908546e175ffdae
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-06-04 02:45:02 +0400

    add ttf-win7-fonts

---
+++ gui/ttf-win7-fonts/package.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+pkgname=ttf-win7-fonts
+pkgver=7.1
+urls="got-from-windows7-installation/${pkgname}-${pkgver}.tar.xz"
+srctar=${pkgname}-${pkgver}.tar.xz
+srcdir=${location}/${pkgname}-${pkgver}
+
+kiin_make() {
+  echo "nothing to make..."
+}
+
+kiin_install() {
+  install -d -m 755 ${pkgdir}/usr/share/fonts/TTF
+  install -m 644 *.ttf ${pkgdir}/usr/share/fonts/TTF
+}
+
+kiin_after_install() {
+  fc-cache -f > /dev/null
+  mkfontscale /usr/share/fonts/TTF
+  mkfontdir /usr/share/fonts/TTF
+}
+
+kiin_after_upgrade() {
+  kiin_after_install
+}
