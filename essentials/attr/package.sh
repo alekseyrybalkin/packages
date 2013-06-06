ac3f3856c75494858b8bea3b9dabc0a706ad7bc3
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-06-07 00:59:01 +0400

    attr: update to 2.4.47

--- essentials/attr/package.sh
+++ essentials/attr/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=attr
-pkgver=2.4.46
+pkgver=2.4.47
 urls="http://download.savannah.gnu.org/releases/attr/attr-${pkgver}.src.tar.gz"
 srctar=${pkgname}-${pkgver}.src.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
@@ -10,16 +10,15 @@ kiin_make() {
   sed -i -e 's|/@pkg_name@|&-@pkg_version@|' include/builddefs.in
   INSTALL_USER=root  \
   INSTALL_GROUP=root \
-  ./configure --prefix=/usr --libdir=/lib --libexecdir=/usr/lib \
-    --disable-static
+  ./configure --prefix=/usr --disable-static
   make
 }
 
 kiin_install() {
   make DIST_ROOT=${pkgdir} install install-dev install-lib
-  chmod -v 0755 ${pkgdir}/lib/libattr.so.1.1.0
-  rm -v ${pkgdir}/lib/libattr.{a,la,so}
-  sed -i 's@/lib@/usr/lib@' ${pkgdir}/usr/lib/libattr.la
+  chmod -v 755 ${pkgdir}/usr/lib/libattr.so.1.1.0
+  mkdir -pv ${pkgdir}/lib
+  mv -v ${pkgdir}/usr/lib/libattr.so.* ${pkgdir}/lib
   ln -sfv ../../lib/libattr.so.1 ${pkgdir}/usr/lib/libattr.so
   rm -rf ${pkgdir}/usr/share/man/man2
 }
