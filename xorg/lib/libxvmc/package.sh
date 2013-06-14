edb9d0b19ea8454f7f554a76630fa0c5baae0f35
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-06-14 22:36:13 +0400

    libxvmc: update to 1.0.8

--- xorg/lib/libxvmc/package.sh
+++ xorg/lib/libxvmc/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=libxvmc
-pkgver=1.0.7
+pkgver=1.0.8
 urls="http://xorg.freedesktop.org/releases/individual/lib/libXvMC-${pkgver}.tar.bz2"
 srctar=libXvMC-${pkgver}.tar.bz2
 srcdir=${location}/libXvMC-${pkgver}
