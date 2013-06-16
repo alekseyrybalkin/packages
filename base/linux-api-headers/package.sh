2105d6db95de0e0fcc50efde139a7e1265e6fdc5
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-06-16 15:10:51 +0400

    linux: update to 3.10.rc6

--- base/linux-api-headers/package.sh
+++ base/linux-api-headers/package.sh
@@ -3,8 +3,8 @@
 pkgname=linux-api-headers
 git_pkgname=linux
 vcs="git"
-gittag=v3.10-rc5
-pkgver=3.10.rc5
+gittag=v3.10-rc6
+pkgver=3.10.rc6
 srcdir=${location}/linux-${pkgver}
 
 kiin_make() {
--- base/linux/package.sh
+++ base/linux/package.sh
@@ -3,8 +3,8 @@
 pkgname=linux
 #git_pkgname=linux-stable
 vcs="git"
-gittag=v3.10-rc5
-pkgver=3.10.rc5
+gittag=v3.10-rc6
+pkgver=3.10.rc6
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
