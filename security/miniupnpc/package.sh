9e30c33f009dc12ab2da77606d60f8a6453c2570
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-06-21 01:18:34 +0400

    miniupnpc: update to 1.8

--- security/miniupnpc/package.sh
+++ security/miniupnpc/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=miniupnpc
-pkgver=1.6
+pkgver=1.8
 urls="http://miniupnp.free.fr/files/${pkgname}-${pkgver}.tar.gz"
 srctar=${pkgname}-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
