2646fbd168dd60bd7fc4fa67e5c865d42d344df1
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-06-21 01:19:19 +0400

    freetype2: update to 2.5.0.1

--- xorg/freetype2/package.sh
+++ xorg/freetype2/package.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
 pkgname=freetype2
-pkgver=2.4.12
+pkgver=2.5.0.1
 urls="http://downloads.sourceforge.net/freetype/freetype-${pkgver}.tar.bz2"
 srctar=freetype-${pkgver}.tar.bz2
 srcdir=${location}/freetype-${pkgver}
 
 kiin_make() {
-  sed -e "s@FT_CFF_HINTING_FREETYPE@FT_CFF_HINTING_ADOBE@" \
-    -i src/cff/cffobjs.c
   sed -e "/AUX.*.gxvalid/s@^# @@" -e "/AUX.*.otvalid/s@^# @@" \
     -i modules.cfg
   sed -e "/#.*.SUBPIXEL/s@/\* @@" -e "/#.*.SUBPIXEL/s@ \*/@@" \
