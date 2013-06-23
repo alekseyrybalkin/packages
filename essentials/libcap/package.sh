7deeedb490a0861ca5072b300425fbd75f183813
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-06-23 12:14:59 +0400

    libcap: add server updates checking

--- essentials/libcap/package.sh
+++ essentials/libcap/package.sh
@@ -2,9 +2,18 @@
 
 pkgname=libcap
 pkgver=2.22
-urls="http://ftp.de.debian.org/debian/pool/main/libc/libcap2/libcap2_${pkgver}.orig.tar.gz"
-srctar=${pkgname}2_${pkgver}.orig.tar.gz
-srcdir=${location}/${pkgname}-${pkgver}
+extension=gz
+folder="http://ftp.de.debian.org/debian/pool/main/libc/${pkgname}2/"
+check_server=1
+
+srctar=${pkgname}2_${pkgver}.orig.tar.${extension}
+
+. ${KIIN_HOME}/defaults.sh
+
+ver_grep="^${pkgname}2_.*\.orig\.tar.${extension}$"
+ver_seds() {
+  sed -r "s/^${pkgname}2_//g" | sed -r "s/\.orig\.tar.${extension}$//g"
+}
 
 kiin_make() {
   sed -i '/install.*STALIBNAME/ s/^/#/' libcap/Makefile
