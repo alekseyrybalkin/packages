ab180b675a695a11ba7c6467371ca431335dd7e4
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-06-25 22:08:31 +0400

    gimp: update to 2.8.6

--- multimedia/gimp/package.sh
+++ multimedia/gimp/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=gimp
-pkgver=2.8.4
+pkgver=2.8.6
 urls="http://artfiles.org/gimp.org/gimp/v2.8/gimp-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
