5438a1a12af827314888bde30d4c271e7b8ee1c0
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-07-02 10:41:32 +0400

    mesa: update to 9.1.4

--- xorg/mesa/package.sh
+++ xorg/mesa/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=mesa
-pkgver=9.1.3
+pkgver=9.1.4
 urls="ftp://ftp.freedesktop.org/pub/${pkgname}/${pkgver}/MesaLib-${pkgver}.tar.bz2 \
   http://www.linuxfromscratch.org/patches/blfs/svn/MesaLib-${pkgver}-add_xdemos-1.patch"
 srctar=MesaLib-${pkgver}.tar.bz2
