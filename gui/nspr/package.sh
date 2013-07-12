68fb7db2ae5ee712973e0a14752a5676ce2e1d5b
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-07-13 00:58:26 +0400

    nspr: update to 4.10

--- gui/nspr/package.sh
+++ gui/nspr/package.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 pkgname=nspr
-pkgver=4.9.6
+pkgver=4.10
 urls="http://ftp.mozilla.org/pub/mozilla.org/nspr/releases/v${pkgver}/src/nspr-${pkgver}.tar.gz"
 srctar=${pkgname}-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  cd mozilla/nsprpub
+  cd nspr
   sed -ri 's#^(RELEASE_BINS =).*#\1#' pr/src/misc/Makefile.in
   sed -i 's#$(LIBRARY) ##' config/rules.mk
   ./configure --prefix=/usr \
@@ -19,6 +19,6 @@ kiin_make() {
 }
 
 kiin_install() {
-  cd mozilla/nsprpub
+  cd nspr
   make DESTDIR=${pkgdir} install
 }
