6cd5008d595a82ce71470696f979d0ffb7b45736
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-07-18 19:34:11 +0400

    dconf: update to 0.16.1

--- gui/dconf/package.sh
+++ gui/dconf/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=dconf
-pkgver=0.16.0
+pkgver=0.16.1
 urls="http://ftp.gnome.org/pub/gnome/sources/dconf/0.16/dconf-${pkgver}.tar.xz"
 srctar=${pkgname}-${pkgver}.tar.xz
 srcdir=${location}/${pkgname}-${pkgver}
