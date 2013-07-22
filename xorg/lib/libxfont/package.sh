9fe321a510ab0ee3b6a06b862f5b0329d686313c
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-07-23 00:36:42 +0400

    libxfont: update to 1.4.6

--- xorg/lib/libxfont/package.sh
+++ xorg/lib/libxfont/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=libxfont
-pkgver=1.4.5
+pkgver=1.4.6
 urls="http://xorg.freedesktop.org/releases/individual/lib/libXfont-${pkgver}.tar.bz2"
 srctar=libXfont-${pkgver}.tar.bz2
 srcdir=${location}/libXfont-${pkgver}
