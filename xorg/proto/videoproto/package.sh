39f017385e5271411b541b6c310cd4c15dc17f94
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-07-24 19:34:33 +0400

    videoproto: update to 2.3.2

--- xorg/proto/videoproto/package.sh
+++ xorg/proto/videoproto/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=videoproto
-pkgver=2.3.1
+pkgver=2.3.2
 urls="http://xorg.freedesktop.org/releases/individual/proto/${pkgname}-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
