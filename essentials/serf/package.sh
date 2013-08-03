387965576e90d740d1e6e317cef8c383eae6ca93
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-08-03 15:09:09 +0400

    serf: update to 1.3.0

--- essentials/serf/package.sh
+++ essentials/serf/package.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
 pkgname=serf
-pkgver=1.2.1
+pkgver=1.3.0
 urls="https://serf.googlecode.com/files/serf-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  ./configure --prefix=/usr --disable-static
-  make
+  sed -i "/incdir/s:MAJOR':&\n\nenv\['SHLIBVERSION'\]='0.\${MAJOR}.0':" SConstruct
+  sed -i "/Append/s:RPATH=libdir,::" SConstruct
+  sed -i "/Default/s:lib_static,::" SConstruct
+  sed -i "/Alias/s:install_static,::" SConstruct
+  scons PREFIX=/usr
 }
 
 kiin_install() {
-  make DESTDIR=${pkgdir} install
+  mkdir -p ${pkgdir}/usr
+  scons PREFIX=${pkgdir}/usr install
+  ln -sfv libserf-1.so.0.1.0 ${pkgdir}/usr/lib/libserf-1.so.0
+  ln -sfv libserf-1.so.0.1.0 ${pkgdir}/usr/lib/libserf-1.so
 }
