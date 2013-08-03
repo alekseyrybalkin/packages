dab7ce2364af923b2b638acd6f1f49031e0b88fc
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-08-03 13:36:27 +0400

    unrar: update to 5.0.8

--- essentials/unrar/package.sh
+++ essentials/unrar/package.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
 pkgname=unrar
-pkgver=5.0.2
+pkgver=5.0.8
 urls="http://www.rarlab.com/rar/unrarsrc-${pkgver}.tar.gz"
 srctar=${pkgname}src-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}
 
 kiin_make() {
+  MAKEFLAGS=
   make -f makefile CXXFLAGS="-fPIC ${CXXFLAGS}" STRIP="true"
 }
 
 kiin_install() {
+  MAKEFLAGS=
   install -Dm755 unrar ${pkgdir}/usr/bin/unrar
 }
