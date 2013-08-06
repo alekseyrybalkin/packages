880a52428ed591e5d26dd6e3aaa603494cf708c5
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-08-06 16:39:09 +0400

    add libmemcached

---
+++ dev/libmemcached/package.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+pkgname=libmemcached
+pkgver=1.0.17
+urls="http://launchpad.net/${pkgname}/1.0/${pkgver}/+download/${pkgname}-${pkgver}.tar.gz \
+  http://rybalkin.org/kiin-files/libmemcached-gcc-4.8.patch"
+srctar=${pkgname}-${pkgver}.tar.gz
+srcdir=${location}/${pkgname}-${pkgver}
+
+kiin_make() {
+  # Fix build with GCC 4.8 (patch from Debian)
+  patch -Np1 -i ../libmemcached-gcc-4.8.patch
+  ./configure --prefix=/usr
+  make
+}
+
+kiin_install() {
+  make DESTDIR=${pkgdir} install
+}
