d6b7e37dcab58685407e0a9784a6c4a5885871e3
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-08-10 13:49:45 +0400

    Revert "db: update to 6.0.20"
    This reverts commit 9228c07d5faf1b73bb1993fbac989e68bf5d5fe7.

--- security/db/package.sh
+++ security/db/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=db
-pkgver=6.0.20
+pkgver=5.3.21
 urls="http://download.oracle.com/berkeley-db/db-${pkgver}.tar.gz"
 srctar=${pkgname}-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
@@ -19,9 +19,9 @@ kiin_make() {
 kiin_install() {
   cd build_unix
   make DESTDIR=${pkgdir} docdir=/usr/share/doc/db install
-  rm -rf ${pkgdir}/usr/share
   chown -v -R root:root \
     ${pkgdir}/usr/bin/db_* \
     ${pkgdir}/usr/include/db{,_185,_cxx}.h \
-    ${pkgdir}/usr/lib/libdb*.{so,la}
+    ${pkgdir}/usr/lib/libdb*.{so,la} \
+    ${pkgdir}/usr/share/doc/db
 }
