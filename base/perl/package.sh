83b52553954801be51183439c40e906e0d27b66c
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-08-13 18:17:32 +0400

    perl: update to 5.18.1

--- base/perl/package.sh
+++ base/perl/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=perl
-pkgver=5.18.0
+pkgver=5.18.1
 urls="http://www.cpan.org/src/5.0/${pkgname}-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
