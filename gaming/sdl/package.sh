0ed614232dd28e173d23853aec93b31d3daebc46
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-08-14 12:50:48 +0400

    sdl: really update to the latest mercurial version

--- gaming/sdl/package.sh
+++ gaming/sdl/package.sh
@@ -2,13 +2,11 @@
 
 pkgname=sdl
 vcs="mercurial"
-hgtag="3665bc284271"
+hgtag="952791dbe6ba"
 pkgver=1.2.15+
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  sed -i -e '168d' src/video/x11/SDL_x11sym.h
-  sed -i -e '167aSDL_X11_SYM(int,_XData32,(Display *dpy,register _Xconst long *data,unsigned len),(dpy,data,len),return)' src/video/x11/SDL_x11sym.h
   ./autogen.sh
   mkdir sdl-build
   cd sdl-build
