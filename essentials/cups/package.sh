cef411683cfa156cbf20768fd121c12572c242fc
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-08-19 19:43:35 +0400

    cups: repair build

--- essentials/cups/package.sh
+++ essentials/cups/package.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-pkgname=libcups
+pkgname=cups
 pkgver=1.6.3
 urls="http://www.cups.org/software/${pkgver}/cups-${pkgver}-source.tar.bz2 \
-  http://www.linuxfromscratch.org/patches/blfs/svn/cups-1.6.2-blfs-1.patch"
+  http://www.linuxfromscratch.org/patches/blfs/svn/cups-${pkgver}-blfs-1.patch"
 srctar=cups-${pkgver}-source.tar.bz2
 srcdir=${location}/cups-${pkgver}
 
 kiin_make() {
   sed -i 's#@CUPS_HTMLVIEW@#firefox#' desktop/cups.desktop.in
-  patch -Np1 -i ../cups-1.6.2-blfs-1.patch
+  patch -Np1 -i ../cups-${pkgver}-blfs-1.patch
   ./configure --with-rcdir=/tmp/cupsinit \
     --with-docdir=/usr/share/cups/doc \
     --with-system-groups=lpadmin \
