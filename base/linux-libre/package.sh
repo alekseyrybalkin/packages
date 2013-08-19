f5792b11b671bf3e54ba03b6cf2020637ecc38a0
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-08-19 22:39:49 +0400

    linux-libre: check server version

--- base/linux-libre/package.sh
+++ base/linux-libre/package.sh
@@ -2,9 +2,20 @@
 
 pkgname=linux-libre
 pkgver=3.10.7
-urls="http://linux-libre.fsfla.org/pub/linux-libre/releases/${pkgver}-gnu/linux-libre-${pkgver}-gnu.tar.xz"
-srctar=${pkgname}-${pkgver}-gnu.tar.xz
+extension=xz
+folder="http://linux-libre.fsfla.org/pub/linux-libre/releases/"
+check_server=1
+
+srctar=${pkgname}-${pkgver}-gnu.tar.${extension}
 srcdir=${location}/linux-${pkgver}
+
+. ${KIIN_HOME}/defaults.sh
+
+urls="${folder}${pkgver}-gnu/${srctar}"
+ver_grep="^[^-]*-gnu/$"
+ver_seds() {
+  sed -r "s/-gnu\/$//g"
+}
 
 kiin_make() {
   make mrproper
