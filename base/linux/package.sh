80bb2bfe53bbcd633aa681f4ac051eda91550c17
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-08-23 19:18:18 +0400

    linux: downgrade to stable version

--- base/linux/package.sh
+++ base/linux/package.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 pkgname=linux
-#git_pkgname=linux-stable
+git_pkgname=linux-stable
 vcs="git"
-gittag=v3.11-rc6
-pkgver=3.11.rc6
+gittag=v3.10.9
+pkgver=3.10.9
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
