c146047ba417f64bfe907d63e167328c1c1e4fa0
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-08-23 18:47:56 +0400

    kmod: update to 15

--- base/kmod/package.sh
+++ base/kmod/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=kmod
-pkgver=14
+pkgver=15
 urls="http://www.kernel.org/pub/linux/utils/kernel/${pkgname}/${pkgname}-${pkgver}.tar.xz"
 srctar=${pkgname}-${pkgver}.tar.xz
 srcdir=${location}/${pkgname}-${pkgver}
