ffd4d27a88a382838d6b6c6e0eff7679306af757
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-08-23 18:47:38 +0400

    kbd: update to 2.0.0

--- base/kbd/package.sh
+++ base/kbd/package.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
 
 pkgname=kbd
-pkgver=1.15.5
-urls="http://ftp.altlinux.org/pub/people/legion/kbd/kbd-${pkgver}.tar.gz \
-  http://www.linuxfromscratch.org/patches/lfs/development/kbd-${pkgver}-backspace-1.patch"
+pkgver=2.0.0
+urls="http://ftp.altlinux.org/pub/people/legion/kbd/kbd-${pkgver}.tar.gz"
 srctar=${pkgname}-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  patch -Np1 -i ../kbd-1.15.5-backspace-1.patch
-  sed -i -e '326 s/if/while/' src/loadkeys.analyze.l
-  sed -i 's/\(RESIZECONS_PROGS=\)yes/\1no/g' configure
-  sed -i 's/resizecons.8 //' man/man8/Makefile.in
   ./configure --prefix=/usr \
     --disable-vlock
   make
@@ -19,9 +14,4 @@ kiin_make() {
 
 kiin_install() {
   make DESTDIR=${pkgdir} install
-  mkdir -pv ${pkgdir}/bin
-  mv -v ${pkgdir}/usr/bin/{kbd_mode,loadkeys,openvt,setfont} ${pkgdir}/bin
-  mkdir -pv ${pkgdir}/usr/share/doc/kbd
-  cp -R -v doc/* \
-          ${pkgdir}/usr/share/doc/kbd
 }
