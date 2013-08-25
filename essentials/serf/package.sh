ab2e7cb954ac1891bd07ebbbbf99a51ff24706fa
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-08-25 23:05:08 +0400

    serf: remove unnassesary sed

--- essentials/serf/package.sh
+++ essentials/serf/package.sh
@@ -7,7 +7,6 @@ srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  sed -i "/incdir/s:MAJOR':&\n\nenv\['SHLIBVERSION'\]='0.\${MAJOR}.0':" SConstruct
   sed -i "/Append/s:RPATH=libdir,::" SConstruct
   sed -i "/Default/s:lib_static,::" SConstruct
   sed -i "/Alias/s:install_static,::" SConstruct
