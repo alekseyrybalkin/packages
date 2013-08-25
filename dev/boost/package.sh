0dd3613e1a490d795cafb2b78d193ea26e624ee9
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-08-25 23:03:22 +0400

    boost: fix build with glibc 2.18

--- dev/boost/package.sh
+++ dev/boost/package.sh
@@ -3,11 +3,13 @@
 pkgname=boost
 pkgver=1.54.0
 _pkgver=1_54_0
-urls="http://downloads.sourceforge.net/boost/boost_${_pkgver}.tar.bz2"
+urls="http://downloads.sourceforge.net/boost/boost_${_pkgver}.tar.bz2 \
+  http://www.linuxfromscratch.org/patches/blfs/svn/boost-${pkgver}-glibc-1.patch"
 srctar=${pkgname}_${_pkgver}.tar.bz2
 srcdir=${location}/${pkgname}_${_pkgver}
 
 kiin_make() {
+  patch -Np1 -i ../boost-${pkgver}-glibc-1.patch
   ./bootstrap.sh --prefix=/usr
   ./b2 stage threading=multi link=shared
 }
