4dd5ab9f6db97f1f354739b3b970842dddc68c84
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-08-30 23:30:47 +0400

    gstreamer: update to 1.0.10

--- webkit/gstreamer/package.sh
+++ webkit/gstreamer/package.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
 pkgname=gstreamer
-pkgver=1.0.9
+pkgver=1.0.10
 urls="http://gstreamer.freedesktop.org/src/gstreamer/gstreamer-${pkgver}.tar.xz"
 srctar=${pkgname}-${pkgver}.tar.xz
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  # fix build with bison 3.0
-  sed -i '/int priv_gst_parse_yylex/s/, yyscan_t yyscanner//' \
-    gst/parse/grammar.y
   ./configure --prefix=/usr \
     --libexecdir=/usr/lib \
     --with-package-name="GStreamer ${pkgver} kiin" \
