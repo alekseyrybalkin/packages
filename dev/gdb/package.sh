4ddfa2ead9537a1948b9b54d610f171668d7e7b6
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-08-31 23:53:35 +0400

    gdb: update to 7.6.1

--- dev/gdb/package.sh
+++ dev/gdb/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=gdb
-pkgver=7.6
+pkgver=7.6.1
 urls="http://ftp.gnu.org/gnu/gdb/gdb-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
