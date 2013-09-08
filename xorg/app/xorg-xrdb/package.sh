a5e13fce185c423e3791719583968f60fba39767
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-09-08 21:50:08 +0400

    xorg-xrdb: update to 1.1.0

--- xorg/app/xorg-xrdb/package.sh
+++ xorg/app/xorg-xrdb/package.sh
@@ -2,7 +2,7 @@
 
 _pkgname=xrdb
 pkgname=xorg-${_pkgname}
-pkgver=1.0.9
+pkgver=1.1.0
 urls="http://xorg.freedesktop.org/releases/individual/app/${_pkgname}-${pkgver}.tar.bz2"
 srctar=${_pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${_pkgname}-${pkgver}
