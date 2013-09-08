ae4592bd54e8fe90d30b82e4281f2e8a363ac8a8
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-09-08 21:49:46 +0400

    xorg-xmodmap: update to 1.0.8

--- xorg/app/xorg-xmodmap/package.sh
+++ xorg/app/xorg-xmodmap/package.sh
@@ -3,8 +3,8 @@
 _pkgname=xmodmap
 pkgname=xorg-${_pkgname}
 vcs="git"
-gittag=bf4620e2d78d2a4766948f136f2372a596ac275d
-pkgver=1.0.7+
+gittag=xmodmap-1.0.8
+pkgver=1.0.8
 srcdir=${location}/${_pkgname}-${pkgver}
 
 kiin_make() {
