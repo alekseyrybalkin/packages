bbc7125f6e146b70c53b5a5113813f124a6de486
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-09-15 00:41:58 +0400

    gnome-themes-standard: update to 3.8.4

--- gui/gnome-themes-standard/package.sh
+++ gui/gnome-themes-standard/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=gnome-themes-standard
-pkgver=3.8.3
+pkgver=3.8.4
 urls="http://download.gnome.org/sources/${pkgname}/3.8/${pkgname}-${pkgver}.tar.xz"
 srctar=${pkgname}-${pkgver}.tar.xz
 srcdir=${location}/${pkgname}-${pkgver}
