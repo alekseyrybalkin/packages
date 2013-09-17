33d08b7b42f4f2afff4c5e4a4bf8706d9381943d
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-09-17 17:46:44 +0400

    xorg-xinput: update to 1.6.1

--- xorg/app/xorg-xinput/package.sh
+++ xorg/app/xorg-xinput/package.sh
@@ -2,7 +2,7 @@
 
 _pkgname=xinput
 pkgname=xorg-${_pkgname}
-pkgver=1.6.0
+pkgver=1.6.1
 urls="http://xorg.freedesktop.org/releases/individual/app/${_pkgname}-${pkgver}.tar.bz2"
 srctar=${_pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${_pkgname}-${pkgver}
