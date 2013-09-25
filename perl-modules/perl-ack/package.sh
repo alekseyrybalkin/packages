a249733ca1e8c4b0a58422cbb00c6ce0a734c488
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-09-26 01:00:50 +0400

    perl-ack: update to 2.10

--- perl-modules/perl-ack/package.sh
+++ perl-modules/perl-ack/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=perl-ack
-pkgver=2.08
+pkgver=2.10
 urls="http://search.cpan.org/CPAN/authors/id/P/PE/PETDANCE/ack-${pkgver}.tar.gz"
 srctar=ack-${pkgver}.tar.gz
 srcdir=${location}/ack-${pkgver}
