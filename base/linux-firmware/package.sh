c976cf17423b3091142abb9886b9b69534047ea7
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-10-05 01:01:15 +0400

    levison is back

--- base/linux-firmware/package.sh
+++ base/linux-firmware/package.sh
@@ -11,14 +11,19 @@ kiin_make() {
 }
 
 kiin_install() {
+  mkdir -pv ${pkgdir}/lib/firmware
   if [ `hostname` == 'ritchie' ]; then
     mkdir -pv ${pkgdir}/lib/firmware/brcm
     cp -v ./brcm/bcm43xx-0.fw ${pkgdir}/lib/firmware/brcm
     cp -v ./brcm/bcm43xx_hdr-0.fw ${pkgdir}/lib/firmware/brcm
   fi
   if [ `hostname` == 'snowden' ]; then
-    mkdir -pv ${pkgdir}/lib/firmware
     cp -v ./iwlwifi-6000g2a-5.ucode ${pkgdir}/lib/firmware
     cp -v ./iwlwifi-6000g2a-6.ucode ${pkgdir}/lib/firmware
+  fi
+  if [ `hostname` == 'alien' ]; then
+    cp -v ./iwlwifi-5000-1.ucode ${pkgdir}/lib/firmware
+    cp -v ./iwlwifi-5000-2.ucode ${pkgdir}/lib/firmware
+    cp -v ./iwlwifi-5000-5.ucode ${pkgdir}/lib/firmware
   fi
 }
