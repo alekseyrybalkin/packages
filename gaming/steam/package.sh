0c043a281a1bad7176099d8b4feaaf0c3c8c0722
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-10-06 22:34:27 +0400

    re-introduce steam, for levison

---
+++ gaming/steam/package.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+pkgname=steam
+pkgver=1.0.0.42
+urls="http://repo.steampowered.com/${pkgname}/pool/${pkgname}/s/${pkgname}/${pkgname}_${pkgver}.tar.gz \
+  http://rybalkin.org/kiin-files/steam_alsa_sdl_audiodriver.patch"
+srctar=${pkgname}_${pkgver}.tar.gz
+srcdir=${location}/${pkgname}
+
+kiin_make() {
+  patch -Np1 -i ../steam_alsa_sdl_audiodriver.patch
+  echo "nothing to make..."
+}
+
+kiin_install() {
+  make DESTDIR=${pkgdir} install
+  ln -sf /bin/true ${pkgdir}/usr/bin/steamdeps
+}
+
+kiin_after_install() {
+  chmod 700 /usr/bin/steam
+  getent group steam >/dev/null || groupadd steam
+  getent passwd steam >/dev/null || \
+    useradd -m -g steam -G audio,video -s /bin/bash steam
+  chown steam:steam /usr/bin/steam
+}
+
+kiin_after_upgrade() {
+  kiin_after_install
+}
