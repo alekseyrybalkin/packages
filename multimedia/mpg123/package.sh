7d1b4565d37e4f1531589dde6bb345eab97b39f4
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-10-08 16:01:35 +0400

    mpg123: update to 1.16.0

--- multimedia/mpg123/package.sh
+++ multimedia/mpg123/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=mpg123
-pkgver=1.15.4
+pkgver=1.16.0
 urls="http://downloads.sourceforge.net/mpg123/mpg123-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
