6c0b380029037a343865837fdc7096c10012f190
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-10-12 00:47:16 +0400

    fontconfig: update to 2.11.0

--- xorg/fontconfig/package.sh
+++ xorg/fontconfig/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=fontconfig
-pkgver=2.10.95
+pkgver=2.11.0
 urls="http://${pkgname}.org/release/${pkgname}-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
