9708296262cb21b017db5d50425f7a2d36bf166f
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-10-18 14:11:51 +0400

    gcc: update to 4.8.2

--- base/gcc/package.sh
+++ base/gcc/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=gcc
-pkgver=4.8.1
+pkgver=4.8.2
 urls="http://ftp.gnu.org/gnu/${pkgname}/${pkgname}-${pkgver}/${pkgname}-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
