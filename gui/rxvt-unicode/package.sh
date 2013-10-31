f8fa04edefcc80cfaeb95018bc8b627710ba35ab
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-10-31 18:28:18 +0400

    rxvt-unicode: update to 9.19

--- gui/rxvt-unicode/package.sh
+++ gui/rxvt-unicode/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=rxvt-unicode
-pkgver=9.18
+pkgver=9.19
 urls="http://dist.schmorp.de/${pkgname}/Attic/${pkgname}-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
