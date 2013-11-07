fa5433e6fbf042e8e0f5dc863792c6e9dc5114fb
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-11-07 17:47:48 +0400

    linux: update to 3.12

--- base/linux-api-headers/package.sh
+++ base/linux-api-headers/package.sh
@@ -3,8 +3,8 @@
 pkgname=linux-api-headers
 git_pkgname=linux-stable
 vcs="git"
-gittag=v3.11.7
-pkgver=3.11.7
+gittag=v3.12
+pkgver=3.12
 srcdir=${location}/linux-${pkgver}
 
 kiin_make() {
--- base/linux-libre/package.sh
+++ base/linux-libre/package.sh
@@ -3,7 +3,7 @@
 KIIN_NO_CLEARING=1
 
 pkgname=linux-libre
-pkgver=3.11.7
+pkgver=3.12
 extension=xz
 folder="http://linux-libre.fsfla.org/pub/linux-libre/releases/"
 check_server=1
