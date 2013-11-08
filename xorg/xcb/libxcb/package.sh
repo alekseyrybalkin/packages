4c9792c3f35d9f0a8de8a0439eb5d2d143a776b5
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-11-08 15:59:41 +0400

    update xtrans, libxcb & xcb-proto

--- xorg/xcb/libxcb/package.sh
+++ xorg/xcb/libxcb/package.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
 pkgname=libxcb
-pkgver=1.9.1
-urls="http://xcb.freedesktop.org/dist/${pkgname}-${pkgver}.tar.bz2 \
-  http://www.linuxfromscratch.org/patches/blfs/svn/libxcb-${pkgver}-automake_bug-1.patch"
+pkgver=1.9.3
+urls="http://xcb.freedesktop.org/dist/${pkgname}-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  patch -Np1 -i ../libxcb-${pkgver}-automake_bug-1.patch
   sed -e "s/pthread-stubs//" -i configure.ac
   autoreconf -fi
   ./configure $XORG_CONFIG \
--- xorg/xcb/xcb-proto/package.sh
+++ xorg/xcb/xcb-proto/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=xcb-proto
-pkgver=1.8
+pkgver=1.9
 urls="http://xcb.freedesktop.org/dist/${pkgname}-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
