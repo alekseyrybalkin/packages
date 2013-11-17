90aa25890612ec1a64f50479495bb3d9fd287c37
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-11-18 03:28:09 +0400

    tar: update to 1.27.1

--- base/tar/package.sh
+++ base/tar/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=tar
-pkgver=1.27
+pkgver=1.27.1
 urls="http://ftp.gnu.org/gnu/tar/tar-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
