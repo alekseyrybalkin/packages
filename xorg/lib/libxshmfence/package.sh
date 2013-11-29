65afd03d658bd2a7dbd70020b6cef658c1ea1d68
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-11-30 03:23:28 +0400

    add dri3proto, presentproto and libxshmfence

---
+++ xorg/lib/libxshmfence/package.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+pkgname=libxshmfence
+pkgver=1.1
+urls="http://xorg.freedesktop.org/releases/individual/lib/${pkgname}-${pkgver}.tar.bz2"
+srctar=${pkgname}-${pkgver}.tar.bz2
+srcdir=${location}/${pkgname}-${pkgver}
+
+kiin_make() {
+  ./configure $XORG_CONFIG
+  make
+}
+
+kiin_install() {
+  make DESTDIR=${pkgdir} install
+}
---
+++ xorg/proto/dri3proto/package.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+pkgname=dri3proto
+pkgver=1.0
+urls="http://xorg.freedesktop.org/releases/individual/proto/${pkgname}-${pkgver}.tar.bz2"
+srctar=${pkgname}-${pkgver}.tar.bz2
+srcdir=${location}/${pkgname}-${pkgver}
+
+kiin_make() {
+  ./configure $XORG_CONFIG
+  make
+}
+
+kiin_install() {
+  make DESTDIR=${pkgdir} install
+}
