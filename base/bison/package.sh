fb1e740c163364ca873c32c59108095dc6399051
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-12-06 03:42:32 +0400

    bison: update to 3.0.2

--- base/bison/package.sh
+++ base/bison/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=bison
-pkgver=3.0.1
+pkgver=3.0.2
 urls="http://ftp.gnu.org/gnu/bison/bison-${pkgver}.tar.xz"
 srctar=${pkgname}-${pkgver}.tar.xz
 srcdir=${location}/${pkgname}-${pkgver}
