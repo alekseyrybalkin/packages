30cb446b1a372e5a07215f606d33552c342e81b7
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-12-09 18:40:04 +0400

    update freetype2, xf86-input-synaptics and perl-archive-zip

--- xorg/driver/xf86-input-synaptics/package.sh
+++ xorg/driver/xf86-input-synaptics/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=xf86-input-synaptics
-pkgver=1.7.1
+pkgver=1.7.2
 urls="http://xorg.freedesktop.org/releases/individual/driver/${pkgname}-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
