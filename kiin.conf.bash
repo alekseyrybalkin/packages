10d92b32f5283fab5fc29d4cd6f5e7727cebf763
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-12-10 02:33:49 +0400

    first steps in packaging kiin

---
+++ kiin.conf.bash
@@ -0,0 +1,9 @@
+UPSTREAM_KIIN_PKGDB=/var/kiin
+
+KIIN_HOME=/var/kiin
+KIIN_DB=$KIIN_HOME/kiin-db/kiin.sqlite3
+PACKAGE_MANAGER=rybalkin
+ARCH_PACKAGES_REPO=~/sources/packages
+ARCH_COMMUNITY_REPO=~/sources/community
+SOURCES_HOME=/var/src
+HEAVEN=aleksey@rybalkin.org
