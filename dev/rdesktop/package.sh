9204cca06afd9e0b0625a7b5996d8aba3984c668
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-12-14 20:18:37 +0400

    add rdesktop

---
+++ dev/rdesktop/package.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+pkgname=rdesktop
+pkgver=1.8.1
+urls="http://downloads.sourceforge.net/${pkgname}/${pkgname}-${pkgver}.tar.gz"
+srctar=${pkgname}-${pkgver}.tar.gz
+srcdir=${location}/${pkgname}-${pkgver}
+
+kiin_make() {
+  ./configure --prefix=/usr \
+    --disable-smartcard \
+    --with-ipv6 \
+    --disable-credssp
+  make
+}
+
+kiin_install() {
+  make DESTDIR=${pkgdir} install
+}
