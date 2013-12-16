7132957f1e35ec919d34e10d70de623a6e4007a4
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-12-16 16:46:19 +0400

    mplayer: update and add dvd support

--- multimedia/mplayer/package.sh
+++ multimedia/mplayer/package.sh
@@ -2,18 +2,20 @@
 
 pkgname=mplayer
 vcs="git"
-pkgver=36500
+pkgver=36546
 gittag=`pushd ${SOURCES_HOME}/mplayer >/dev/null && git log | head -n 1000 | grep "^    git-svn-id\|^commit" | grep -B 1 "trunk@${pkgver}" | head -n 1 | cut -d ' ' --fields=2 && popd >/dev/null`
 srcdir=${location}/MPlayer-${pkgver}
 
 kiin_make() {
   git clone $SOURCES_HOME/ffmpeg ffmpeg
+  cd ffmpeg
+  git checkout n2.1.1
+  cd ..
   ./configure --prefix=/usr \
     --confdir=/etc/mplayer \
     --enable-dynamic-plugins \
     --disable-libvpx-lavc \
     --disable-gui \
-    --disable-dvdread \
     --disable-dvdread-internal \
     --disable-xss \
     --disable-liblzo
