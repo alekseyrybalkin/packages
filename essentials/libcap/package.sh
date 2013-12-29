a763b8cc3f0555b627ce5b07c76764069479afa9
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-12-29 20:25:14 +0400

    libcap: update to 2.23

--- essentials/libcap/package.sh
+++ essentials/libcap/package.sh
@@ -1,19 +1,12 @@
 #!/bin/sh
 
 pkgname=libcap
-pkgver=2.22
-extension=gz
-folder="http://ftp.de.debian.org/debian/pool/main/libc/${pkgname}2/"
+pkgver=2.23
+extension=xz
+folder="https://www.kernel.org/pub/linux/libs/security/linux-privs/libcap2/"
 check_server=1
 
-srctar=${pkgname}2_${pkgver}.orig.tar.${extension}
-
 . ${KIIN_HOME}/defaults.sh
-
-ver_grep="^${pkgname}2_.*\.orig\.tar\.${extension}$"
-ver_seds() {
-  sed -r "s/^${pkgname}2_//g" | sed -r "s/\.orig\.tar\.${extension}$//g"
-}
 
 kiin_make() {
   sed -i '/install.*STALIBNAME/ s/^/#/' libcap/Makefile
