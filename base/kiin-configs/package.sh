12b79a1befb75e456d58dfdce672a693b20acbb1
Aleksey Rybalkin <aleksey@rybalkin.org>
2013-12-29 21:40:35 +0400

    kiin-configs: support snowden

--- base/kiin-configs/package.sh
+++ base/kiin-configs/package.sh
@@ -10,7 +10,16 @@ kiin_make() {
   rm -rf .git
   find . -type f | xargs sed -i -e "s/%KIIN_HOSTNAME%/`hostname`/g"
   if [ "`hostname`" == "snowden" ]; then
-    true
+    sed -i -e "s/#clientid/clientid/g" etc/dhcpcd.conf
+    echo "domain pyn.ru
+search pyn.ru
+nameserver 94.124.205.83
+nameserver 192.168.0.3" > etc/resolv.conf.snow
+    cat etc/resolv.conf >> etc/resolv.conf.snow
+    mv etc/resolv.conf{.snow,}
+  fi
+  if [ "`hostname`" == "ritchie" ]; then
+    find etc/rc.d -name "*postgresql*" | xargs rm
   fi
 }
 
