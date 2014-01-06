8ed6650b84d183e1909c753048fed14bbfbfbc4d
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-01-06 20:04:19 +0400

    libcap: update to 2.24

--- essentials/libcap/package.sh
+++ essentials/libcap/package.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
 
 pkgname=libcap
-pkgver=2.23
+pkgver=2.24
 extension=xz
 folder="https://www.kernel.org/pub/linux/libs/security/linux-privs/libcap2/"
 check_server=1
 
 . ${KIIN_REPO}/defaults.sh
 
-urls="${urls} http://rybalkin.org/kiin-files/libcap-${pkgver}-header.patch"
-
 kiin_make() {
-  patch -p1 -i ${KIIN_HOME}/tarballs/libcap-${pkgver}-header.patch
-  sed -i "s#uapi/##" libcap/Makefile
   sed -i '/install.*STALIBNAME/ s/^/#/' libcap/Makefile
   make
 }
