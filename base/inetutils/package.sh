3b9012a68a0c3e3b882db8250a8c58808dfdc9ba
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-01-14 02:12:53 +0400

    inetutils: update to 1.9.2

--- base/inetutils/package.sh
+++ base/inetutils/package.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 pkgname=inetutils
-pkgver=1.9.1
-ARCH_VERSION=1.9.1.341
+pkgver=1.9.2
+#ARCH_VERSION=1.9.1.341
 urls="http://ftp.gnu.org/gnu/inetutils/inetutils-${pkgver}.tar.gz"
 srctar=${pkgname}-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
