2b673d7058c49ba52077d6459e3bb66024158fcf
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-01-19 00:53:34 +0400

    update imlib2 to 1.4.6

--- multimedia/imlib2/package.sh
+++ multimedia/imlib2/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=imlib2
-pkgver=1.4.5
+pkgver=1.4.6
 urls="http://downloads.sourceforge.net/enlightenment/imlib2-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
