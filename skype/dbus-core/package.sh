99b274a279ff11491b56fd06fce9e0d9ebad09d5
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-01-27 14:08:01 +0400

    dbus: fix arch lib32 package name

--- skype/dbus-core/package.sh
+++ skype/dbus-core/package.sh
@@ -2,6 +2,7 @@
 
 pkgname=dbus-core
 ARCH_NAME=dbus
+ARCH_NAME_LIB32=lib32-libdbus
 pkgver=1.8.0
 urls="http://dbus.freedesktop.org/releases/dbus/dbus-${pkgver}.tar.gz"
 srctar=dbus-${pkgver}.tar.gz
