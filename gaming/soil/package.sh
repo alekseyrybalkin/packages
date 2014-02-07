ed3c5a5b41aae4160e7c7e83181eae5008880839
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-02-07 14:20:43 +0400

    add soil

---
+++ gaming/soil/package.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+pkgname=soil
+pkgver=1.16
+urls="http://www.lonesock.net/files/soil.zip"
+srczip=${pkgname}.zip
+
+kiin_make() {
+  mv "Simple OpenGL Image Library" soil
+  cd ${location}/soil
+  sed 's/-Wall/-Wall -fPIC/' -i "projects/makefile/alternate Makefile.txt"
+  sed 's/1.07-20071110/1.16/' -i "projects/makefile/alternate Makefile.txt"
+  cd src
+  make -f "../projects/makefile/alternate Makefile.txt"
+}
+
+kiin_install() {
+  cd ${location}/soil/src
+  make -f "../projects/makefile/alternate Makefile.txt" DESTDIR=${pkgdir} install
+  rm -rf ${location}/soil
+}
