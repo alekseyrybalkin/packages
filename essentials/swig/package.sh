108ce57570f1af64d68bd37778c29b430bc6069b
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-02-11 13:42:40 +0400

    update swig, gstreamer and gst-plugins-base

--- essentials/swig/package.sh
+++ essentials/swig/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=swig
-pkgver=2.0.11
+pkgver=2.0.12
 urls="http://prdownloads.sourceforge.net/swig/swig-${pkgver}.tar.gz"
 srctar=${pkgname}-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
