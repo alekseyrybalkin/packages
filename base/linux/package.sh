f7a7f3b68bc1b574fd14df132468363635ffac73
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-02-21 12:56:15 +0400

    update linux, postgresql and tmux

--- base/linux/package.sh
+++ base/linux/package.sh
@@ -4,8 +4,8 @@ pkgname=linux
 SKIP_ARCH_CHECK=1
 git_pkgname=linux-stable
 vcs="git"
-gittag=v3.13.3
-pkgver=3.13.3
+gittag=v3.13.4
+pkgver=3.13.4
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
