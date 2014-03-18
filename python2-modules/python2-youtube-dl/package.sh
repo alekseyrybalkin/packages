2f9ecd6c11949fdd83668d9ef875c16b1a8fbb82
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-03-19 01:05:40 +0400

    add python2-youtube-dl

---
+++ python2-modules/python2-youtube-dl/package.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+pkgname=python2-youtube-dl
+ARCH_NAME=youtube-dl
+pkgver=2014.03.17
+urls="http://youtube-dl.org/downloads/${pkgver}/youtube-dl-${pkgver}.tar.gz"
+srctar=youtube-dl-${pkgver}.tar.gz
+srcdir=${location}/youtube-dl
+
+kiin_make() {
+  echo "nothing to make..."
+}
+
+kiin_install() {
+  python2 setup.py install --root=${pkgdir} --optimize=1
+  rm -rf ${pkgdir}/usr/etc
+}
