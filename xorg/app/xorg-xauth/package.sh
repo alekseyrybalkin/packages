9af05716456ac885e1157236ba53fcaa1aa10a8e
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-04-03 21:29:32 +0400

    update xorg-xauth

--- xorg/app/xorg-xauth/package.sh
+++ xorg/app/xorg-xauth/package.sh
@@ -2,7 +2,7 @@
 
 _pkgname=xauth
 pkgname=xorg-${_pkgname}
-pkgver=1.0.8
+pkgver=1.0.9
 urls="http://xorg.freedesktop.org/releases/individual/app/${_pkgname}-${pkgver}.tar.bz2"
 srctar=${_pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${_pkgname}-${pkgver}
