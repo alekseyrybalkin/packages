412a3069bdfdc499e761350877f2a09826906326
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-04-14 18:34:49 +0400

    update xproto

--- xorg/proto/xproto/package.sh
+++ xorg/proto/xproto/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=xproto
-pkgver=7.0.25
+pkgver=7.0.26
 urls="http://xorg.freedesktop.org/releases/individual/proto/${pkgname}-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
