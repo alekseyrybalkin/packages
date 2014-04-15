dcc4177c517e245cfff4e936485f4f553ddd40fc
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-04-15 14:26:29 +0400

    shared-mime-info: fix known files for 1.3

--- gui/shared-mime-info/package.sh
+++ gui/shared-mime-info/package.sh
@@ -25,13 +25,4 @@ kiin_after_upgrade() {
   kiin_after_install
 }
 
-known="usr/share/applications/mimeinfo.cache \
-  usr/share/mime/application/vnd.ms-word.xml \
-  usr/share/mime/application/vnd.ms-powerpoint.template.macroenabled.12.xml \
-  usr/share/mime/application/vnd.ms-excel.sheet.binary.macroenabled.12.xml \
-  usr/share/mime/application/vnd.ms-excel.template.macroenabled.12.xml \
-  usr/share/mime/application/vnd.ms-excel.sheet.macroenabled.12.xml \
-  usr/share/mime/application/vnd.sun.xml.base.xml \
-  usr/share/mime/application/vnd.ms-powerpoint.presentation.macroenabled.12.xml \
-  usr/share/mime/application/vnd.ms-word.document.macroenabled.12.xml \
-  usr/share/mime/application/vnd.ms-word.template.macroenabled.12.xml"
+known="usr/share/applications/mimeinfo.cache"
