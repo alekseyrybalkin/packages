5579cd50e2e58ad1f6d85c0f0fc82520a256cca7
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-04-19 01:32:32 +0400

    update nmap, libdvdread, gstreamer and gst-plugins-base

--- multimedia/libdvdread/package.sh
+++ multimedia/libdvdread/package.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 pkgname=libdvdread
-pkgver=4.2.1
+pkgver=4.9.9
 urls="http://dvdnav.mplayerhq.hu/releases/${pkgname}-${pkgver}.tar.xz"
 srctar=${pkgname}-${pkgver}.tar.xz
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  ./autogen.sh --prefix=/usr
+  ./configure --prefix=/usr --disable-static \
+    --docdir=/usr/share/doc/libdvdread
   make
 }
 
