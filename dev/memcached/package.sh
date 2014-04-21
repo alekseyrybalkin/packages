708667827b8242b408ce63c2633b5dcd3482a33d
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-04-22 01:00:59 +0400

    update qemu and memcached

--- dev/memcached/package.sh
+++ dev/memcached/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=memcached
-pkgver=1.4.17
+pkgver=1.4.18
 urls="http://www.memcached.org/files/${pkgname}-${pkgver}.tar.gz"
 srctar=${pkgname}-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
--- dev/qemu/package.sh
+++ dev/qemu/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=qemu
-pkgver=1.7.1
+pkgver=2.0.0
 extension=bz2
 folder="http://wiki.qemu.org/download/"
 check_server=1
