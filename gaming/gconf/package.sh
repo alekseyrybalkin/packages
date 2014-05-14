9bbefc2d3b62644a0fa3d3963766cb940047116f
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-05-15 01:43:20 +0400

    add dbus-glib and gconf for game dev tycoon

---
+++ gaming/gconf/package.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+pkgname=gconf
+majorver=3.2
+pkgver=${majorver}.6
+urls="http://ftp.gnome.org/pub/gnome/sources/GConf/${majorver}/GConf-${pkgver}.tar.xz"
+srctar=GConf-${pkgver}.tar.xz
+srcdir=${location}/GConf-${pkgver}
+
+kiin_make() {
+  ./configure --prefix=/usr --sysconfdir=/etc --disable-orbit --disable-static --libexecdir=/usr/lib/gconf
+  make
+}
+
+kiin_install() {
+  make DESTDIR=${pkgdir} install
+  ln -sv gconf.xml.defaults ${pkgdir}/etc/gconf/gconf.xml.system
+}
