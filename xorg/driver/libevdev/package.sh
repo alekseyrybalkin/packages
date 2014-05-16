e82355774328c8008fa578fa6ff6e3f956ac35f0
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-05-16 12:35:40 +0400

    add libevdev and update xf86-input-synaptics

---
+++ xorg/driver/libevdev/package.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+pkgname=libevdev
+pkgver=1.2
+urls="http://freedesktop.org/software/${pkgname}/${pkgname}-${pkgver}.tar.xz"
+srctar=${pkgname}-${pkgver}.tar.xz
+srcdir=${location}/${pkgname}-${pkgver}
+
+kiin_make() {
+  ./configure --prefix=/usr --disable-static
+  make
+}
+
+kiin_install() {
+  make DESTDIR=${pkgdir} install
+}
