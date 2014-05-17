90186736ed4edd7fa77857321bd1097fbd051210
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-05-18 02:05:54 +0400

    add libidn, for Isaac

---
+++ gaming/libidn/package.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+pkgname=libidn
+pkgver=1.28
+urls="http://ftp.gnu.org/gnu/libidn/libidn-${pkgver}.tar.gz"
+srctar=${pkgname}-${pkgver}.tar.gz
+srcdir=${location}/${pkgname}-${pkgver}
+
+kiin_make() {
+  ./configure --prefix=/usr --disable-static --libdir=${LIBDIR}
+  make
+}
+
+kiin_install() {
+  make DESTDIR=${pkgdir} install
+}
