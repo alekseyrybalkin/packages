a49761c2a2a671b10e6f91d7b10a6d030376ad91
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-05-27 20:21:46 +0400

    update e2fsprogs, readline and bash

--- base/e2fsprogs/package.sh
+++ base/e2fsprogs/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=e2fsprogs
-pkgver=1.42.9
+pkgver=1.42.10
 urls=" http://prdownloads.sourceforge.net/${pkgname}/${pkgname}-${pkgver}.tar.gz"
 srctar=${pkgname}-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
