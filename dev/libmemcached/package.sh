52edb55a9ece79dd316efa4fa66da68de8405b25
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-06-03 13:23:21 +0400

    update mercurial and libmemcached

--- dev/libmemcached/package.sh
+++ dev/libmemcached/package.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
 pkgname=libmemcached
-pkgver=1.0.17
-urls="http://launchpad.net/${pkgname}/1.0/${pkgver}/+download/${pkgname}-${pkgver}.tar.gz \
-  http://rybalkin.org/kiin-files/libmemcached-gcc-4.8.patch"
+pkgver=1.0.18
+urls="http://launchpad.net/${pkgname}/1.0/${pkgver}/+download/${pkgname}-${pkgver}.tar.gz"
 srctar=${pkgname}-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  # Fix build with GCC 4.8 (patch from Debian)
-  patch -Np1 -i ${KIIN_HOME}/tarballs/libmemcached-gcc-4.8.patch
   ./configure --prefix=/usr
   make
 }
