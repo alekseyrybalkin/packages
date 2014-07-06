a7c8aa0b5004f00afadd30c0fe48740e684eee6c
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-07-06 21:50:40 +0400

    update pixman

--- xorg/pixman/package.sh
+++ xorg/pixman/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=pixman
-pkgver=0.32.4
+pkgver=0.32.6
 urls="http://cairographics.org/releases/${pkgname}-${pkgver}.tar.gz"
 srctar=${pkgname}-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
