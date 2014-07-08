a2bf92c825977eae40e823d9f2a547ffd344e0f9
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-07-08 18:25:34 +0400

    bring back libreoffice

--- essentials/cups/package.sh
+++ essentials/cups/package.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
 pkgname=cups
-pkgver=1.7.1
-urls="http://www.cups.org/software/${pkgver}/cups-${pkgver}-source.tar.bz2"
+pkgver=1.7.3
+urls="http://www.cups.org/software/${pkgver}/cups-${pkgver}-source.tar.bz2 \
+  http://www.linuxfromscratch.org/patches/blfs/svn/cups-${pkgver}-blfs-1.patch \
+  http://www.linuxfromscratch.org/patches/blfs/svn/cups-${pkgver}-avahi_optional-1.patch"
 srctar=cups-${pkgver}-source.tar.bz2
 srcdir=${location}/cups-${pkgver}
 
 kiin_make() {
-  ./configure --with-rcdir=/tmp/cupsinit \
+  sed -i 's#@CUPS_HTMLVIEW@#firefox#' desktop/cups.desktop.in
+  patch -Np1 -i ${KIIN_HOME}/tarballs/cups-${pkgver}-avahi_optional-1.patch
+  patch -Np1 -i ${KIIN_HOME}/tarballs/cups-${pkgver}-blfs-1.patch
+  aclocal -I config-scripts
+  autoconf -I config-scripts
+  CC=gcc ./configure --with-rcdir=/tmp/cupsinit \
     --with-docdir=/usr/share/cups/doc \
     --with-system-groups=lpadmin \
     --libdir=$LIBDIR
