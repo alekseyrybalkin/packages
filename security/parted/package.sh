f7e7df14e0452d7d0980af62fae89469d7cc5a66
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-07-31 23:20:52 +0400

    update parted

--- security/parted/package.sh
+++ security/parted/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=parted
-pkgver=3.1
+pkgver=3.2
 urls="http://ftp.gnu.org/gnu/${pkgname}/${pkgname}-${pkgver}.tar.xz"
 srctar=${pkgname}-${pkgver}.tar.xz
 srcdir=${location}/${pkgname}-${pkgver}
