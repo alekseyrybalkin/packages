0ce0c198d3de4d94879e76713422845a6d3ecf6d
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-09-08 10:55:35 +0400

    strace: update to 4.9

--- dev/strace/package.sh
+++ dev/strace/package.sh
@@ -2,15 +2,12 @@
 
 pkgname=strace
 vcs="git"
-gittag=v4.8
-pkgver=4.8
+gittag=v4.9
+pkgver=4.9
 srcdir=${location}/${pkgname}-${pkgver}
-urls="http://www.linuxfromscratch.org/patches/downloads/strace/strace-${pkgver}-glibc_2.18_build_fix-1.patch"
 
 kiin_make() {
-  patch -Np1 -i ${KIIN_HOME}/tarballs/strace-${pkgver}-glibc_2.18_build_fix-1.patch
-  sed -i -e 's/AM_CONFIG_HEADER/AC_CONFIG_HEADERS/g' configure.ac
-  autoreconf -i -f
+  ./bootstrap
   ./configure --prefix=/usr
   make
 }
