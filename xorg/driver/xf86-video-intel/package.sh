743503e33c9e7ce07e28347578592e82a6c93189
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-09-08 14:42:38 +0400

    update xf86-video-intel

--- xorg/driver/xf86-video-intel/package.sh
+++ xorg/driver/xf86-video-intel/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=xf86-video-intel
-pkgver=2.99.914
+pkgver=2.99.916
 urls="http://xorg.freedesktop.org/releases/individual/driver/${pkgname}-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
