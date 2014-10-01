dfd1129c108e56d0c359a9c23e87e602743201f5
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-10-01 09:23:16 +0400

    update git

--- essentials/git/package.sh
+++ essentials/git/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=git
-pkgver=2.1.1
+pkgver=2.1.2
 urls="https://www.kernel.org/pub/software/scm/git/${pkgname}-${pkgver}.tar.xz \
   https://www.kernel.org/pub/software/scm/git/${pkgname}-manpages-${pkgver}.tar.xz"
 srctar=${pkgname}-${pkgver}.tar.xz
