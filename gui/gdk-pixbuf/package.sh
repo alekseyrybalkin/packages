8dcc3dc13fd654929e1f8de36f346c3b491da82c
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-10-13 21:57:51 +0400

    gdk-pixbuf: odd version numbers are stable too

--- gui/gdk-pixbuf/package.sh
+++ gui/gdk-pixbuf/package.sh
@@ -10,8 +10,6 @@ check_server=1
 
 . ${KIIN_REPO}/defaults.sh
 
-majorver_grep="^[0-9]+\.[0-9]*[02468]{1}/?$"
-
 kiin_make() {
   if [ -z "${KIIN_LIB32}" ]; then
     ./configure --prefix=/usr --with-x11 --libdir=$LIBDIR
