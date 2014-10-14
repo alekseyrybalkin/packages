5f4fd2039607617426c6e70893bbb9b2d95b271a
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-10-14 19:59:07 +0400

    bring back scummvm and mono

--- gaming/libgdiplus/package.sh
+++ gaming/libgdiplus/package.sh
@@ -1,21 +1,13 @@
 #!/bin/sh
 
 pkgname=libgdiplus
-pkgver=2.10.9
-urls="http://download.mono-project.com/sources/${pkgname}/${pkgname}-${pkgver}.tar.bz2 \
-  http://rybalkin.org/kiin-files/libgdiplus-${pkgver}-freetype25.patch \
-  http://rybalkin.org/kiin-files/libgdiplus-${pkgver}-gold.patch \
-  http://rybalkin.org/kiin-files/libgdiplus-${pkgver}-giflib5.patch \
-  http://rybalkin.org/kiin-files/libgdiplus-${pkgver}-libpng15.patch \
-  http://rybalkin.org/kiin-files/libgdiplus-${pkgver}-giflib-5.1.0.patch"
-srctar=${pkgname}-${pkgver}.tar.bz2
+pkgver=3.8
+urls="http://download.mono-project.com/sources/${pkgname}/${pkgname}-${pkgver}.tar.gz"
+srctar=${pkgname}-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  patch -Np0 -i ${KIIN_HOME}/tarballs/libgdiplus-${pkgver}-libpng15.patch
-  for i in giflib5 gold freetype25 giflib-5.1.0; do
-    patch -Np1 -i ${KIIN_HOME}/tarballs/libgdiplus-${pkgver}-${i}.patch
-  done
+  patch -Np1 -i ../libgdiplus0-giflib5.patch
   sed -i -e 's/-L\${libjpeg_prefix}\/lib -ljpeg/-ljpeg/' configure
   ./configure --prefix=/usr --with-cairo=system
   make
--- gaming/mono/package.sh
+++ gaming/mono/package.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
 pkgname=mono
-pkgver=3.4.0
+pkgver=3.10.0
 urls="http://download.mono-project.com/sources/mono/${pkgname}-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  patch -p1 < ../build_fix.patch
-  cp ../Microsoft.Portable.Common.targets mcs/tools/xbuild/targets/
-
   ./configure --prefix=/usr \
     --sysconfdir=/etc \
     --bindir=/usr/bin \
@@ -23,11 +20,4 @@ kiin_make() {
 
 kiin_install() {
   make DESTDIR=${pkgdir} install
-  # remove broken symlinks
-  rm ${pkgdir}/usr/lib/mono/xbuild/12.0/bin/Microsoft.Build.dll
-  rm ${pkgdir}/usr/lib/mono/xbuild/12.0/bin/Microsoft.Build.Engine.dll
-  rm ${pkgdir}/usr/lib/mono/xbuild/12.0/bin/Microsoft.Build.Framework.dll
-  rm ${pkgdir}/usr/lib/mono/xbuild/12.0/bin/Microsoft.Build.Tasks.v12.0.dll
-  rm ${pkgdir}/usr/lib/mono/xbuild/12.0/bin/Microsoft.Build.Utilities.v12.0.dll
-  rm ${pkgdir}/usr/lib/mono/xbuild/12.0/bin/Mono.XBuild.Tasks.dll
 }
