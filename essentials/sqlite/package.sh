4022d6f979fea7c695f3ef244f12af84f2575ec6
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-10-18 02:09:35 +0400

    update sqlite

--- essentials/sqlite/package.sh
+++ essentials/sqlite/package.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 pkgname=sqlite
-pkgver=3.8.6
-_pkgver=3080600
+pkgver=3.8.7
+_pkgver=3080700
 srctar=${pkgname}-autoconf-${_pkgver}.tar.gz
 urls="http://sqlite.org/2014/${pkgname}-autoconf-${_pkgver}.tar.gz"
 srcdir=${location}/${pkgname}-autoconf-${_pkgver}
