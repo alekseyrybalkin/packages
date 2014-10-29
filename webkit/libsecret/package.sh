760bf86d537766d34f9a84907f330f18f8cf30e9
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-10-29 13:44:05 +0300

    Revert "libsecret: manpage building is broken"
    This reverts commit 57248160cd05fe73537703d5fc82b082a36dc2a2.

--- webkit/libsecret/package.sh
+++ webkit/libsecret/package.sh
@@ -7,7 +7,7 @@ srctar=${pkgname}-${pkgver}.tar.xz
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  ./configure --prefix=/usr --disable-static --disable-manpages
+  ./configure --prefix=/usr --disable-static
   make
 }
 
