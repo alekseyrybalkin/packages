7d149e0c932e9ac2284666bd18a068db53f8183c
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-11-01 03:28:31 +0300

    update wine

--- gaming/wine/package.sh
+++ gaming/wine/package.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 pkgname=wine
-pkgver=1.7.29
-_pkgver=1.7.29
+pkgver=1.7.30
+_pkgver=1.7.30
 urls="http://prdownloads.sourceforge.net/${pkgname}/${pkgname}-${_pkgver}.tar.bz2"
 srctar=${pkgname}-${_pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${_pkgver}
