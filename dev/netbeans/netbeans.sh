4ab77c6aabae994860060688bd8ae2e4c5ebc503
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-11-03 11:48:59 +0300

    add netbeans

---
+++ dev/netbeans/netbeans.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true' /usr/share/netbeans/bin/netbeans
---
+++ dev/netbeans/package.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+pkgname=netbeans
+pkgver=8.0.1
+_subver=201408251540
+urls="http://dlc.sun.com.edgesuite.net/${pkgname}/${pkgver}/final/zip/${pkgname}-${pkgver}-${_subver}.zip"
+srczip=${pkgname}-${pkgver}-${_subver}.zip
+srcdir=${location}/${pkgname}
+
+kiin_make() {
+  echo "nothing to make..."
+}
+
+kiin_install() {
+  cd ../
+  mkdir -p ${pkgdir}/usr/{bin,share}
+  install -Dm755 netbeans.sh ${pkgdir}/usr/bin/netbeans
+  cp -r netbeans ${pkgdir}/usr/share/
+  rm -rf ${pkgdir}/usr/share/netbeans/bin/*exe
+  sed -i -e 's/^basedir=.*$/basedir=\/usr\/share\/netbeans/g' ${pkgdir}/usr/share/netbeans/bin/netbeans
+}
