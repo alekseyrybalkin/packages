e27ca3b1ce0b44fc96350fbc96bf5179576766f1
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-11-05 19:57:48 +0300

    update groff

--- base/groff/package.sh
+++ base/groff/package.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
 pkgname=groff
-pkgver=1.22.2
+pkgver=1.22.3
 urls="http://ftp.gnu.org/gnu/groff/groff-${pkgver}.tar.gz"
 srctar=${pkgname}-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
+  MAKEFLAGS=
   PAGE=A4 ./configure --prefix=/usr --without-x
   make
 }
 
 kiin_install() {
+  MAKEFLAGS=
   make prefix=${pkgdir}/usr install
   ln -sv eqn ${pkgdir}/usr/bin/geqn
   ln -sv tbl ${pkgdir}/usr/bin/gtbl
