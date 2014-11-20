3b1cb048217b7c0a4532538f885ddcc1ea61289e
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-11-21 02:50:09 +0300

    update xorg-xkbcomp

--- xorg/app/xorg-xkbcomp/package.sh
+++ xorg/app/xorg-xkbcomp/package.sh
@@ -2,7 +2,7 @@
 
 _pkgname=xkbcomp
 pkgname=xorg-${_pkgname}
-pkgver=1.2.4
+pkgver=1.3.0
 urls="http://xorg.freedesktop.org/releases/individual/app/${_pkgname}-${pkgver}.tar.bz2"
 srctar=${_pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${_pkgname}-${pkgver}
