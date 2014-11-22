7159f449aab86b9946121dd7beacd0977b2ed417
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-11-22 08:26:49 +0300

    update mesa

--- xorg/mesa/package.sh
+++ xorg/mesa/package.sh
@@ -2,7 +2,7 @@
 
 pkgname=mesa
 majorver=10.3
-pkgver=${majorver}.3
+pkgver=${majorver}.4
 patchver=10.2.6
 urls="ftp://ftp.freedesktop.org/pub/${pkgname}/${pkgver}/MesaLib-${pkgver}.tar.bz2 \
   http://www.linuxfromscratch.org/patches/blfs/svn/MesaLib-${patchver}-add_xdemos-3.patch"
