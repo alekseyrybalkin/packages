6fceef4241659c133a8b54639c2acd4a7079f887
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-12-12 00:41:47 +0300

    add llvm and blender

---
+++ dev/llvm/package.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+pkgname=llvm
+pkgver=3.5.0
+urls="http://llvm.org/releases/${pkgver}/llvm-${pkgver}.src.tar.xz"
+srctar=${pkgname}-${pkgver}.src.tar.xz
+srcdir=${location}/${pkgname}-${pkgver}.src
+
+kiin_make() {
+  CC=gcc CXX=g++ \
+  ./configure --prefix=/usr \
+    --sysconfdir=/etc \
+    --enable-libffi \
+    --enable-optimized \
+    --enable-shared \
+    --disable-assertions
+  make
+}
+
+kiin_install() {
+  make DESTDIR=${pkgdir} install
+  for file in ${pkgdir}/usr/lib/lib{clang,LLVM,LTO}*.a; do
+    test -f $file && chmod -v 644 $file
+  done
+  rm -rf ${pkgdir}/usr/docs
+}
---
+++ multimedia/blender/package.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+pkgname=blender
+vcs="git"
+pkgver=2.72b+
+gittag=19eb700fefa3d6f1151bae8078edeabd75f11dc2
+srcdir=${location}/${pkgname}-${pkgver}
+
+kiin_make() {
+  mkdir build
+  cd build
+  cmake .. \
+    -DCMAKE_INSTALL_PREFIX=/usr \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DWITH_INSTALL_PORTABLE=OFF \
+    -DWITH_PYTHON_INSTALL=OFF \
+    -DWITH_OPENCOLLADA=OFF \
+    -DWITH_GAMEENGINE=ON \
+    -DWITH_JACK=ON \
+    -DWITH_PLAYER=ON \
+    -DWITH_CODEC_FFMPEG=ON \
+    -DWITH_CODEC_SNDFILE=ON \
+    -DWITH_CYCLES=OFF \
+    -DWITH_LLVM=ON \
+    -DLLVM_VERSION=3.5 \
+    -DLLVM_STATIC=OFF \
+    -DWITH_CYCLES_CUDA_BINARIES=OFF \
+    -DWITH_CYCLES_OSL=OFF \
+    -DWITH_FFTW3=OFF \
+    -DWITH_MOD_OCEANSIM=OFF \
+    -DPYTHON_VERSION=3.4 \
+    -DPYTHON_LIBPATH=/usr/lib \
+    -DPYTHON_LIBRARY=python3.4m \
+    -DPYTHON_INCLUDE_DIRS=/usr/include/python3.4m \
+    -DCMAKE_CXX_FLAGS_RELEASE="-fno-delete-null-pointer-checks -DNDEBUG" \
+    -DCMAKE_C_FLAGS_RELEASE="-fno-delete-null-pointer-checks -DNDEBUG"
+  make
+}
+
+kiin_install() {
+  cd build
+  make DESTDIR=${pkgdir} install
+  python3 -m compileall "${pkgdir}/usr/share/blender"
+  python3 -O -m compileall "${pkgdir}/usr/share/blender"
+}
+
+kiin_after_install() {
+  update-desktop-database -q
+  update-mime-database /usr/share/mime
+}
+
+kiin_after_upgrade() {
+  kiin_after_install
+}
