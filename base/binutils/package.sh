2c083c2a2e1d3a1178d39185c75b5fbcf6070509
Aleksey Rybalkin <aleksey@rybalkin.org>
2014-12-24 23:47:01 +0300

    update binutils and gettext

--- base/binutils/package.sh
+++ base/binutils/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=binutils
-pkgver=2.24
+pkgver=2.25
 extension=gz
 folder="http://ftp.gnu.org/gnu/${pkgname}/"
 check_server=1
@@ -13,7 +13,7 @@ kiin_make() {
   sed -i.bak '/^INFO/s/standards.info //' etc/Makefile.in
   mkdir -v binutils-build
   cd binutils-build
-  ../configure --prefix=/usr --enable-shared --enable-multilib --disable-werror
+  ../configure --prefix=/usr --enable-shared --enable-multilib
   make tooldir=${pkgdir}/usr
 }
 
--- base/gettext/package.sh
+++ base/gettext/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=gettext
-pkgver=0.19.3
+pkgver=0.19.4
 urls="http://ftp.gnu.org/gnu/gettext/gettext-${pkgver}.tar.gz"
 srctar=${pkgname}-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
