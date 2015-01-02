269afe7136481dbd33ea7a3e4712e7ed32b14883
Aleksey Rybalkin <aleksey@rybalkin.org>
2015-01-02 21:50:34 +0300

    update libxshmfence

--- xorg/lib/libxshmfence/package.sh
+++ xorg/lib/libxshmfence/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=libxshmfence
-pkgver=1.1
+pkgver=1.2
 urls="http://xorg.freedesktop.org/releases/individual/lib/${pkgname}-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
