dbb6d9f1fbf212bec057b30b252fb0deb0fd399a
Aleksey Rybalkin <aleksey@rybalkin.org>
2015-01-10 18:53:13 +0300

    update dbus and ffmpeg

--- multimedia/ffmpeg/package.sh
+++ multimedia/ffmpeg/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=ffmpeg
-pkgver=2.5.2
+pkgver=2.5.3
 urls="http://ffmpeg.org/releases/ffmpeg-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
