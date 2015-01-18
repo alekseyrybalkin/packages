86c95ca848032915a74df1300703c979f6dbbede
Aleksey Rybalkin <aleksey@rybalkin.org>
2015-01-18 07:27:19 +0300

    update gnutls

--- webkit/gnutls/package.sh
+++ webkit/gnutls/package.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 pkgname=gnutls
-pkgver=3.3.11
-_pkgfolder=3.3.11
+pkgver=3.3.12
+_pkgfolder=3.3.12
 urls="ftp://ftp.gnutls.org/gcrypt/gnutls/v3.3/gnutls-${pkgver}.tar.xz"
 srctar=${pkgname}-${pkgver}.tar.xz
 srcdir=${location}/${pkgname}-${_pkgfolder}
