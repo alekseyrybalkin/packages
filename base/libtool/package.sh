71df8e24c9c9fe4f0ca5ab44bb689bb5efa0d09f
Aleksey Rybalkin <aleksey@rybalkin.org>
2015-01-20 13:00:56 +0300

    update libtool

--- base/libtool/package.sh
+++ base/libtool/package.sh
@@ -2,7 +2,7 @@
 
 pkgname=libtool
 ARCH_NAME_LIB32=libtool-multilib
-pkgver=2.4.4
+pkgver=2.4.5
 urls="http://ftp.gnu.org/gnu/libtool/libtool-${pkgver}.tar.gz"
 srctar=${pkgname}-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
