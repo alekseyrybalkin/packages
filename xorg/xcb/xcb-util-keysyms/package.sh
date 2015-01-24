5e80091b4d751587f5b3e9763acde9ace84ee6de
Aleksey Rybalkin <aleksey@rybalkin.org>
2015-01-24 10:34:19 +0300

    add xcb-util-keysyms

---
+++ xorg/xcb/xcb-util-keysyms/package.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+pkgname=xcb-util-keysyms
+pkgver=0.4.0
+urls="http://xcb.freedesktop.org/dist/${pkgname}-${pkgver}.tar.bz2"
+srctar=${pkgname}-${pkgver}.tar.bz2
+srcdir=${location}/${pkgname}-${pkgver}
+
+kiin_make() {
+  ./configure $XORG_CONFIG
+  make
+}
+
+kiin_install() {
+  make DESTDIR=${pkgdir} install
+}
