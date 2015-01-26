2f7e8a9e9c2e47961991fd7dcd9b98a3044e254d
Aleksey Rybalkin <aleksey@rybalkin.org>
2015-01-26 14:57:07 +0300

    update patch and device-mapper

--- base/patch/package.sh
+++ base/patch/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=patch
-pkgver=2.7.1
+pkgver=2.7.3
 urls="http://ftp.gnu.org/gnu/${pkgname}/${pkgname}-${pkgver}.tar.xz"
 srctar=${pkgname}-${pkgver}.tar.xz
 srcdir=${location}/${pkgname}-${pkgver}
--- security/device-mapper/package.sh
+++ security/device-mapper/package.sh
@@ -2,7 +2,7 @@
 
 pkgname=device-mapper
 ARCH_NAME=lvm2
-pkgver=2.02.114
+pkgver=2.02.115
 urls="ftp://sources.redhat.com/pub/lvm2/LVM2.${pkgver}.tgz"
 srctar=LVM2.${pkgver}.tgz
 srcdir=${location}/LVM2.${pkgver}
