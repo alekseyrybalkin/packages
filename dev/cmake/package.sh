43a2c6cc1cbac0b6b01046f322342920ffac6221
Aleksey Rybalkin <aleksey@rybalkin.org>
2015-02-06 08:06:52 +0300

    update cmake, postgresql, git and virtualenv

--- dev/cmake/package.sh
+++ dev/cmake/package.sh
@@ -2,19 +2,12 @@
 
 pkgname=cmake
 majorver=3.1
-pkgver=${majorver}.1
+pkgver=${majorver}.2
 extension=gz
-major_folder="http://www.cmake.org/files/"
+folder="http://www.cmake.org/files/v${majorver}/"
 check_server=1
 
 . ${KIIN_REPO}/defaults.sh
-
-folder="${major_folder}v${majorver}/"
-urls="${folder}${srctar}"
-majorver_grep="^v3\.[0-9]+/?$"
-majorver_seds() {
-  sed -r "s/\///g" | sed -r "s/^v//g"
-}
 
 kiin_make() {
   ./bootstrap --prefix=/usr \
