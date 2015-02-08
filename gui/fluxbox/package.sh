5a3310624785dc8226208e16d005d3ee7aaec48c
Aleksey Rybalkin <aleksey@rybalkin.org>
2015-02-08 22:11:20 +0300

    update goffice, fluxbox and shared-mime-info

--- gui/fluxbox/package.sh
+++ gui/fluxbox/package.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 pkgname=fluxbox
-pkgver=1.3.6
+pkgver=1.3.7
 urls="http://downloads.sourceforge.net/fluxbox/fluxbox-${pkgver}.tar.bz2"
 srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  patch -p1 -i ../fix_screen_search.diff
   ./configure --prefix=/usr --disable-imlib2
   make
 }
--- gui/shared-mime-info/package.sh
+++ gui/shared-mime-info/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=shared-mime-info
-pkgver=1.3
+pkgver=1.4
 urls="http://freedesktop.org/~hadess/shared-mime-info-${pkgver}.tar.xz"
 srctar=${pkgname}-${pkgver}.tar.xz
 srcdir=${location}/${pkgname}-${pkgver}
