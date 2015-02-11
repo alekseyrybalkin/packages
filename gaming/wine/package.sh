57701a4160747aea58769bfa014c8a7cb1476b47
Aleksey Rybalkin <aleksey@rybalkin.org>
2015-02-11 16:24:58 +0300

    bring back wine in all its UNSTRIPPED glory

--- gaming/wine/package.sh
+++ gaming/wine/package.sh
@@ -1,61 +1,35 @@
 #!/bin/sh
 
+KIIN_NO_STRIPPING=1
 pkgname=wine
-pkgver=1.7.32
-_pkgver=1.7.32
-urls="http://prdownloads.sourceforge.net/${pkgname}/${pkgname}-${_pkgver}.tar.bz2"
-srctar=${pkgname}-${_pkgver}.tar.bz2
-srcdir=${location}/${pkgname}-${_pkgver}
+pkgver=1.7.36
+urls="http://mirrors.ibiblio.org/wine/source/1.7/wine-${pkgver}.tar.bz2"
+srctar=${pkgname}-${pkgver}.tar.bz2
+srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  # ncurses fix
-  sed -i 's|libncurses|libncursesw|g' configure
-  sed -i 's|lncurses|lncursesw|g' configure
-
   mkdir ${location}/${pkgname}-32-build
   mkdir ${location}/${pkgname}-64-build
 
-  export CFLAGS="-march=native -O0 -pipe -D_FORTIFY_SOURCE=0"
-  export CXXFLAGS="-march=native -O0 -pipe -D_FORTIFY_SOURCE=0"
-
   cd ${location}/${pkgname}-64-build
-  ../${pkgname}-${_pkgver}/configure \
+  ../${pkgname}-${pkgver}/configure \
     --prefix=/usr \
     --libdir=/usr/lib \
     --with-x \
     --without-gstreamer \
-    --without-opencl \
-    --without-hal \
-    --without-sane \
-    --without-v4l \
-    --without-gphoto \
-    --without-oss \
-    --without-capi \
-    --without-cups \
-    --without-gsm \
-    --without-ldap \
     --enable-win64
   make
 
   export PKG_CONFIG_PATH="/usr/lib32/pkgconfig"
   cd ${location}/${pkgname}-32-build
-  ../${pkgname}-${_pkgver}/configure \
+  ../${pkgname}-${pkgver}/configure \
     --prefix=/usr \
     --libdir=/usr/lib32 \
     --with-x \
     --without-gstreamer \
-    --without-opencl \
-    --without-hal \
-    --without-sane \
-    --without-v4l \
-    --without-gphoto \
-    --without-oss \
-    --without-capi \
-    --without-cups \
-    --without-gsm \
-    --without-ldap \
     --with-wine64=${location}/${pkgname}-64-build
   make CFLAGS+="-mstackrealign -mincoming-stack-boundary=2" CXXFLAGS+="-mstackrealign -mincoming-stack-boundary=2"
+  unset PKG_CONFIG_PATH
 }
 
 kiin_install() {
@@ -72,4 +46,14 @@ kiin_install() {
   cd ..
   rm -rf ${pkgname}-32-build
   rm -rf ${pkgname}-64-build
+}
+
+kiin_after_install() {
+  getent group arwen >/dev/null || groupadd arwen
+  getent passwd arwen >/dev/null || \
+    useradd -m -g arwen -G audio,video -s /bin/bash arwen
+}
+
+kiin_after_upgrade() {
+  kiin_after_install
 }
