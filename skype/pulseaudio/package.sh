932881d05b6e8698195e00339028c99b8825435d
Aleksey Rybalkin <aleksey@rybalkin.org>
2015-02-13 13:08:03 +0300

    update cmake, perl-error and pulseaudio

--- skype/pulseaudio/package.sh
+++ skype/pulseaudio/package.sh
@@ -2,7 +2,7 @@
 
 pkgname=pulseaudio
 ARCH_NAME_LIB32=pulseaudio
-pkgver=5.99.3
+pkgver=6.0
 urls="http://freedesktop.org/software/pulseaudio/releases/pulseaudio-${pkgver}.tar.xz"
 srctar=${pkgname}-${pkgver}.tar.xz
 srcdir=${location}/${pkgname}-${pkgver}
