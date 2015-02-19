b807ee5f4c398ca592439c63b8f9f3a50530f4fa
Aleksey Rybalkin <aleksey@rybalkin.org>
2015-02-19 17:08:39 +0300

    add gtk-doc, build gtk2 from git repo

--- gui/gtk2/package.sh
+++ gui/gtk2/package.sh
@@ -2,17 +2,16 @@
 
 pkgname=gtk2
 pkgver=2.24.26
-urls="http://ftp.gnome.org/pub/gnome/sources/gtk+/2.24/gtk+-${pkgver}.tar.xz"
-srctar=gtk+-${pkgver}.tar.xz
+vcs=git
+git_pkgname=gtk+
+gittag=${pkgver}
 srcdir=${location}/gtk+-${pkgver}
 
 kiin_make() {
-  sed -i 's#l \(gtk-.*\).sgml#& -o \1#' docs/{faq,tutorial}/Makefile.in
-  sed -i 's#.*@man_#man_#' docs/reference/gtk/Makefile.in
-  # remove this with 2.24.26 release [ http://wiki.linuxfromscratch.org/blfs/ticket/5852 ]
-  sed -i '/GDK_PIXBUF_DISABLE_DEPRECATED/d' configure
+  NOCONFIGURE=1 ./autogen.sh
   ./configure --prefix=/usr \
     --sysconfdir=/etc \
+    --enable-gtk-doc \
     --libdir=$LIBDIR
   make
 }
@@ -21,7 +20,6 @@ kiin_install() {
   make DESTDIR=${pkgdir} install
   # fix conflict with gtk3
   mv ${pkgdir}/usr/bin/gtk{,2}-update-icon-cache
-  mv ${pkgdir}/usr/share/man/man1/gtk{,2}-update-icon-cache.1
 }
 
 kiin_after_install() {
