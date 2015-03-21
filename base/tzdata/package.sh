853ff31e47264312cd987bf49fd916fff3962948
Aleksey Rybalkin <aleksey@rybalkin.org>
2015-03-21 12:11:59 +0300

    update tzdata

--- base/tzdata/package.sh
+++ base/tzdata/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=tzdata
-pkgver=2015a
+pkgver=2015b
 urls="http://www.iana.org/time-zones/repository/releases/${pkgname}${pkgver}.tar.gz"
 _srctar=${pkgname}${pkgver}.tar.gz
 _srcdir=${location}/${pkgname}${pkgver}
