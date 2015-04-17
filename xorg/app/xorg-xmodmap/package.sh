4d7b9d82574f10e54bf3af41d5affa267026123a
Aleksey Rybalkin <aleksey@rybalkin.org>
2015-04-17 14:26:46 +0300

    update xorg-setxkbmap and xorg-xmodmap

--- xorg/app/xorg-xmodmap/package.sh
+++ xorg/app/xorg-xmodmap/package.sh
@@ -3,8 +3,8 @@
 _pkgname=xmodmap
 pkgname=xorg-${_pkgname}
 vcs="git"
-gittag=xmodmap-1.0.8
-pkgver=1.0.8
+gittag=xmodmap-1.0.9
+pkgver=1.0.9
 srcdir=${location}/${_pkgname}-${pkgver}
 
 kiin_make() {
