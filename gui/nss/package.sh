13133ce21cceb6897e7cd33f05e9a74612a15c28
Aleksey Rybalkin <aleksey@rybalkin.org>
2015-04-23 12:53:05 +0300

    update nss

--- gui/nss/package.sh
+++ gui/nss/package.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 pkgname=nss
-pkgver=3.18
-patchver=3.17.4
-folderver=3_18
+pkgver=3.18.1
+patchver=3.18.1
+folderver=3_18_1
 urls="http://ftp.mozilla.org/pub/mozilla.org/security/nss/releases/NSS_${folderver}_RTM/src/nss-${pkgver}.tar.gz \
   http://www.linuxfromscratch.org/patches/blfs/svn/nss-${patchver}-standalone-1.patch"
 srctar=${pkgname}-${pkgver}.tar.gz
