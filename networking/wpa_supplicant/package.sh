8d34963e5335377f37f729fdbfd7d1640cfef405
Aleksey Rybalkin <aleksey@rybalkin.org>
2015-04-26 16:25:30 +0300

    wpa_supplicant: build from git and apply security patch

--- networking/wpa_supplicant/package.sh
+++ networking/wpa_supplicant/package.sh
@@ -2,11 +2,12 @@
 
 pkgname=wpa_supplicant
 pkgver=2.4
-urls="http://hostap.epitest.fi/releases/${pkgname}-${pkgver}.tar.gz"
-srctar=${pkgname}-${pkgver}.tar.gz
+vcs=git
+gittag=hostap_2_4
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
+  git cherry-pick 9ed4eee345f85e3025c33c6e20aa25696e341ccd
   cp ../config ${pkgname}/.config
   cd ${pkgname}
   make BINDIR=/usr/bin LIBDIR=/lib
@@ -18,6 +19,4 @@ kiin_install() {
   mkdir -p ${pkgdir}/usr/share/man/man8
   cd ${pkgname}
   install -v -m755 wpa_{cli,passphrase,supplicant} ${pkgdir}/usr/bin
-  install -v -m644 doc/docbook/wpa_supplicant.conf.5 ${pkgdir}/usr/share/man/man5
-  install -v -m644 doc/docbook/wpa_{cli,passphrase,supplicant}.8 ${pkgdir}/usr/share/man/man8
 }
