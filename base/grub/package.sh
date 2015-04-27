2d45ec4b7dfa42092b5e1ace19850615cf81357f
Aleksey Rybalkin <aleksey@rybalkin.org>
2015-04-27 04:24:30 +0300

    grub: update and build from git

--- base/grub/package.sh
+++ base/grub/package.sh
@@ -1,19 +1,15 @@
 #!/bin/sh
 
 pkgname=grub
-pkgver=2.00
+pkgver=2.02+
+vcs=git
+gittag=70b002de55abbfd6b2d2f2ea4408078d93c9e0de
 ARCH_VERSION=2.02.beta2
-urls="http://ftp.gnu.org/gnu/grub/grub-${pkgver}.tar.xz \
-  http://rybalkin.org/kiin-files/unicode.pf2"
-srctar=${pkgname}-${pkgver}.tar.xz
+urls="http://rybalkin.org/kiin-files/unicode.pf2"
 srcdir=${location}/${pkgname}-${pkgver}
 
-# FIXME: grub is broken if built with gcc5
 kiin_make() {
-  sed -i -e '/gets is a/d' grub-core/gnulib/stdio.in.h
-  #don't build against fuse (will not build /usr/bin/grub-mount
-  sed -i -e 's/fuse_main_real/fuse_main_real_fail/g' configure
-  sed -i -e 's/fuse_main_real/fuse_main_real_fail/g' configure.ac
+  ./autogen.sh
   ./configure --prefix=/usr \
     --sbindir=/usr/bin \
     --sysconfdir=/etc \
@@ -29,13 +25,6 @@ kiin_install() {
   make DESTDIR="${pkgdir}/" install
   mkdir -pv ${pkgdir}/boot/grub/i386-pc
   cp -av ${pkgdir}/usr/lib/grub/i386-pc/*.{mod,lst,img} ${pkgdir}/boot/grub/i386-pc
-  mkdir -pv ${pkgdir}/boot/grub/locale
-  for dir in ${pkgdir}/usr/share/locale/*; do
-    if test -f "$dir/LC_MESSAGES/grub.mo"; then
-      cp -fv "$dir/LC_MESSAGES/grub.mo" "${pkgdir}/boot/grub/locale/${dir##*/}.mo"
-    fi
-  done
-  cp -av ${pkgdir}/boot/grub/locale/en@quot.mo ${pkgdir}/boot/grub/locale/en.mo
   mkdir ${pkgdir}/boot/grub/fonts
   cp -av ${KIIN_HOME}/tarballs/unicode.pf2 ${pkgdir}/boot/grub/fonts
 }
