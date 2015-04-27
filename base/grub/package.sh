d0a834f0964824b92f051995a175cddc84f060e5
Aleksey Rybalkin <aleksey@rybalkin.org>
2015-04-28 00:41:43 +0300

    grub: do not use fancy fonts

--- base/grub/package.sh
+++ base/grub/package.sh
@@ -5,7 +5,6 @@ pkgver=2.02+
 vcs=git
 gittag=70b002de55abbfd6b2d2f2ea4408078d93c9e0de
 ARCH_VERSION=2.02.beta2
-urls="http://rybalkin.org/kiin-files/unicode.pf2"
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
@@ -25,8 +24,6 @@ kiin_install() {
   make DESTDIR="${pkgdir}/" install
   mkdir -pv ${pkgdir}/boot/grub/i386-pc
   cp -av ${pkgdir}/usr/lib/grub/i386-pc/*.{mod,lst,img} ${pkgdir}/boot/grub/i386-pc
-  mkdir ${pkgdir}/boot/grub/fonts
-  cp -av ${KIIN_HOME}/tarballs/unicode.pf2 ${pkgdir}/boot/grub/fonts
 }
 
 known="boot/grub/i386-pc/core.img \
