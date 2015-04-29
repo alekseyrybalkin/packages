9a67c93dd4f2e160e0bcdfaee5e88cee8c64fd8c
Aleksey Rybalkin <aleksey@rybalkin.org>
2015-04-29 03:29:53 +0300

    xorg-fonts-misc: build from git repos

--- xorg/xorg-fonts-misc/package.sh
+++ xorg/xorg-fonts-misc/package.sh
@@ -2,33 +2,17 @@
 
 pkgname=xorg-fonts-misc
 pkgver=1.0.3
-url="http://xorg.freedesktop.org/"
-urls="${url}/releases/individual/font/font-arabic-misc-${pkgver}.tar.bz2 \
-  ${url}/releases/individual/font/font-cursor-misc-${pkgver}.tar.bz2 \
-  ${url}/releases/individual/font/font-daewoo-misc-${pkgver}.tar.bz2 \
-  ${url}/releases/individual/font/font-dec-misc-${pkgver}.tar.bz2 \
-  ${url}/releases/individual/font/font-isas-misc-${pkgver}.tar.bz2 \
-  ${url}/releases/individual/font/font-jis-misc-${pkgver}.tar.bz2 \
-  ${url}/releases/individual/font/font-micro-misc-${pkgver}.tar.bz2 \
-  ${url}/releases/individual/font/font-misc-misc-1.1.2.tar.bz2 \
-  ${url}/releases/individual/font/font-mutt-misc-${pkgver}.tar.bz2 \
-  ${url}/releases/individual/font/font-schumacher-misc-1.1.2.tar.bz2 \
-  ${url}/releases/individual/font/font-sony-misc-${pkgver}.tar.bz2 \
-  ${url}/releases/individual/font/font-sun-misc-${pkgver}.tar.bz2"
-#srctar=${_pkgname}-${pkgver}.tar.bz2
-#srcdir=${location}/${_pkgname}-${pkgver}
 
 kiin_make() {
   mkdir build
   cd build
-  for i in ${KIIN_HOME}/tarballs/font-{arabic,cursor,daewoo,dec,isas,jis,micro,mutt,sony,sun}-misc-${pkgver}.tar.bz2 \
-    ${KIIN_HOME}/tarballs/font-misc-misc-1.1.2.tar.bz2 \
-    ${KIIN_HOME}/tarballs/font-schumacher-misc-1.1.2.tar.bz2; do
-    tar xvf $i
+  for i in ${KIIN_HOME}/sources/font-{arabic,cursor,daewoo,dec,isas,jis,micro,mutt,sony,sun,misc,schumacher}-misc; do
+    git clone ${i}
   done
   for dir in *; do
     if [ -d "${dir}" ]; then
       pushd "${dir}"
+      ./autogen.sh
       ./configure $XORG_CONFIG
       make
       popd
