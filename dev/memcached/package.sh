666f92c2983c8955de608de83a622a2bbed96af8
Aleksey Rybalkin <aleksey@rybalkin.org>
2015-04-30 02:13:46 +0300

    memcached: fix build with automake-1.15

--- dev/memcached/package.sh
+++ dev/memcached/package.sh
@@ -7,6 +7,7 @@ gittag=${pkgver}
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
+  sed -i -e 's/automake-1.14/automake-1.15\ automake-1.14/g' autogen.sh
   ./autogen.sh
   ./configure --prefix=/usr
   make
