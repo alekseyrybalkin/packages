6594226fbae254d772d3cd76ef9c27b293e00462
Aleksey Rybalkin <aleksey@rybalkin.org>
2016-03-06 15:38:52 +0300

    xf86-video-intel: update Arch version

--- xorg/xf86-video-intel/package.sh
+++ xorg/xf86-video-intel/package.sh
@@ -2,6 +2,7 @@
 
 pkgname=xf86-video-intel
 pkgver=2.99.917+
+ARCH_VERSION=2.99.917+560+gd167280
 vcs=git
 git_repo=git://anongit.freedesktop.org/xorg/driver/xf86-video-intel
 gittag=d1672806a5222f00dcc2eb24ccddd03f727f71bc
