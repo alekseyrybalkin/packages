ba86f0a0c97e4d240cfe9fd15d8f823d5422e57e
Aleksey Rybalkin <aleksey@rybalkin.org>
2016-03-14 00:05:42 +0300

    linux-firmware: update for X220

--- networking/linux-firmware/package.sh
+++ networking/linux-firmware/package.sh
@@ -15,6 +15,6 @@ kiin_make() {
 kiin_install() {
   mkdir -pv ${pkgdir}/usr/lib/firmware
   if [ `hostname` == 'sol' ]; then
-    cp -v ./iwlwifi*7265* ${pkgdir}/usr/lib/firmware/
+    cp -v ./iwlwifi-6*.ucode ${pkgdir}/usr/lib/firmware/
   fi
 }
