06b1af0a93152aea8ac36ee50858a02774ff2af0
Aleksey Rybalkin <aleksey@rybalkin.org>
2016-03-26 16:03:42 +0300

    gnome-icon-theme: build from git

--- gui/gnome-icon-theme/package.sh
+++ gui/gnome-icon-theme/package.sh
@@ -3,6 +3,8 @@
 pkgname=gnome-icon-theme
 majorver=3.12
 pkgver=${majorver}.0
+vcs=git
+gittag=${pkgver}
 extension=xz
 major_folder="http://ftp.gnome.org/pub/gnome/sources/${pkgname}/"
 check_server=1
@@ -12,12 +14,15 @@ check_server=1
 majorver_grep="^[0-9]+\.[0-9]*[02468]{1}/?$"
 
 kiin_make() {
-  ./configure --prefix=/usr
+  ./autogen.sh
+  ./configure --prefix=/usr --datadir=/usr/share
   make
 }
 
 kiin_install() {
   make DESTDIR=${pkgdir} install
+  mkdir -p ${pkgdir}/usr/share
+  mv ${pkgdir}/usr/locale ${pkgdir}/usr/share/
 }
 
 kiin_after_install() {
