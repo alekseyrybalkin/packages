6fc3628b81be9a01958b426f5ae7a9c903044c6d
Aleksey Rybalkin <aleksey@rybalkin.org>
2016-03-28 21:18:03 +0300

    openjdk: build from mercurial

--- dev-java/openjdk/package.sh
+++ dev-java/openjdk/package.sh
@@ -5,10 +5,11 @@ ARCH_NAME=java8-openjdk
 _java_ver=8
 _jdk_update=74
 _jdk_build=02
-pkgver=${_java_ver}.u${_jdk_update}
 _repo_ver=jdk${_java_ver}u${_jdk_update}-b${_jdk_build}
+pkgver=${_java_ver}.u${_jdk_update}
+vcs=mercurial
+hgtag=${_repo_ver}
 _url_src="http://hg.openjdk.java.net/jdk8u/jdk8u"
-# TODO: add all subprojects to urls!
 urls="${_url_src}/archive/${_repo_ver}.tar.bz2"
 srctar=${_repo_ver}.tar.bz2
 srcdir=${location}/jdk8u-${_repo_ver}
@@ -16,12 +17,10 @@ srcdir=${location}/jdk8u-${_repo_ver}
 kiin_make() {
     MAKEFLAGS=
     for subproject in corba hotspot jdk jaxws jaxp langtools nashorn; do
-        if [ ! -f ${KIIN_HOME}/tarballs/${subproject}-${_repo_ver}.tar.bz2 ]; then
-            wget -c ${_url_src}/${subproject}/archive/${_repo_ver}.tar.bz2 \
-                -O ${KIIN_HOME}/tarballs/${subproject}-${_repo_ver}.tar.bz2
-        fi
-        mkdir -pv ${subproject}
-        tar xf ${KIIN_HOME}/tarballs/${subproject}-${_repo_ver}.tar.bz2 --strip-components=1 -C ${subproject}
+        hg clone ${SOURCES_HOME}/openjdk-${subproject} ${subproject}
+        cd ${subproject}
+        hg update -r ${hgtag}
+        cd ../
     done
     export PATH=${PATH}:/usr/lib/openjdk/bin
     sh ./configure \
