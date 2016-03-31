d598c0413f20829782352def39c69f616e298b59
Aleksey Rybalkin <aleksey@rybalkin.org>
2016-03-31 13:13:28 +0300

    ttf-ubuntu: add nohinting version of mono font

--- gui/ttf-ubuntu/package.sh
+++ gui/ttf-ubuntu/package.sh
@@ -3,7 +3,9 @@
 pkgname=ttf-ubuntu
 ARCH_NAME=ttf-ubuntu-font-family
 pkgver=0.83
-urls="http://font.ubuntu.com/download/ubuntu-font-family-${pkgver}.zip"
+_nohinting_ver=0.80
+urls="http://font.ubuntu.com/download/ubuntu-font-family-${pkgver}.zip \
+    https://launchpad.net/ubuntu-font-mono-nohinting/trunk/${_nohinting_ver}/+download/ubuntu-font-mono-nohinting-${_nohinting_ver}.zip"
 srczip=ubuntu-font-family-${pkgver}.zip
 srcdir=${location}/ubuntu-font-family-${pkgver}
 
@@ -12,9 +14,11 @@ kiin_make() {
 }
 
 kiin_install() {
+  unzip ${KIIN_HOME}/tarballs/ubuntu-font-mono-nohinting-${_nohinting_ver}.zip
   install -m755 -d ${pkgdir}/usr/share/fonts/TTF
   install -m644 Ubuntu-{L,R,B,C}*.ttf ${pkgdir}/usr/share/fonts/TTF/
   install -m644 UbuntuMono-*.ttf ${pkgdir}/usr/share/fonts/TTF/
+  install -m644 ubuntu-font-mono-nohinting/UbuntuMonoNohinting-*.ttf ${pkgdir}/usr/share/fonts/TTF/
 }
 
 kiin_after_install() {
