2760936f3bb79e36e0afdfbe388761f5d8f1e25b
Aleksey Rybalkin <aleksey@rybalkin.org>
2016-04-17 20:09:48 +0300

    update file

--- base/file/package.sh
+++ base/file/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=file
-pkgver=5.25
+pkgver=5.26
 vcs=git
 gittag=FILE${pkgver//\./_}
 urls="ftp://ftp.astron.com/pub/file/file-${pkgver}.tar.gz"
@@ -9,6 +9,7 @@ srctar=${pkgname}-${pkgver}.tar.gz
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
+  git cherry-pick c4b6ea9b998882d08692599e28f4245714eaf8bb
   autoreconf -fi
   ./configure --prefix=/usr
   make
