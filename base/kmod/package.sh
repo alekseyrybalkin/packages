9ff08be4d83d5c5dd4f2f486e4c274a52a754817
Aleksey Rybalkin <aleksey@rybalkin.org>
2016-04-18 14:32:47 +0300

    kmod, tar: build from git

--- base/kmod/package.sh
+++ base/kmod/package.sh
@@ -2,13 +2,15 @@
 
 pkgname=kmod
 pkgver=22
-#vcs=git
-#gittag=v${pkgver}
+vcs=git
+gittag=v${pkgver}
 urls="http://www.kernel.org/pub/linux/utils/kernel/${pkgname}/${pkgname}-${pkgver}.tar.xz"
 srctar=${pkgname}-${pkgver}.tar.xz
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
+  touch libkmod/docs/gtk-doc.make
+  autoreconf -f -i
   ./configure --prefix=/usr \
     --sysconfdir=/etc \
     --with-xz \
--- base/tar/package.sh
+++ base/tar/package.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
-#vcs=git
-#git_repo=git://git.savannah.gnu.org/tar.git
-#git_problem="fails to build."
 pkgname=tar
 pkgver=1.28
+vcs=git
+gittag=release_${pkgver//\./_}
 urls="http://ftp.gnu.org/gnu/tar/tar-${pkgver}.tar.xz"
 srctar=${pkgname}-${pkgver}.tar.xz
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-  ./configure --prefix=/usr --libexecdir=/usr/bin
+  git clone ${KIIN_HOME}/sources/gnulib
+  git clone ${KIIN_HOME}/sources/paxutils
+  # use `file-has-acl` (instead of `acl`) gnulib module
+  git cherry-pick d95457e0071f26b1e524c754adf211abdd53d0fa
+  ./bootstrap
+  ./configure --prefix=/usr --enable-gcc-warnings=no --libexecdir=/usr/bin
   make
 }
 
