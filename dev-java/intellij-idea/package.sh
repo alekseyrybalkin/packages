1f93a0d115c0dbeba7b5a830db0b5bc603b3b337
Aleksey Rybalkin <aleksey@rybalkin.org>
2016-05-02 17:54:47 +0300

    update intellij-idea

--- dev-java/intellij-idea/package.sh
+++ dev-java/intellij-idea/package.sh
@@ -3,23 +3,24 @@
 KIIN_NON_FREE=1 # has jars in source tree
 pkgname=intellij-idea
 SKIP_ARCH_CHECK=1
-_pkgbase=145
-pkgver=${_pkgbase}.955
+pkgver=145
 vcs=git
-gittag=idea/${pkgver}
+gittag=670d130f31125a26282b438ade9762f3ff553355
+kotlinver=1.0.0-release-IJ143-78
+extra_urls="https://teamcity.jetbrains.com/guestAuth/repository/download/Kotlin_Rc_Idea142branch150versionNoTests/${kotlinver}/kotlin-plugin-${kotlinver}.zip"
 srcdir=${location}/idea-IC-${pkgver}
 
 kiin_make() {
-    sed -i -e 's/buildNumber = new URL(buildLocator.buildNumberUrl).text/buildNumber = "1.0.0-release-IJ143-78"/g' \
+    sed -i -e "s/buildNumber = new URL(buildLocator.buildNumberUrl).text/buildNumber = \"${kotlinver}\"/g" \
         build/scripts/download_kotlin.gant
-    cp ${KIIN_HOME}/tarballs/kotlin-plugin-1.0.0-release-IJ143-78.zip build/
+    cp ${KIIN_HOME}/tarballs/kotlin-plugin-${kotlinver}.zip build/
     rm -rf python/{ipnb,educational-{core,python}}
     sed -i -e 's/if (p("jdk.linux") != "false")/if (false)/g' build/scripts/dist.gant
     export PATH=${PATH}:/usr/lib/openjdk/bin:/usr/lib/apache-ant/bin
     ant
 
     cd python
-    cp ../out/artifacts/ideaIC-${_pkgbase}.SNAPSHOT{-no-jdk,}.tar.gz
+    cp ../out/artifacts/ideaIC-${pkgver}.SNAPSHOT{-no-jdk,}.tar.gz
     sed -i -e '/ipnb/d' build/plugin-list.txt
     sed -i -e '/ipnb\/src/d' build/python_plugin_build.gant
     mkdir trash
@@ -30,14 +31,14 @@ kiin_make() {
     sed -i -e "s/major = .*/major = \"${_major}\"/g" build/python_plugin_build.gant
     sed -i -e "s/minor = .*/minor = \"${_minor}\"/g" build/python_plugin_build.gant
     sed -i -e '/ipython/d' build/python-plugin-dependencies.xml
-    ant -Didea.path=../out/artifacts/ -Didea.build.number=${_pkgbase}.SNAPSHOT plugin
+    ant -Didea.path=../out/artifacts/ -Didea.build.number=${pkgver}.SNAPSHOT plugin
 }
 
 kiin_install() {
-    tar xvf out/artifacts/ideaIC-${_pkgbase}.SNAPSHOT-no-jdk.tar.gz
-    cd idea-IC-${_pkgbase}.SNAPSHOT
+    tar xvf out/artifacts/ideaIC-${pkgver}.SNAPSHOT-no-jdk.tar.gz
+    cd idea-IC-${pkgver}.SNAPSHOT
     cd plugins
-    unzip ../../python/distCE/python-community-${_pkgbase}.SNAPSHOT.zip
+    unzip ../../python/distCE/python-community-${pkgver}.SNAPSHOT.zip
     cd ../
     mkdir -p ${pkgdir}/usr/lib/intellij-idea
     cp -a ./* ${pkgdir}/usr/lib/intellij-idea
