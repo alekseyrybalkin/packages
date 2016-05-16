c23ffdcdd3355aa201b44157df001d83596394ff
Aleksey Rybalkin <aleksey@rybalkin.org>
2016-05-17 01:25:49 +0300

    Revert "update tar"
    This reverts commit a430c6fd87ddbc86db6918fd80b8dba5484f214a.

--- base/tar/package.sh
+++ base/tar/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=tar
-pkgver=1.29
+pkgver=1.28
 vcs=git
 gittag=release_${pkgver//\./_}
 urls="http://ftp.gnu.org/gnu/tar/tar-${pkgver}.tar.xz"
@@ -11,6 +11,8 @@ srcdir=${location}/${pkgname}-${pkgver}
 kiin_make() {
     git clone ${KIIN_HOME}/sources/gnulib
     git clone ${KIIN_HOME}/sources/paxutils
+    # use `file-has-acl` (instead of `acl`) gnulib module
+    git cherry-pick d95457e0071f26b1e524c754adf211abdd53d0fa
     ./bootstrap --skip-po
     ./configure --prefix=/usr --enable-gcc-warnings=no --libexecdir=/usr/bin
     make
