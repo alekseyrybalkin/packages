9312da835c715d113a7827776ef0a090691bfafd
Aleksey Rybalkin <aleksey@rybalkin.org>
2016-06-06 08:54:28 +0300

    make 4.2: fix parallel builds for some packages

--- base/make/package.sh
+++ base/make/package.sh
@@ -9,6 +9,8 @@ srctar=${pkgname}-${pkgver}.tar.bz2
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
+    # fix parallel builds for some packages, e.g. perl, git, ncurses
+    git cherry-pick 4762480ae9cb8df4878286411f178d32db14eff0
     sed -i -e 's/po //g' Makefile.am
     sed -i -e 's/doc //g' Makefile.am
     sed -i -e 's/-Werror//g' configure.ac
