a154eb1a786b345e9780a4f4b7fc5191fa77ad32
Aleksey Rybalkin <aleksey@rybalkin.org>
2016-06-09 15:58:43 +0300

    update gettext

--- base/gettext/package.sh
+++ base/gettext/package.sh
@@ -4,7 +4,7 @@
 #git_repo=git://git.savannah.gnu.org/gettext.git
 #git_problem="requires to download archive.dir.tar.xz from internets"
 pkgname=gettext
-pkgver=0.19.7
+pkgver=0.19.8
 urls="http://ftp.gnu.org/gnu/gettext/gettext-${pkgver}.tar.xz"
 srctar=${pkgname}-${pkgver}.tar.xz
 srcdir=${location}/${pkgname}-${pkgver}
