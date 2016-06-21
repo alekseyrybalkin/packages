0feab6e9c24e0aea94083de6dea37846548f76f8
Aleksey Rybalkin <aleksey@rybalkin.org>
2016-06-21 19:58:44 +0300

    update xf86-video-intel to the latest commit

--- xorg/xf86-video-intel/package.sh
+++ xorg/xf86-video-intel/package.sh
@@ -6,8 +6,8 @@ git_repo=git://anongit.freedesktop.org/xorg/driver/xf86-video-intel
 
 pkgver=2.99.917+
 # git describe --long | sed 's/-/+/g'
-ARCH_VERSION=2.99.917+590+g094924f
-gittag=094924f7f882da1f32395d4c982db6ad8e0f609f
+ARCH_VERSION=2.99.917+666+g7b6e219
+gittag=7b6e219fe461fbbe0bd498f7731781358d11f934
 
 srcdir=${location}/${pkgname}-${pkgver}
 
