046111cad37875493dba1c04aa20d1fc2cbb3893
Aleksey Rybalkin <aleksey@rybalkin.org>
2016-07-28 23:11:39 +0300

    falcon: build docs

--- python-modules/python-falcon/package.sh
+++ python-modules/python-falcon/package.sh
@@ -9,9 +9,15 @@ gittag=${pkgver}
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
+    # Migrate to sphinx.ext.napolean
+    git cherry-pick ad95b0ef281c65b34994858ebd445d2296c508ae
     python setup.py build
+    cd doc
+    make html
 }
 
 kiin_install() {
     python setup.py install --root=${pkgdir}
+    mkdir -pv ${pkgdir}/usr/share/doc
+    cp -r doc/_build/html ${pkgdir}/usr/share/doc/python-falcon
 }
