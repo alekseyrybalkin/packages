8b7b3ebfea31fcc9ad33be7782fbff6291938941
Aleksey Rybalkin <aleksey@rybalkin.org>
2016-10-16 21:04:03 +0500

    kernel: use commit from repo instead of patch

--- base/linux/package.sh
+++ base/linux/package.sh
@@ -9,7 +9,7 @@ gittag=v${pkgver}
 srcdir=${location}/${pkgname}-${pkgver}
 
 kiin_make() {
-    git apply ../conservative.diff
+    git cherry-pick abb6627910a1e783c8e034b35b7c80e5e7f98f41
     make mrproper
     cp -v ../config-x86_64-`hostname` .config
     make
