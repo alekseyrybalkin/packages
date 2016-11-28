010a4416c02a62e796b667788cf49b14c19b09af
Aleksey Rybalkin <aleksey@rybalkin.org>
2016-11-28 07:27:11 +0300

    update kernel, links and ffmpeg

--- essentials/links/package.sh
+++ essentials/links/package.sh
@@ -2,7 +2,7 @@
 
 #vcs=none
 pkgname=links
-pkgver=2.13
+pkgver=2.14
 extension=bz2
 folder="http://${pkgname}.twibright.com/download/"
 check_server=1
--- multimedia/ffmpeg/package.sh
+++ multimedia/ffmpeg/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=ffmpeg
-pkgver=3.2
+pkgver=3.2.1
 vcs=git
 gittag=n${pkgver}
 srcdir=${location}/${pkgname}-${pkgver}
