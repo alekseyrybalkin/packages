f630f8a851511932cbbb4ac6cf1b806f91ea25a9
Aleksey Rybalkin <aleksey@rybalkin.org>
2016-12-22 11:27:46 +0300

    docutils: better git svn clone

--- python-modules/python-docutils/package.sh
+++ python-modules/python-docutils/package.sh
@@ -5,18 +5,17 @@ ARCH_NAME=docutils
 pkgver=0.13.1
 vcs=git-svn
 vcs_pkgname=docutils-git-svn
-svnrev=7984
+vcs_clone="git svn clone -Ttrunk -ttags http://svn.code.sf.net/p/docutils/code ${vcs_pkgname}"
+gittag=origin/tags/docutils-${pkgver}
 urls="http://downloads.sourceforge.net/docutils/docutils-${pkgver}.tar.gz"
 srctar=docutils-${pkgver}.tar.gz
 srcdir=${location}/docutils-${pkgver}
 
 kiin_make() {
-    cd docutils
     python setup.py build
 }
 
 kiin_install() {
-    cd docutils
     python setup.py install --root=${pkgdir}
     find ${pkgdir}/usr/lib/python3*/site-packages -type f | xargs chmod 644
 }
