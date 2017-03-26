1abe081a859e55e10e2567d069da5716e3b98dc6
Aleksey Rybalkin <aleksey@rybalkin.org>
2017-03-26 20:51:54 +0500

    kernel: update and enable modules

--- base/linux/package.sh
+++ base/linux/package.sh
@@ -4,7 +4,7 @@ pkgname=linux
 SKIP_ARCH_CHECK=1
 vcs=git
 vcs_pkgname=linux-stable
-pkgver=4.10.5
+pkgver=4.10.6
 gittag=v${pkgver}
 srcdir=${location}/${pkgname}-${pkgver}
 
@@ -17,4 +17,14 @@ kiin_make() {
 kiin_install() {
     mkdir -pv ${pkgdir}/boot
     cp -v arch/x86_64/boot/bzImage ${pkgdir}/boot/vmlinuz-kiin
+
+    mkdir -pv ${pkgdir}/usr/lib/
+    make INSTALL_MOD_PATH=${pkgdir} modules_install
+    mv ${pkgdir}/lib/modules ${pkgdir}/usr/lib/
+    rmdir ${pkgdir}/lib
+
+    for link in build source; do
+        rm ${pkgdir}/usr/lib/modules/${pkgver}-kiin/${link}
+        ln -sv ${SOURCES_HOME}/${vcs_pkgname} ${pkgdir}/usr/lib/modules/${pkgver}-kiin/${link}
+    done
 }
