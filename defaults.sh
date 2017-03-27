1e03cc0fd561865fd12353f2b80062484f118bcd
Aleksey Rybalkin <aleksey@rybalkin.org>
2017-03-27 18:25:25 +0500

    better licensing and copyright information

--- defaults.sh
+++ defaults.sh
@@ -1,3 +1,24 @@
+#!/bin/sh
+
+# defaults.sh - default settings for packages
+
+# Copyright (C) 2013-2017 Aleksey Rybalkin
+
+# This file is part of kiin-repo.
+
+# kiin-repo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# kiin-repo is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with kiin-repo.  If not, see <http://www.gnu.org/licenses/>.
+
 if [ -z "${srctar}" ]; then
     srctar=${pkgname}-${pkgver}.tar.${extension}
 fi
--- essentials/kiin/package.sh
+++ essentials/kiin/package.sh
@@ -2,7 +2,7 @@
 
 pkgname=kiin
 SKIP_ARCH_CHECK=1
-pkgver=3
+pkgver=4
 vcs=git
 gittag=v${pkgver}
 srcdir=${location}/${pkgname}-${pkgver}
