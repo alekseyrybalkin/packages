9165b040d594b1871d6231412dec6b3cab94f114
Aleksey Rybalkin <aleksey@rybalkin.org>
2017-04-28 00:48:36 +0500

    update llvm

--- dev/llvm/package.sh
+++ dev/llvm/package.sh
@@ -1,24 +1,21 @@
 #!/bin/sh
 
-# used by icu, blender, protobuf, python-protobuf and rust
-# version locked by rust
+# used by icu, blender, protobuf, python-protobuf
 
 pkgname=llvm
-pkgver=3.9.1
+pkgver=4.0.0
 vcs=git
-gittag=a093ef43dd592b729da46db4ff3057fef9a46023
+gittag=4423e351176a92975739dd4ea43c2ff5877236ae
 relmon_id=1830
 
 kiin_make() {
-    sed -i -e 's/${TT_OUT}/"x86_64-pc-linux-gnu"/g' cmake/modules/GetHostTriple.cmake
-
     git clone -s -n ${SOURCES_HOME}/clang tools/clang
     cd tools/clang
-    git checkout 54f5752c3600d39ee8de62ba9ff304154baf5e80
+    git checkout 559aa046fe3260d8640791f2249d7b0d458b5700
     cd ../../
     git clone -s -n ${SOURCES_HOME}/compiler-rt projects/compiler-rt
     cd projects/compiler-rt
-    git checkout 9822763f9403c0740a01b7b5b50e3f15eec76b65
+    git checkout 1fdc27db84c9d0d9ae4ae60185629e8c43b4a11c
     cd ../../
     mkdir -v build
     cd build
@@ -28,8 +25,6 @@ kiin_make() {
         -DCMAKE_BUILD_TYPE=Release \
         -DBUILD_SHARED_LIBS=ON \
         -DLLVM_TARGETS_TO_BUILD="host;AMDGPU" \
-        -DLLVM_DEFAULT_TARGET_TRIPLE=x86_64-pc-linux-gnu \
-        -DLLVM_HOST_TRIPLE=x86_64-pc-linux-gnu \
         -Wno-dev ..
     make
 }
