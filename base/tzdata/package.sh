2ddfe16b5225a543b300e95851a1c0a220ba5f41
Aleksey Rybalkin <aleksey@rybalkin.org>
2017-05-08 09:35:01 +0300

    tzdata: set timezone to Europe/Moscow

--- base/tzdata/package.sh
+++ base/tzdata/package.sh
@@ -28,7 +28,7 @@ kiin_install() {
     unset ZONEINFO
 
     mkdir ${pkgdir}/etc
-    cp -v --remove-destination ${pkgdir}/usr/share/zoneinfo/Asia/Yekaterinburg \
+    cp -v --remove-destination ${pkgdir}/usr/share/zoneinfo/Europe/Moscow \
         ${pkgdir}/etc/localtime
     cd ${location}
     rm -rf ${_srcdir}
