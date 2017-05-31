6eabe4e5f69100fb47b9b34b25765f5b5941656f
Aleksey Rybalkin <aleksey@rybalkin.org>
2017-05-31 22:56:33 +0500

    update chromium

--- gui/chromium/package.sh
+++ gui/chromium/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=chromium
-pkgver=57.0.2987.154
+pkgver=59.0.3071.71
 #vcs=git
 #gittag=${pkgver}
 urls="https://commondatastorage.googleapis.com/chromium-browser-official/chromium-${pkgver}.tar.xz"
@@ -9,57 +9,49 @@ srctar=chromium-${pkgver}.tar.xz
 relmon_id=13344
 
 kiin_make() {
-    sed "s/^config(\"compiler\") {/&\ncflags_cc = [ \"-fno-delete-null-pointer-checks\" ]/" \
+    patch -Np1 -i ../gcc7.patch
+    sed -i -e 's/4, 6/4, 15/g' ui/gfx/linux/client_native_pixmap_dmabuf.cc
+
+    sed 's/^config("compiler") {/&\ncflags_cc = [ "-fno-delete-null-pointer-checks" ]/' \
         -i build/config/linux/BUILD.gn
 
     find . -name '*.py' -exec sed -i -r 's|/usr/bin/python$|&2|g' {} +
     mkdir -p ${srcdir}/python2-path
     ln -sf /usr/bin/python2 ${srcdir}/python2-path/python
 
-    for LIB in harfbuzz-ng libxslt yasm; do
-        find -type f -path "*third_party/$LIB/*" \
-            \! -path "*third_party/$LIB/chromium/*" \
-            \! -path "*third_party/$LIB/google/*" \
-            \! -regex '.*\.\(gn\|gni\|isolate\|py\)' \
-            -delete
-    done
-
-    python2 build/linux/unbundle/replace_gn_files.py \
-        --system-libraries harfbuzz-ng libxslt yasm
-
-    python2 third_party/libaddressinput/chromium/tools/update-strings.py
+    mkdir -p third_party/node/linux/node-linux-x64/bin
+    ln -s /usr/bin/node third_party/node/linux/node-linux-x64/bin/
 
     export PATH=${srcdir}/python2-path:${PATH}
     export TMPDIR=${srcdir}/temp
     mkdir -p ${TMPDIR}
 
-    GN_CONFIG=('clang_use_chrome_plugins=false'
-        'enable_hangout_services_extension=true'
-        'enable_nacl=false'
-        'enable_nacl_nonsfi=false'
-        'enable_widevine=false'
-        'fatal_linker_warnings=false'
-        'ffmpeg_branding="Chrome"'
-        'fieldtrial_testing_like_official_build=true'
-        'is_debug=false'
+    GN_CONFIG=(
         'is_clang=false'
-        'link_pulseaudio=true'
-        'linux_use_bundled_binutils=false'
-        'proprietary_codecs=true'
-        'remove_webcore_debug_symbols=true'
-        'symbol_level=0'
+        'clang_use_chrome_plugins=false'
+        'is_debug=false'
+        'fatal_linker_warnings=false'
         'treat_warnings_as_errors=false'
-        'use_allocator="none"'
-        'use_cups=true'
+        'fieldtrial_testing_like_official_build=true'
+        'remove_webcore_debug_symbols=true'
+        'ffmpeg_branding="Chrome"'
+        'proprietary_codecs=true'
+        'link_pulseaudio=false'
+        'linux_use_bundled_binutils=false'
         'use_gconf=false'
         'use_gnome_keyring=false'
         'use_gold=false'
-        'use_gtk3=false'
+        'use_sysroot=false'
+        'enable_hangout_services_extension=true'
+        'enable_widevine=false'
+        'enable_nacl=false'
+        'symbol_level=0'
+        'use_cups=true'
         'use_kerberos=false'
         'use_dbus=true'
         'use_libpci=false'
         'use_pulseaudio=false'
-        'use_sysroot=false')
+    )
     python2 tools/gn/bootstrap/bootstrap.py --gn-gen-args "${GN_CONFIG[*]}"
     out/Release/gn gen out/Release --args="${GN_CONFIG[*]}" \
         --script-executable=/usr/bin/python2
