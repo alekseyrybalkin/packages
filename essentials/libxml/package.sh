a6a529f5dcdd2e90de311688766e5135e186be97
Aleksey Rybalkin <aleksey@rybalkin.org>
2017-06-25 12:27:33 +0500

    libxml: stop checking server

--- essentials/libxml/package.sh
+++ essentials/libxml/package.sh
@@ -2,19 +2,12 @@
 
 pkgname=libxml
 ARCH_NAME=libxml2
-vcs=git
-vcs_pkgname=libxml2
-
 pkgver=2.9.4
 ARCH_VERSION=2.9.4+16+g07418011
+vcs=git
+vcs_pkgname=libxml2
 gittag=v${pkgver}
-
-extension=gz
-folder="http://xmlsoft.org/sources/"
-check_server=1
 relmon_id=1783
-
-. ${KIIN_REPO}/defaults.sh
 
 kiin_make() {
     sed -e 's|/usr/bin/python -u|/usr/bin/python2 -u|g' -e 's|/usr/bin/python$|/usr/bin/python2|g' -i python/tests/*.py
