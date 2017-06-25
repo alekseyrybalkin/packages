f6f0f4f015945accf5bbd620517b9c739fbfb86b
Aleksey Rybalkin <aleksey@rybalkin.org>
2017-06-26 02:03:38 +0500

    xrdb: disable ccache

--- xorg-app/xorg-xrdb/package.sh
+++ xorg-app/xorg-xrdb/package.sh
@@ -7,10 +7,12 @@ vcs=git
 gittag=${_pkgname}-${pkgver}
 
 kiin_make() {
+    export PATH=/usr/bin:${PATH}
     ./autogen.sh $XORG_CONFIG
     make
 }
 
 kiin_install() {
+    export PATH=/usr/bin:${PATH}
     make DESTDIR=${pkgdir} install
 }
