a8304ddcab2440795376b79ba6fa1b823027a59b
Aleksey Rybalkin <aleksey@rybalkin.org>
2017-11-06 02:32:46 +0500

    update fossil and xkeyboard-config

--- dev/fossil/package.sh
+++ dev/fossil/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=fossil
-pkgver=2.3
+pkgver=2.4
 vcs=fossil
 fossiltag=version-${pkgver}
 urls="https://www.fossil-scm.org/download/fossil-src-${pkgver}.tar.gz"
--- xorg/xkeyboard-config/package.sh
+++ xorg/xkeyboard-config/package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 pkgname=xkeyboard-config
-pkgver=2.21
+pkgver=2.22
 vcs=git
 git_repo=git://anongit.freedesktop.org/xkeyboard-config
 gittag=${pkgname}-${pkgver}
