4ecb32b3f76a4b17bcde184218a07a24e9596b3f
Aleksey Rybalkin <aleksey@rybalkin.org>
2019-06-23 16:43:57 +0300

    chrony: install user/group and service file

--- networking/chrony/package.sh
+++ networking/chrony/package.sh
@@ -8,13 +8,27 @@ relmon_id=8810
 
 kiin_make() {
     sed -i -e '/doc install$/d' Makefile.in
-    ./configure --prefix=/usr
+    ./configure --prefix=/usr \
+        --with-user=chrony
     make
 }
 
 kiin_install() {
     make DESTDIR=${pkgdir} install
+    install -Dm 644 examples/{chronyd,chrony-wait}.service -t ${pkgdir}/usr/lib/systemd/system
     mv ${pkgdir}/usr/sbin/chronyd ${pkgdir}/usr/bin
     rmdir ${pkgdir}/usr/sbin
     rm -rf ${pkgdir}/var
+}
+
+kiin_after_install() {
+    getent group chrony >/dev/null || groupadd -g 183 chrony
+    getent passwd chrony >/dev/null || \
+        useradd -c 'chrony' -d /var/lib/chrony -g chrony \
+        -s /bin/false -u 183 chrony
+    chown -R chrony:chrony /var/lib/chrony &> /dev/null
+}
+
+kiin_after_upgrade() {
+    kiin_after_install
 }
