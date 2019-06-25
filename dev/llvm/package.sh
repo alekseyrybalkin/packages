f8b698b4e41e870ab45d5d6694a15a0486ea1565
Aleksey Rybalkin <aleksey@rybalkin.org>
2019-06-25 14:52:50 +0300

    add llvm

---
+++ dev/llvm/package.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+pkgname=llvm
+pkgver=8.0.0
+vcs=git
+gittag=llvmorg-${pkgver}
+relmon_id=1830
+
+kiin_make() {
+    cd llvm
+    mv ../clang tools/
+    mv ../compiler-rt projects/
+    mkdir -v build
+    cd build
+    CC=gcc CXX=g++ \
+    cmake -DCMAKE_INSTALL_PREFIX=/usr \
+        -DLLVM_ENABLE_FFI=ON \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DLLVM_BUILD_LLVM_DYLIB=ON \
+        -DLLVM_LINK_LLVM_DYLIB=ON \
+        -DLLVM_ENABLE_RTTI=ON \
+        -DLLVM_TARGETS_TO_BUILD="host" \
+        -Wno-dev -G Ninja ..
+    ninja
+}
+
+kiin_install() {
+    cd llvm
+    cd build
+    DESTDIR=${pkgdir} ninja install
+    mv ${pkgdir}/usr/libexec/* ${pkgdir}/usr/bin/
+    rmdir ${pkgdir}/usr/libexec
+}
