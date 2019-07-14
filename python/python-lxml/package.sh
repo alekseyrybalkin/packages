59ec2e485e39f2e1fbb0cc436bbbfbd8e00e1cba
Aleksey Rybalkin <aleksey@rybalkin.org>
2019-07-14 14:11:52 +0300

    lxml: build html docs

--- python/python-lxml/package.sh
+++ python/python-lxml/package.sh
@@ -8,9 +8,16 @@ gittag=lxml-${pkgver}
 relmon_id=3914
 
 kiin_make() {
+    # python3
+    sed -i -e 's/itervalues/values/g' doc/mkhtml.py
     python setup.py build
+
+    cd doc
+    python mkhtml.py html-build .. ${pkgver}
 }
 
 kiin_install() {
     python setup.py install --skip-build --root=${pkgdir} --optimize=1
+    mkdir -p ${pkgdir}/usr/share/doc
+    cp -r doc/html-build ${pkgdir}/usr/share/doc/python-lxml
 }
