92e1ee34a5d295d609785e313590d967bf44566a
Aleksey Rybalkin <aleksey@rybalkin.org>
2019-07-23 21:49:34 +0300

    rubberband: build without ladspa and vamp plugins

--- audio/rubberband/package.sh
+++ audio/rubberband/package.sh
@@ -8,6 +8,15 @@ relmon_id=4222
 
 kiin_make() {
     sed -i -e '/cp.*JNI_TARGET/d' Makefile.in
+
+    # disable ladspa and vamp plugins
+    for plugin in ladspa vamp; do
+        sed -i -e "s/ @${plugin}_cflags@//Ig" Makefile.in
+        sed -i -e "s/ \$(${plugin}_objects)//Ig" Makefile.in
+        sed -i -e "s/ \$(${plugin}_target)//Ig" Makefile.in
+        sed -i -e "/${plugin}/Id" Makefile.in configure.ac
+    done
+
     autoreconf -vfi
     ./configure --prefix=/usr
     make
