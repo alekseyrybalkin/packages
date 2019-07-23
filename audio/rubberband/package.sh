5689c4f35d95145c2766b230e79262ed3bf7e5ff
Aleksey Rybalkin <aleksey@rybalkin.org>
2019-07-23 22:56:02 +0300

    rubberband: use kissfft instead of fftw

--- audio/rubberband/package.sh
+++ audio/rubberband/package.sh
@@ -7,6 +7,8 @@ hgtag=v${pkgver}
 relmon_id=4222
 
 kiin_make() {
+    patch -Np1 -i ../kissfft.patch
+
     sed -i -e '/cp.*JNI_TARGET/d' Makefile.in
 
     # disable ladspa and vamp plugins
@@ -16,6 +18,12 @@ kiin_make() {
         sed -i -e "s/ \$(${plugin}_target)//Ig" Makefile.in
         sed -i -e "/${plugin}/Id" Makefile.in configure.ac
     done
+
+    # use kissfft instead of fftw
+    sed -i -e '/fftw/Id' configure.ac
+    sed -i -e 's/ -DHAVE_FFTW3 -DFFTW_DOUBLE_ONLY/ -DUSE_KISSFFT/g' Makefile.in
+    sed -i -e "s/ @fftw_cflags@//Ig" Makefile.in
+    sed -i -e "s/ @fftw_libs@//Ig" Makefile.in
 
     autoreconf -vfi
     ./configure --prefix=/usr
