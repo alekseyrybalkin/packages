80a6dc6c6fbf79e570abdef0fdb4e6063342db7a
Aleksey Rybalkin <aleksey@rybalkin.org>
2019-07-24 15:14:53 +0300

    less: build from git

--- base/less/package.sh
+++ base/less/package.sh
@@ -1,18 +1,31 @@
 #!/bin/sh
 
-#vcs=none
 pkgname=less
 pkgver=551
-urls="http://www.greenwoodsoftware.com/${pkgname}/${pkgname}-${pkgver}.tar.gz"
-srctar=${pkgname}-${pkgver}.tar.gz
-# beta versions
-#relmon_id=1550
+vcs=git
+gittag=v${pkgver}
+relmon_id=1550
 
 kiin_make() {
+    # generate defines.h.in
+    autoheader
+
+    # don't create tarball
+    sed -i -e '/gpg/d' Makefile.aut
+    sed -i -e '/zip/d' Makefile.aut
+    sed -i -e '/tar/d' Makefile.aut
+    sed -i -e 's/rm -rf $$REL/true/g' Makefile.aut
+
+    # prepare sources in less-${pkgver}
+    make -f Makefile.aut dist
+
+    cd less-${pkgver}
+    autoreconf -fi
     ./configure --prefix=/usr --sysconfdir=/etc
     make
 }
 
 kiin_install() {
+    cd less-${pkgver}
     make prefix=${pkgdir}/usr install
 }
