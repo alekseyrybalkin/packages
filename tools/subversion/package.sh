c1f037ef6a621327cf78ee166e473050322de1ea
Aleksey Rybalkin <aleksey@rybalkin.org>
2019-07-24 13:50:06 +0300

    subversion: build from git

--- tools/subversion/package.sh
+++ tools/subversion/package.sh
@@ -1,25 +1,28 @@
 #!/bin/sh
 
-#vcs=git
 pkgname=subversion
-pkgver=1.12.0
-urls="http://www.apache.org/dist/${pkgname}/${pkgname}-${pkgver}.tar.bz2"
-srctar=${pkgname}-${pkgver}.tar.bz2
+pkgver=1.12.2
+vcs=git
+gittag=${pkgver}
 relmon_id=4905
 
 kiin_make() {
-    patch -Np1 -i ../apr-1.7.0-fix.diff
-
     MAKEFLAGS=
-    autoreconf -f -i
-    ./configure --prefix=/usr --with-apr=/usr --with-apr-util=/usr \
-        --with-zlib=/usr --with-serf=/usr --with-neon=/usr \
-        --with-sqlite=/usr --disable-static --with-lz4=internal \
-        --with-utf8proc=internal
-    make LT_LDFLAGS="-L$Fdestdir/usr/lib" local-all
+    export PYTHON=/usr/bin/python2
+    ./autogen.sh
+    ./configure --prefix=/usr \
+        --with-apr=/usr \
+        --with-apr-util=/usr \
+        --with-zlib=/usr \
+        --with-serf=/usr \
+        --with-sqlite=/usr \
+        --disable-static \
+        --with-lz4=internal \
+        --with-utf8proc=internal \
+        --enable-experimental-libtool
+    make local-all
+    mkdir -p subversion/bindings/swig/proxy
     make swig-pl
-    make swig-py swig_pydir=/usr/lib/python2.7/site-packages/libsvn \
-        swig_pydir_extra=/usr/lib/python2.7/site-packages/svn
 }
 
 kiin_install() {
@@ -29,8 +32,6 @@ kiin_install() {
     install -v -m755 -d ${pkgdir}/usr/share/doc/subversion-${pkgver}
     cp -v -R doc/* ${pkgdir}/usr/share/doc/subversion-${pkgver}
     make DESTDIR=${pkgdir} install-swig-pl
-    make DESTDIR=${pkgdir} install-swig-py swig_pydir=/usr/lib/python2.7/site-packages/libsvn \
-        swig_pydir_extra=/usr/lib/python2.7/site-packages/svn
     find ${pkgdir} -name perllocal.pod -delete
     find ${pkgdir} -name .packlist -delete
 }
