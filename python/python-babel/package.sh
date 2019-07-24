7bce14672abca201f5cbe60c319414a794fadcd3
Aleksey Rybalkin <aleksey@rybalkin.org>
2019-07-24 20:27:58 +0300

    babel: use cldr from git

--- python/python-babel/package.sh
+++ python/python-babel/package.sh
@@ -6,13 +6,15 @@ _cldr_core_ver=35.1
 vcs=git
 vcs_pkgname=babel
 gittag=v${pkgver}
-extra_urls="https://unicode.org/Public/cldr/${_cldr_core_ver}/core.zip"
-wget_set_filename=cldr-core-${_cldr_core_ver}.zip
 relmon_id=11984
 
 kiin_make() {
-    cp ${TARBALLS_HOME}/cldr-core-${_cldr_core_ver}.zip ./cldr/
-    python setup.py import_cldr
+    rm -rf cldr
+    git clone -s -n `find_vcs_repo cldr`
+    cd cldr
+    git checkout release-${_cldr_core_ver//\./-}
+    cd ../
+
     python setup.py build
 }
 
