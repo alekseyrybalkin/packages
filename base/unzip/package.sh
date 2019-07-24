84aaf370b6b996f9e7d4db5d842bb5c43ed7cc94
Aleksey Rybalkin <aleksey@rybalkin.org>
2019-07-24 18:26:23 +0300

    Revert "replace zip and unzip with minizip"
    This reverts commit 9ab0927223c9ff8ce6d2113f2c8fade35ea486c8.

---
+++ base/unzip/package.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#vcs=none
+pkgname=unzip
+pkgver=6.0
+urls="http://downloads.sourceforge.net/infozip/unzip60.tar.gz"
+srctar=unzip60.tar.gz
+srcdir=${location}/unzip60
+relmon_id=8684
+
+kiin_make() {
+    sed -i "/MANDIR =/s#)/#)/share/#" unix/Makefile
+    sed -i -e 's/CFLAGS="-O -Wall/& -DNO_LCHMOD/' unix/Makefile
+    make -f unix/Makefile linux_noasm
+}
+
+kiin_install() {
+    make prefix=${pkgdir}/usr install
+}
