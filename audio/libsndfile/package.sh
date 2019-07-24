c8832ea8cfa760ba628b2ec85bf16f0220a8f08a
Aleksey Rybalkin <aleksey@rybalkin.org>
2019-07-24 14:32:38 +0300

    libsndfile: build from git

--- audio/libsndfile/package.sh
+++ audio/libsndfile/package.sh
@@ -2,16 +2,23 @@
 
 pkgname=libsndfile
 pkgver=1.0.28
-#vcs=git
-#gittag=${pkgver}
-urls="http://www.mega-nerd.com/libsndfile/files/libsndfile-${pkgver}.tar.gz"
-srctar=${pkgname}-${pkgver}.tar.gz
+vcs=git
+gittag=${pkgver}
 relmon_id=13277
 
 kiin_make() {
+    # disable tests build
+    sed -i -e 's/examples tests/examples/g' Makefile.am
+    sed -i -e 's/regtest tests/regtest/g' Makefile.am
+    sed -i -e 's/tests\/Makefile//g' configure.ac
+    sed -i -e 's/tests\/pedantic-header-test.sh//g' configure.ac
+    sed -i -e 's/tests\/test_wrapper.sh//g' configure.ac
+
+    autoreconf -fi
     ./configure --prefix=/usr \
         --build=x86_64-unknown-linux-gnu \
-        --disable-sqlite
+        --disable-sqlite \
+        --disable-test-coverage
     make
 }
 
