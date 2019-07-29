b494ccd66c6b64020e76578c317528f3f8cf3b9f
Aleksey Rybalkin <aleksey@rybalkin.org>
2019-07-29 18:13:53 +0300

    mercurial: use python3

--- tools/mercurial/package.sh
+++ tools/mercurial/package.sh
@@ -6,14 +6,14 @@ relmon_id=1969
 relmon_rules="skip_rc"
 
 build() {
-    sed -i -e 's#env python#env python2#' mercurial/lsprof.py
-    sed -i -e 's/python/python2/g' doc/{docchecker,check-seclevel.py,runrst,Makefile,gendoc.py}
+    export HGPYTHON3=1
     cd doc
     make
 }
 
 package() {
-    python2 setup.py install --root=${pkgdir}/ --optimize=1
+    export HGPYTHON3=1
+    python setup.py install --root=${pkgdir}/ --optimize=1
     install -d ${pkgdir}/usr/share/man/{man1,man5}
     install -m644 doc/hg.1 ${pkgdir}/usr/share/man/man1
     install -m644 doc/{hgrc.5,hgignore.5} ${pkgdir}/usr/share/man/man5
