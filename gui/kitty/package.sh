c635c5e745c899fc6110fcbd4fae8593d0b74a42
Aleksey Rybalkin <aleksey@rybalkin.org>
2019-07-30 11:20:57 +0300

    update kitty

--- gui/kitty/package.sh
+++ gui/kitty/package.sh
@@ -1,10 +1,11 @@
 pkgname=kitty
-pkgver=0.14.2
+pkgver=0.14.3
 vcs=git
 gittag=v${pkgver}
 relmon_id=17405
 
 build() {
+    patch -Np1 -i ../remove_canberra.patch
     python setup.py linux-package --update-check-interval=0
 }
 
